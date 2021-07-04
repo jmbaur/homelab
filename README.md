6a276c7e76a3a89923996b0683e832677bd34337
Jared Baur <jaredbaur@fastmail.com>
2021-07-04 04:36:43 +0000

    new host and updated README

--- README.md
+++ README.md
@@ -2,7 +2,11 @@ Steps for a new machine:
 
 1. Install NixOS (https://nixos.org/manual/nixos/stable/index.html#ch-installation)
 1. Reboot into new installation and `cd /etc/nixos`
+1. `sudo chown -R <user>:users .`
 1. `git init .`
 1. `git remote add origin https://github.com/jmbaur/nixos-configs`
 1. `git branch -M main`
 1. `git pull origin main`
+1. `cp ./configuration.nix ./hosts/<hostname>/configuration.nix`
+1. Change imports in `./hosts/<hostname>/configuration.nix` to point to correct hardware-configuration.nix
+1. Change imports in `./configuration.nix` to just import `./hosts/<hostname>/configuration.nix`
---
+++ hosts/titan/configuration.nix
@@ -0,0 +1,112 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+
+  imports = [ ../../hardware-configuration.nix ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "titan"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  # time.timeZone = "America/Los_Angeles";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.eno2.useDHCP = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jared = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKU/J9T/6BwzloIiXP5wCkgkJbSl5B3z+c6Z/J3baa/u"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [ vim wget nixfmt htop tmux git ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      sleep 2
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then
+        exit 0
+      fi
+      ${tailscale}/bin/tailscale up -authkey $(cat /var/lib/tailscale.key)
+      rm /var/lib/tailscale.key
+    '';
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      signal-cli-rest-api = {
+        image = "bbernhard/signal-cli-rest-api:0.41";
+        environment = { USE_NATIVE = "0"; };
+        # TODO: use user variable here
+        volumes = [
+          "/home/jared/.local/share/signal-cli:/home/.local/share/signal-cli"
+        ];
+        ports = [ "8080:8080" ];
+      };
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+}
+
