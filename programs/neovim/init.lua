e521172bb8cf28e92e202e3e064fc5c7070eb4de
Jared Baur <jaredbaur@fastmail.com>
2021-09-22 14:44:06 -0700

    Add function for format on save

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -148,6 +148,25 @@ for _, lsp in ipairs(servers) do
     }
 end
 
+function org_imports(wait_ms)
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction",
+                                            params, wait_ms)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit)
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+    vim.lsp.buf.formatting()
+end
+
+vim.api.nvim_command("au BufWritePre *.go lua org_imports(1000)")
+
 lspconfig.tsserver.setup {
     on_attach = on_attach,
     flags = {debounce_text_changes = 150},
