1de225819ae6cbe59e8aa1ec68d6a4d435c3dcaa
Jared Baur <jaredbaur@fastmail.com>
2021-09-24 19:13:08 -0700

    Remove custom program nix drv

--- programs/gosee.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.buildGoModule {
-  name = "gosee";
-  src = builtins.fetchGit { url = "https://github.com/jmbaur/gosee.git"; };
-  vendorSha256 = "07q9war08k1pqg5hz6pvc1pf1s9k70jgfwp7inxygh9p4k7lwnr1";
-  runVend = true;
-}
