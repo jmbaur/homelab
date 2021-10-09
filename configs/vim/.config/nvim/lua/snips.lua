ee3dbd166335104efa056dd3fb21a54aa2a4b2bb
Jared Baur <jaredbaur@fastmail.com>
2021-10-08 17:35:55 -0700

    Add new snippets

--- configs/vim/.config/nvim/lua/snips.lua
+++ configs/vim/.config/nvim/lua/snips.lua
@@ -3,5 +3,11 @@ require"snippets".snippets = {
         todo = "TODO(jared): ",
         date = function() return os.date() end,
         time = function() return tostring(os.time()) end
+    },
+    go = {ife = [[if err != nil { $1 }]]},
+    typescript = {
+        log = [[console.log("${1}", $1)]],
+        info = [[console.info("${1}", $1)]],
+        error = [[console.error("${1}", $1)]]
     }
 }
