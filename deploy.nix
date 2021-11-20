a8761f5a3b52b7ef26ec68dacee51c9a10a175bf
Jared Baur <jaredbaur@fastmail.com>
2021-11-19 17:53:45 -0800

    Change nixops deployment file...
    ...until dns records are made for other hosts.

--- deploy.nix
+++ deploy.nix
@@ -1,15 +1,15 @@
 {
   network.description = "homelab";
   broccoli = { config, pkgs, ... }: {
-    deployment.targetHost = "broccoli.home.arpa";
+    deployment.targetHost = "192.168.101.1";
     imports = [ ./lib/nixops.nix ./hosts/broccoli/configuration.nix ];
   };
-  rhubarb = { config, pkgs, ... }: {
-    deployment.targetHost = "rhubarb.home.arpa";
-    imports = [ ./lib/nixops.nix ./hosts/rhubarb/configuration.nix ];
-  };
-  asparagus = { config, pkgs, ... }: {
-    deployment.targetHost = "asparagus.home.arpa";
-    imports = [ ./lib/nixops.nix ./hosts/asparagus/configuration.nix ];
-  };
+  # rhubarb = { config, pkgs, ... }: {
+  #   deployment.targetHost = "rhubarb.home.arpa";
+  #   imports = [ ./lib/nixops.nix ./hosts/rhubarb/configuration.nix ];
+  # };
+  # asparagus = { config, pkgs, ... }: {
+  #   deployment.targetHost = "asparagus.home.arpa";
+  #   imports = [ ./lib/nixops.nix ./hosts/asparagus/configuration.nix ];
+  # };
 }
