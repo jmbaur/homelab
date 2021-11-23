72e006088adc19a00d92afa19ceb5d105d41342e
Jared Baur <jaredbaur@fastmail.com>
2021-11-23 12:20:00 -0800

    Add garbage collection by default on remote hosts

--- lib/nixops.nix
+++ lib/nixops.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 with lib;
 {
+  nix.gc.automatic = mkDefault true;
   services.openssh.enable = mkDefault true;
   users.extraUsers.root.openssh.authorizedKeys.keys =
     [ "${builtins.readFile ./yubikeySshKey.txt}" ];
