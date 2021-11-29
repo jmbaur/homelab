f897c2d7250d975f7251079bf29f54ea3d60225f
Jared Baur <jaredbaur@fastmail.com>
2021-11-29 10:15:36 -0800

    Add flake utils

--- flake.nix
+++ flake.nix
@@ -2,61 +2,80 @@
   description = "NixOS configurations for homelab";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-21.05";
   };
 
-  outputs = { self, ... }@inputs: rec {
-
-    nixosConfigurations.beetroot = with inputs.nixos-hardware.nixosModules; inputs.nixpkgs-unstable.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        common-pc-ssd
-        common-cpu-amd
-        common-gpu-amd
-        common-pc-laptop-acpi_call
-        lenovo-thinkpad
-        ./hosts/beetroot/configuration.nix
-        { nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ]; }
-      ];
-    };
-
-    nixopsConfigurations.default = with inputs.nixos-hardware.nixosModules; {
-      nixpkgs = inputs.nixpkgs;
-      network = {
-        description = "homelab";
-        enableRollback = true;
-        storage.legacy.databasefile = "~/.nixops/deployments.nixops";
-      };
-      broccoli = { config, pkgs, ... }: {
-        deployment.targetHost = "broccoli.home.arpa.";
-        imports = [
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , nixos-hardware
+    , nixpkgs-unstable
+    , neovim-nightly-overlay
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: {
+      nixosConfigurations.beetroot = with nixos-hardware.nixosModules; nixpkgs-unstable.lib.nixosSystem {
+        inherit system;
+        modules = [
           common-pc-ssd
-          common-cpu-intel
-          ./lib/nixops.nix
-          ./hosts/broccoli/configuration.nix
+          common-cpu-amd
+          common-gpu-amd
+          common-pc-laptop-acpi_call
+          lenovo-thinkpad
+          ./hosts/beetroot/configuration.nix
+          { nixpkgs.overlays = [ neovim-nightly-overlay.overlay ]; }
         ];
       };
-      rhubarb = { config, pkgs, ... }: {
-        deployment.targetHost = "rhubarb.home.arpa.";
-        imports = [
-          raspberry-pi-4
-          ./lib/nixops.nix
-          ./hosts/rhubarb/configuration.nix
-        ];
-      };
-      asparagus = { config, pkgs, ... }: {
-        deployment.targetHost = "asparagus.home.arpa.";
-        imports = [
-          common-pc-ssd
-          common-cpu-intel
-          ./lib/nixops.nix
-          ./hosts/asparagus/configuration.nix
-        ];
-      };
-    };
 
-  };
+      nixopsConfigurations.default = with nixos-hardware.nixosModules; {
+        inherit nixpkgs;
+        network = {
+          description = "homelab";
+          enableRollback = true;
+          storage.legacy.databasefile = "~/.nixops/deployments.nixops";
+        };
+        broccoli = { config, pkgs, ... }: {
+          deployment.targetHost = "broccoli.home.arpa.";
+          imports = [
+            common-pc-ssd
+            common-cpu-intel
+            ./lib/nixops.nix
+            ./hosts/broccoli/configuration.nix
+          ];
+        };
+        rhubarb = { config, pkgs, ... }: {
+          deployment.targetHost = "rhubarb.home.arpa.";
+          imports = [
+            raspberry-pi-4
+            ./lib/nixops.nix
+            ./hosts/rhubarb/configuration.nix
+          ];
+        };
+        asparagus = { config, pkgs, ... }: {
+          deployment.targetHost = "asparagus.home.arpa.";
+          imports = [
+            common-pc-ssd
+            common-cpu-intel
+            ./lib/nixops.nix
+            ./hosts/asparagus/configuration.nix
+          ];
+        };
+      };
+
+      devShell =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            gnumake
+          ];
+        };
+    });
+
 }
