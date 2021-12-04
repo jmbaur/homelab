d0d0dad778913aeee71ef395857f3f34bc2428b8
Jared Baur <jaredbaur@fastmail.com>
2021-12-04 14:58:23 -0800

    Treesitter highlighting unstable

--- pkgs/neovim/lua/treesitter.lua
+++ pkgs/neovim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {}, -- ensure_installed must not be set!
-    highlight = {enable = true, additional_vim_regex_highlighting = true},
+    highlight = {enable = false, additional_vim_regex_highlighting = false},
     incremental_selection = {
         enable = true,
         keymaps = {
