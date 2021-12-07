0fcf5e3c12394fe4be47683a98ec83b9cf843363
Jared Baur <jaredbaur@fastmail.com>
2021-12-06 21:08:31 -0800

    Don't use icons

--- pkgs/neovim/lua/troublings.lua
+++ pkgs/neovim/lua/troublings.lua
@@ -1,7 +1,8 @@
-require'trouble'.setup{
+require'trouble'.setup {
     fold_open = "v", -- icon used for open folds
     fold_closed = ">", -- icon used for closed folds
     indent_lines = false, -- add an indent guide below the fold icons
+    icons = false,
     signs = {
         -- icons / text used for a diagnostic
         error = "error",
