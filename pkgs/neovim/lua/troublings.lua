7d231459e7572f0cb5d7f1eee06afade088258d3
Jared Baur <jaredbaur@fastmail.com>
2021-12-06 21:12:05 -0800

    Add trouble.nvim keybindings

--- pkgs/neovim/lua/troublings.lua
+++ pkgs/neovim/lua/troublings.lua
@@ -12,3 +12,18 @@ require'trouble'.setup {
     },
     use_lsp_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 }
+
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+                        {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xw",
+                        "<cmd>Trouble lsp_workspace_diagnostics<cr>",
+                        {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xd",
+                        "<cmd>Trouble lsp_document_diagnostics<cr>",
+                        {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+                        {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+                        {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+                        {silent = true, noremap = true})
