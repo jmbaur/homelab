9ab68708580e07a9d6a27e4b4a5df657c60cf2dc
Jared Baur <jaredbaur@fastmail.com>
2021-12-21 14:46:32 -0800

    Use TroubleToggle instead of just Trouble

--- config/neovim/settings/lua/troublings.lua
+++ config/neovim/settings/lua/troublings.lua
@@ -14,10 +14,10 @@ require'trouble'.setup {
 }
 
 vim.api.nvim_set_keymap("n", "<leader>w",
-                        "<cmd>Trouble workspace_diagnostics<cr>",
+                        "<cmd>TroubleToggle workspace_diagnostics<cr>",
                         {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>d",
-                        "<cmd>Trouble document_diagnostics<cr>",
+                        "<cmd>TroubleToggle document_diagnostics<cr>",
                         {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+vim.api.nvim_set_keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
                         {silent = true, noremap = true})
