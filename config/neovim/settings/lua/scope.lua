0fa33f68a70121b7755ef41425116ce399140b32
Jared Baur <jaredbaur@fastmail.com>
2021-12-21 15:17:33 -0800

    Use custom find_files

--- config/neovim/settings/lua/scope.lua
+++ config/neovim/settings/lua/scope.lua
@@ -1,12 +1,14 @@
-require"telescope".setup {}
+require"telescope".setup {
+    defaults = {layout_config = {vertical = {height = 1.0}}}
+}
 
 local function nnoremap(key, cmd)
-    vim.api.nvim_set_keymap("n", key, cmd .. "<cr>",
+    vim.api.nvim_set_keymap("n", key, '<cmd>' .. cmd .. "<cr>",
                             {noremap = true, silent = true})
 end
 
 nnoremap("<leader>f",
-         "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files")
-nnoremap("<leader>g", "<cmd>Telescope live_grep")
-nnoremap("<leader>b", "<cmd>Telescope buffers")
-nnoremap("<leader>h", "<cmd>Telescope help_tags")
+         "lua require('telescope.builtin').find_files({find_command={'rg','--ignore','--files'},layout_strategy='vertical',previewer=false,layout_config={height=0.95,width=0.95}})")
+nnoremap("<leader>g", "Telescope live_grep")
+nnoremap("<leader>b", "Telescope buffers")
+nnoremap("<leader>h", "Telescope help_tags")
