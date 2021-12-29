b772d9f009b0965ce3aedcc986a242d3a6f854a8
Jared Baur <jaredbaur@fastmail.com>
2021-12-29 15:17:55 -0800

    Move almost all deployments to deploy-rs
    Except broccoli, which requires secrets management.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS configurations for homelab";
+  description = "NixOS configurations for the homelab";
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
@@ -17,67 +17,101 @@
     (system:
       let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
       rec {
-        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-          src = builtins.path { path = ./.; };
-          hooks.nixpkgs-fmt.enable = true;
-        };
+        # checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        #   src = builtins.path { path = ./.; };
+        #   hooks.nixpkgs-fmt.enable = true;
+        # };
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [ git gnumake nixopsUnstable ] ++
             pkgs.lib.singleton inputs.deploy-rs.defaultPackage.${system};
-          inherit (checks.pre-commit-check) shellHook;
+          # inherit (checks.pre-commit-check) shellHook;
         };
         packages.p = pkgs.callPackage ./pkgs/p.nix { };
-      }) // {
-    nixopsConfigurations.default = with inputs.nixos-hardware.nixosModules; {
-      nixpkgs = inputs.nixpkgs;
-      network = {
-        description = "homelab";
-        enableRollback = true;
-        storage.legacy = { };
+      }) // rec {
+
+    # recommended by deploy-rs
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks deploy) inputs.deploy-rs.lib;
+
+    nixosConfigurations.asparagus = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = with inputs.nixos-hardware.nixosModules; [
+        common-pc-ssd
+        common-cpu-intel
+        ./lib/nixops.nix
+        ./hosts/asparagus/configuration.nix
+      ];
+    };
+
+    deploy.nodes.asparagus = {
+      hostname = "asparagus.home.arpa.";
+      profiles.system = {
+        user = "root";
+        sshUser = "deploy";
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos nixosConfigurations.asparagus;
       };
+    };
+
+    nixosConfigurations.rhubarb =
+      let system = "aarch64-linux"; in
+      inputs.nixpkgs.lib.nixosSystem {
+        modules = with inputs.nixos-hardware.nixosModules;[
+          ({ ... }: {
+            nixpkgs.overlays = [ inputs.promtop.overlay.${system} ];
+            nixpkgs.localSystem = {
+              inherit system;
+              config = "aarch64-unknown-linux-gnu";
+            };
+          })
+          raspberry-pi-4
+          ./lib/nixops.nix
+          ./hosts/rhubarb/configuration.nix
+        ];
+      };
+
+    deploy.nodes.rhubarb = {
+      hostname = "rhubarb.home.arpa.";
+      profiles.system = {
+        user = "root";
+        sshUser = "deploy";
+        path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos nixosConfigurations.rhubarb;
+      };
+    };
+
+    nixosConfigurations.spinach = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = with inputs.nixos-hardware.nixosModules; [
+        # common-pc-ssd # TODO(jared): enable this?
+        common-cpu-intel
+        ./lib/nixops.nix
+        ./lib/supermicro.nix
+        ./hosts/spinach/configuration.nix
+      ];
+    };
+
+    deploy.nodes.spinach = {
+      hostname = "spinach.home.arpa.";
+      profiles.system = {
+        user = "root";
+        sshUser = "deploy";
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos nixosConfigurations.spinach;
+      };
+    };
+
+    nixopsConfigurations.default = {
+      nixpkgs = inputs.nixpkgs;
+      network = { description = "homelab"; storage.memory = { }; };
       broccoli = { ... }: {
-        deployment.targetHost = "broccoli.home.arpa.";
-        imports = [
+        deployment = {
+          targetHost = "broccoli.home.arpa.";
+          targetUser = "deploy";
+        };
+        nix.trustedUsers = [ "deploy" ];
+        imports = with inputs.nixos-hardware.nixosModules; [
           common-pc-ssd
           common-cpu-intel
           ./lib/nixops.nix
           ./lib/supermicro.nix
           ./hosts/broccoli/configuration.nix
-        ];
-      };
-      rhubarb = { ... }:
-        let system = "aarch64-linux"; in
-        {
-          deployment.targetHost = "rhubarb.home.arpa.";
-          nixpkgs.overlays = [ inputs.promtop.overlay.${system} ];
-          # Allows for nixops to build for this system;
-          nixpkgs.localSystem = {
-            inherit system;
-            config = "aarch64-unknown-linux-gnu";
-          };
-          imports = [
-            raspberry-pi-4
-            ./lib/nixops.nix
-            ./hosts/rhubarb/configuration.nix
-          ];
-        };
-      asparagus = { ... }: {
-        deployment.targetHost = "asparagus.home.arpa.";
-        imports = [
-          common-pc-ssd
-          common-cpu-intel
-          ./lib/nixops.nix
-          ./hosts/asparagus/configuration.nix
-        ];
-      };
-      spinach = { ... }: {
-        deployment.targetHost = "spinach.home.arpa.";
-        imports = [
-          # common-pc-ssd # TODO(jared): enable this?
-          common-cpu-intel
-          ./lib/nixops.nix
-          ./lib/supermicro.nix
-          ./hosts/spinach/configuration.nix
         ];
       };
     };
--- lib/nixops.nix
+++ lib/nixops.nix
@@ -1,12 +1,16 @@
-{ config, lib, ... }:
-with lib;
-{
-  imports = [ ./common.nix ];
-  security.sudo.enable = mkDefault false;
-  services.openssh.enable = mkDefault true;
-  users.extraUsers.root.openssh.authorizedKeys.keys = mkIf config.services.openssh.enable (
-    builtins.filter
+{ config, lib, ... }: with lib; {
+  imports = [ ./common.nix ]; # TODO(jared): delete me
+  security.sudo = {
+    enable = mkForce true;
+    wheelNeedsPassword = mkForce false;
+  };
+  services.openssh.enable = mkForce true;
+  users.users.deploy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "helloworld";
+    openssh.authorizedKeys.keys = builtins.filter
       (str: builtins.stringLength str != 0)
-      (lib.splitString "\n" (builtins.readFile ./ssh_keys.txt))
-  );
+      (lib.splitString "\n" (builtins.readFile ./ssh_keys.txt));
+  };
 }
