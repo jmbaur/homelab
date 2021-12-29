03f705f7e246a006dcae3b688462a7ed590117c5
Jared Baur <jaredbaur@fastmail.com>
2021-12-29 07:44:39 +0000

    Whitespace

--- flake.nix
+++ flake.nix
@@ -29,63 +29,62 @@
         };
         packages.p = pkgs.callPackage ./pkgs/p.nix { };
       }) // {
-    nixopsConfigurations.default = with inputs.nixos-hardware.nixosModules;
-      {
-        nixpkgs = inputs.nixpkgs-stable-small;
-        network = {
-          description = "homelab";
-          enableRollback = true;
-          storage.legacy = { };
-        };
-        broccoli = { ... }: {
-          deployment.targetHost = "broccoli.home.arpa.";
-          imports = [
-            common-pc-ssd
-            common-cpu-intel
-            ./lib/nixops.nix
-            ./lib/supermicro.nix
-            ./hosts/broccoli/configuration.nix
-          ];
-        };
-        rhubarb = { ... }:
-          let system = "aarch64-linux"; in
-          {
-            deployment.targetHost = "rhubarb.home.arpa.";
-            nixpkgs.overlays = [
-              (self: super: {
-                promtop = inputs.promtop.defaultPackage.${system};
-              })
-            ];
-            # Allows for nixops to build for this system;
-            nixpkgs.localSystem = {
-              inherit system;
-              config = "aarch64-unknown-linux-gnu";
-            };
-            imports = [
-              raspberry-pi-4
-              ./lib/nixops.nix
-              ./hosts/rhubarb/configuration.nix
-            ];
-          };
-        asparagus = { ... }: {
-          deployment.targetHost = "asparagus.home.arpa.";
-          imports = [
-            common-pc-ssd
-            common-cpu-intel
-            ./lib/nixops.nix
-            ./hosts/asparagus/configuration.nix
-          ];
-        };
-        spinach = { ... }: {
-          deployment.targetHost = "spinach.home.arpa.";
-          imports = [
-            # common-pc-ssd # TODO(jared): enable this?
-            common-cpu-intel
-            ./lib/nixops.nix
-            ./lib/supermicro.nix
-            ./hosts/spinach/configuration.nix
-          ];
-        };
+    nixopsConfigurations.default = with inputs.nixos-hardware.nixosModules; {
+      nixpkgs = inputs.nixpkgs-stable-small;
+      network = {
+        description = "homelab";
+        enableRollback = true;
+        storage.legacy = { };
       };
+      broccoli = { ... }: {
+        deployment.targetHost = "broccoli.home.arpa.";
+        imports = [
+          common-pc-ssd
+          common-cpu-intel
+          ./lib/nixops.nix
+          ./lib/supermicro.nix
+          ./hosts/broccoli/configuration.nix
+        ];
+      };
+      rhubarb = { ... }:
+        let system = "aarch64-linux"; in
+        {
+          deployment.targetHost = "rhubarb.home.arpa.";
+          nixpkgs.overlays = [
+            (self: super: {
+              promtop = inputs.promtop.defaultPackage.${system};
+            })
+          ];
+          # Allows for nixops to build for this system;
+          nixpkgs.localSystem = {
+            inherit system;
+            config = "aarch64-unknown-linux-gnu";
+          };
+          imports = [
+            raspberry-pi-4
+            ./lib/nixops.nix
+            ./hosts/rhubarb/configuration.nix
+          ];
+        };
+      asparagus = { ... }: {
+        deployment.targetHost = "asparagus.home.arpa.";
+        imports = [
+          common-pc-ssd
+          common-cpu-intel
+          ./lib/nixops.nix
+          ./hosts/asparagus/configuration.nix
+        ];
+      };
+      spinach = { ... }: {
+        deployment.targetHost = "spinach.home.arpa.";
+        imports = [
+          # common-pc-ssd # TODO(jared): enable this?
+          common-cpu-intel
+          ./lib/nixops.nix
+          ./lib/supermicro.nix
+          ./hosts/spinach/configuration.nix
+        ];
+      };
+    };
   };
 }
