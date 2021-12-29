a5a484debaccaa580ffbee92085b0ee050a840e0
Jared Baur <jaredbaur@fastmail.com>
2021-12-28 19:55:35 -0800

    Remove unnused host

--- hosts/kale/deploy.nix
+++
@@ -1,7 +0,0 @@
-{
-  network.description = "kale";
-  kale = { config, pkgs, ... }: {
-    deployment.targetHost = "kale.lan";
-    imports = [ ./secrets.nix ../../lib/nixops.nix ./configuration.nix ];
-  };
-}
