56112ddc671cf7a7484ddd7d0d77b1e4d3deb2cd
Jared Baur <jaredbaur@fastmail.com>
2022-01-09 23:16:22 -0800

    Format configuration.nix correctly

--- installer.nix
+++ installer.nix
@@ -9,22 +9,23 @@ writeShellScriptBin "install" ''
   sudo swapon /dev/sda2
   sudo nixos-generate-config --root /mnt
   cat | sudo tee /mnt/etc/nixos/configuration.nix << EOF
-    { config, pkgs, ... }: {
-      imports = [ ./hardware-configuration.nix ];
-      boot.loader.grub.enable = true;
-      boot.loader.grub.version = 2;
-      boot.loader.grub.device = "/dev/sda";
-      networking.useDHCP = false;
-      networking.interfaces.ens18.useDHCP = true;
-      users.users.root.openssh.authorizedKeys.keyFiles = [
-        (builtins.fetchurl {
-          url = "https://github.com/jmbaur.keys";
-          sha256 = "1w3f101ri4rf0d98zf4zcdc5i0nv29mcz39p558l5r38p2s7nbrm";
-        })
-      ];
-      services.openssh.enable = true;
-      services.qemuGuest.enable = true;
-    }
+{ config, pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  networking.useDHCP = false;
+  networking.interfaces.ens18.useDHCP = true;
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    (builtins.fetchurl {
+      url = "https://github.com/jmbaur.keys";
+      sha256 = "1w3f101ri4rf0d98zf4zcdc5i0nv29mcz39p558l5r38p2s7nbrm";
+    })
+  ];
+  environment.systemPackages = with pkgs; [ vim ];
+  services.openssh.enable = true;
+  services.qemuGuest.enable = true;
+}
 EOF
   sudo nixos-install --no-root-passwd
   sudo reboot
