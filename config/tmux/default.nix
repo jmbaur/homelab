27244b2d0ac4580d4cce15a06e2585787a1ecf83
Jared Baur <jaredbaur@fastmail.com>
2022-01-10 19:46:39 -0800

    Add other dotfiles to ansible

--- config/tmux/default.nix
+++ config/tmux/default.nix
@@ -8,39 +8,13 @@ in
 {
   options = {
     custom.tmux = {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Enable custom tmux settings.
-        '';
-      };
+      enable = mkEnableOption "Custom tmux settings";
     };
   };
 
   config = mkIf cfg.enable {
-    programs.tmux = {
-      enable = true;
-      terminal = "screen-256color";
-      secureSocket = true;
-      keyMode = "vi";
-      clock24 = true;
-      baseIndex = 1;
-      aggressiveResize = true;
-      escapeTime = 10;
-      extraConfig = ''
-        unbind C-b
-        set -g prefix C-s
-        bind C-s send-prefix
-        set -g renumber-windows on
-        set -g set-clipboard on
-        set -g default-command "''${SHELL}"
-        set -g status-left-length 50
-        set -g status-right "%H:%M %d-%b-%y"
-        set-option -g focus-events on
-        set-option -sa terminal-overrides ',xterm-256color:RGB'
-      '';
-    };
+    environment.etc."tmux.conf".source = ../../playbooks/files/tmux.conf;
+    environment.systemPackages = [ pkgs.tmux ];
   };
 
 }
