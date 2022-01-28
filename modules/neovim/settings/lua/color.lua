b22580b09a9a8412ad6c9c78dcdf3dd238384d34
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 23:49:50 -0800

    Use transparent background

--- modules/neovim/settings/lua/color.lua
+++ modules/neovim/settings/lua/color.lua
@@ -1,5 +1,5 @@
 local monokai = require 'monokai'
-monokai.pro["base0"] = "#000000"
-monokai.pro["base1"] = "#000000"
-monokai.pro["base2"] = "#000000"
+monokai.pro["base0"] = "NONE"
+monokai.pro["base1"] = "NONE"
+monokai.pro["base2"] = "NONE"
 monokai.setup {palette = monokai.pro}
