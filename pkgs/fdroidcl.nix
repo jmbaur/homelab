5cdb68d8d38446e882b18ffc68063775859b9ad5
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 20:24:39 -0800

    Change fdroidcl derivation

--- pkgs/fdroidcl.nix
+++ pkgs/fdroidcl.nix
@@ -7,6 +7,5 @@ buildGoModule {
     rev = "1bd0f39050540dec815e93c7e4b10c4f8a52ba89";
     sha256 = "13f2yqysbj5scd7swznjhzsf8jalfnzdv1p3ipzaqzz2snc6vbhc";
   };
-  vendorSha256 = "11q0gy3wfjaqyfj015yw3wfz2j1bsq6gchjhjs6fxfjmb77ikwjb";
-  runVend = true;
+  vendorSha256 = "sha256-uy+pfFX1GFRKYk/u1ALiQRzkv7vvqho0x+ahucLKE4U=";
 }
