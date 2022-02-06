ebd739a200f5fb7b2c0590a3e21159ec9322c3c7
Jared Baur <jaredbaur@fastmail.com>
2022-02-05 19:42:57 -0800

    Add nvim-treesitter-textobjects

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -60,6 +60,7 @@ in
               neogit
               nvim-autopairs
               nvim-lspconfig
+              nvim-treesitter-textobjects
               snippets-nvim
               telescope-nvim
               toggleterm-nvim
--- modules/neovim/settings/lua/sitter.lua
+++ modules/neovim/settings/lua/sitter.lua
@@ -1,5 +1,19 @@
 require'nvim-treesitter.configs'.setup {
     highlight = {enable = true},
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
+            }
+        }
+    },
     incremental_selection = {
         enable = true,
         keymaps = {
