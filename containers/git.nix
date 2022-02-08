47bf67a41ae04f8dfa492627ee61c903eb625020
Jared Baur <jaredbaur@fastmail.com>
2022-02-08 13:05:30 -0800

    Enable ssh on git container

--- containers/git.nix
+++ containers/git.nix
@@ -10,4 +10,7 @@
   };
   networking.firewall.allowedTCPPorts = [ 80 ];
   networking.interfaces.mv-trusted.useDHCP = true;
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = false;
+  users.users.git.openssh.authorizedKeys.keyFiles = lib.singleton (import ../data/jmbaur-ssh-keys.nix);
 }
