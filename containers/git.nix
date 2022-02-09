291a5122787305a5da74ca5677aafa948bade61e
Jared Baur <jaredbaur@fastmail.com>
2022-02-08 23:33:03 -0800

    Try nginx

--- containers/git.nix
+++ containers/git.nix
@@ -1,32 +1,53 @@
 { config, lib, pkgs, ... }:
+let
+  cgitrc = pkgs.writeText "cgitrc" ''
+    source-filter=''${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
+    about-filter=''${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
+    cache-size=1000
+    scan-path=${config.services.gitDaemon.basePath}
+  '';
+in
 {
+  networking = {
+    firewall.allowedTCPPorts = [ 80 ];
+    interfaces.mv-trusted.useDHCP = true;
+  };
+  users.users = {
+    # "${config.services.fcgiwrap.user}".extraGroups = [ config.services.gitDaemon.group ];
+    git = {
+      home = config.services.gitDaemon.basePath;
+      createHome = true;
+      shell = "${pkgs.git}/bin/git-shell";
+      openssh.authorizedKeys.keyFiles = lib.singleton (import ../data/jmbaur-ssh-keys.nix);
+    };
+  };
   services.gitDaemon = {
     enable = true;
     exportAll = true;
     basePath = "/srv/git";
   };
-  services.lighttpd = {
+  services.fcgiwrap = {
     enable = true;
-    cgit.enable = true;
-    cgit.configText = ''
-      about-filter=''${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
-      cache-size=1000
-      logo-link=/
-      remove-suffix=1
-      scan-path=${config.services.gitDaemon.basePath}
-      source-filter=''${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
-      # virtual-root=${pkgs.cgit}/cgit
-    '';
   };
-  networking.firewall.allowedTCPPorts = [ 80 ];
-  networking.interfaces.mv-trusted.useDHCP = true;
-  services.openssh.enable = true;
-  services.openssh.passwordAuthentication = false;
-  users.users.git = {
-    home = config.services.gitDaemon.basePath;
-    createHome = true;
-    shell = "${pkgs.git}/bin/git-shell";
-    openssh.authorizedKeys.keyFiles = lib.singleton (import ../data/jmbaur-ssh-keys.nix);
+  services.nginx = {
+    enable = true;
+    virtualHosts.localhost = {
+      locations."/" = {
+        extraConfig = ''
+          include ${pkgs.nginx}/conf/fastcgi_params;
+          fastcgi_param CGIT_CONFIG ${cgitrc};
+          fastcgi_param SCRIPT_FILENAME ${pkgs.cgit}/cgit/cgit.cgi;
+          fastcgi_split_path_info ^(/?)(.+)$;
+          fastcgi_param PATH_INFO $fastcgi_path_info;
+          fastcgi_param QUERY_STRING $args;
+          fastcgi_param HTTP_HOST $server_name;
+          fastcgi_pass unix:${config.services.fcgiwrap.socketAddress};
+        '';
+      };
+    };
   };
-  users.users.lighttpd.extraGroups = [ config.services.gitDaemon.group ];
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+  };
 }
