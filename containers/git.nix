5edcf7a9498b40ec70aee5508b2d4d2e6e30dcd8
Jared Baur <jaredbaur@fastmail.com>
2022-02-08 20:50:10 -0800

    Expand caddy usage

--- containers/git.nix
+++ containers/git.nix
@@ -4,22 +4,26 @@
     exportAll = true;
     basePath = "/srv/git";
   };
+  environment.etc."cgitrc".text = ''
+    source-filter=''${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
+    about-filter=''${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
+    cache-size=1000
+    scan-path=${config.services.gitDaemon.basePath}
+  '';
   services.caddy = {
     enable = true;
+    config = ''
+      git.example.com {
+        # other settings such as TLS, headers, ...
+        root ${pkgs.cgit}/cgit
+        cgi {
+          match /
+          exec  ${pkgs.cgit}/cgit/cgit.cgi
+          except /cgit.png /favicon.ico /cgit.css /robots.txt
+        }
+      }
+    '';
   };
-  # services.lighttpd = {
-  #   enable = true;
-  #   cgit = {
-  #     enable = true;
-  #     subdir = "";
-  #     configText = ''
-  #       source-filter=${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py
-  #       about-filter=${pkgs.cgit}/lib/cgit/filters/about-formatting.sh
-  #       cache-size=1000
-  #       scan-path=${config.services.gitDaemon.basePath}
-  #     '';
-  #   };
-  # };
   networking.firewall.allowedTCPPorts = [ 80 ];
   networking.interfaces.mv-trusted.useDHCP = true;
   services.openssh.enable = true;
