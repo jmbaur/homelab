3848976abffdf4d0cbcbe4f3212bbb8083e36b17
Jared Baur <jaredbaur@fastmail.com>
2022-02-11 21:52:20 -0800

    Add nginx container

--- containers/git.nix
+++ containers/git.nix
@@ -12,7 +12,6 @@ in
 {
   networking.firewall.allowedTCPPorts = [ 80 ];
   users.users = {
-    # "${config.services.fcgiwrap.user}".extraGroups = [ config.services.gitDaemon.group ];
     git = {
       home = config.services.gitDaemon.basePath;
       createHome = true;
@@ -25,43 +24,12 @@ in
     exportAll = true;
     basePath = "/srv/git";
   };
-  services.fcgiwrap. enable = true;
-  services.nginx = {
+  services.fcgiwrap = {
     enable = true;
-    virtualHosts."_" =
-      let
-        index = pkgs.writeText "index.html" ''
-          <h1>These aren’t the droids you’re looking for.</h1>
-        '';
-      in
-      {
-        default = true;
-        locations."/" = {
-          extraConfig = ''
-            index ${index}
-          '';
-        };
-      };
-    virtualHosts."git.jmbaur.com" = {
-      locations."~* ^.+(cgit.(css|png)|favicon.ico|robots.txt)" = {
-        extraConfig = ''
-          root ${pkgs.cgit}/cgit;
-          expires 30d;
-        '';
-      };
-      locations."/" = {
-        extraConfig = ''
-          include ${pkgs.nginx}/conf/fastcgi_params;
-          fastcgi_param CGIT_CONFIG ${cgitrc};
-          fastcgi_param SCRIPT_FILENAME ${pkgs.cgit}/cgit/cgit.cgi;
-          fastcgi_split_path_info ^(/?)(.+)$;
-          fastcgi_param PATH_INFO $fastcgi_path_info;
-          fastcgi_param QUERY_STRING $args;
-          fastcgi_param HTTP_HOST $server_name;
-          fastcgi_pass unix:${config.services.fcgiwrap.socketAddress};
-        '';
-      };
-    };
+    socketType = "tcp";
+    socketAddress = "0.0.0.0:5678";
+    user = services.gitDaemon.user;
+    group = services.gitDaemon.group;
   };
   services.openssh = {
     enable = true;
---
+++ containers/nginx.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }: {
+  # TODO(jared): don't open 80
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  services.nginx = {
+    enable = true;
+    virtualHosts."_" =
+      let
+        index = pkgs.writeText "index.html" ''
+          <h1>These aren’t the droids you’re looking for.</h1>
+        '';
+      in
+      {
+        default = true;
+        locations."/" = {
+          extraConfig = ''
+            index ${index};
+          '';
+        };
+      };
+    virtualHosts."git.jmbaur.com" = {
+      locations."~* ^.+(cgit.(css|png)|favicon.ico|robots.txt)" = {
+        extraConfig = ''
+          root ${pkgs.cgit}/cgit;
+          expires 30d;
+        '';
+      };
+      locations."/" = {
+        extraConfig = ''
+          include ${pkgs.nginx}/conf/fastcgi_params;
+          fastcgi_param CGIT_CONFIG ${cgitrc};
+          fastcgi_param SCRIPT_FILENAME ${pkgs.cgit}/cgit/cgit.cgi;
+          fastcgi_split_path_info ^(/?)(.+)$;
+          fastcgi_param PATH_INFO $fastcgi_path_info;
+          fastcgi_param QUERY_STRING $args;
+          fastcgi_param HTTP_HOST $server_name;
+          fastcgi_pass 192.168.10.21:5678;
+        '';
+      };
+    };
+  };
+}
