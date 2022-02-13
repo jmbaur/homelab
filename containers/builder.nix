754e9e14e72b88564a4f2b5c785943baab33195d
Jared Baur <jaredbaur@fastmail.com>
2022-02-12 18:23:44 -0800

    Set isNormalUser

--- containers/builder.nix
+++ containers/builder.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }: {
   users.users.jared = {
+    isNormalUser = true;
     openssh.authorizedKeys.keyFiles =
       lib.singleton (import ./data/jmbaur-ssh-keys.nix);
   };
