82f15da99a4cb054fb6ce9fec1b82587ad2bc575
Jared Baur <jaredbaur@fastmail.com>
2022-02-25 19:11:51 -0800

    Open firewall for torrent services

--- containers/torrent.nix
+++ containers/torrent.nix
@@ -4,14 +4,18 @@
   };
   services.sonarr = {
     enable = true;
+    openFirewall = true;
   };
   services.radarr = {
     enable = true;
+    openFirewall = true;
   };
   services.lidarr = {
     enable = true;
+    openFirewall = true;
   };
   services.jackett = {
     enable = true;
+    openFirewall = true;
   };
 }
