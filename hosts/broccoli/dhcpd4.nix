b1ccf41bcce876fdf57071ce63a0ed8ebf236eea
Jared Baur <jaredbaur@fastmail.com>
2022-03-11 07:50:08 -0800

    Setup netboot

--- hosts/broccoli/dhcpd4.nix
+++ hosts/broccoli/dhcpd4.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }: {
+  services.atftpd.enable = true;
+  systemd.activationScripts.netboot.text = ''
+    ln -sfT ${pkgs.netbootxyz-efi} ${config.services.atftpd.root}/netboot.xyz.efi
+  '';
   services.dhcpd4 = {
     enable = true;
     interfaces = [ "trusted" "iot" "guest" "mgmt" ];
@@ -25,7 +29,16 @@
         option broadcast-address 192.168.30.255;
         option subnet-mask 255.255.255.0;
         option domain-name-servers 192.168.30.1;
-        filename "https://boot.netboot.xyz/ipxe/netboot.xyz.efi";
+        class "pxeclients"{
+          match if substring (option vendor-class-identifier, 0, 9) = "PXEClient";
+          next-server 192.168.30.1;
+          filename "netboot.xyz.efi";
+        }
+        class "httpclients" {
+          match if substring (option vendor-class-identifier, 0, 10) = "HTTPClient";
+          option vendor-class-identifier "HTTPClient";
+          filename "https://boot.netboot.xyz/ipxe/netboot.xyz.efi";
+        }
       }
 
       subnet 192.168.40.0 netmask 255.255.255.0 {
