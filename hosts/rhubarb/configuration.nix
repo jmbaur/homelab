02eeff1b02b65b0e9c4ea2fe315f4d57e9392c8f
Jared Baur <jaredbaur@fastmail.com>
2022-03-11 19:47:34 -0800

    Setup distributed builds with rhubarb

--- hosts/rhubarb/configuration.nix
+++ hosts/rhubarb/configuration.nix
@@ -24,4 +24,29 @@
     ansible
     deploy-rs
   ];
+
+  nix.buildMachines = [{
+    hostName = "kale";
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    maxJobs = 1;
+    speedFactor = 2;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+    mandatoryFeatures = [ ];
+  }];
+  nix.distributedBuilds = true;
+  # Speeds things up by downloading dependencies remotely:
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
+
+  programs.ssh.knownHosts = {
+    broccoli = {
+      hostNames = [ "broccoli" ];
+      publicKey = "broccoli ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5CvQypTDJ1jl+6/xBw7DLITOCzIwZRZIAefI3+uV6M";
+    };
+    kale = {
+      hostNames = [ "kale" ];
+      publicKey = "kale ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSDTqHc9WfeZxTL97QzcmNAGUP/Qt2J5h3q1OqOvIen";
+    };
+  };
 }
