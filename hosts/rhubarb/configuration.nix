e099db579528d16cdb4f493813c005ef6ea2d058
Jared Baur <jaredbaur@fastmail.com>
2022-03-11 19:06:25 -0800

    Use DHCP for rhubarb

--- hosts/rhubarb/configuration.nix
+++ hosts/rhubarb/configuration.nix
@@ -1,5 +1,4 @@
 { config, lib, pkgs, ... }:
-with lib;
 {
   custom.common.enable = true;
   custom.deploy.enable = true;
@@ -17,20 +16,6 @@ with lib;
 
   networking = {
     hostName = "rhubarb";
-    domain = "home.arpa";
-    nameservers = singleton "192.168.88.1";
-    defaultGateway.address = "192.168.88.1";
-    defaultGateway.interface = "eth0";
-    interfaces.eth0 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.88.88";
-        prefixLength = 24;
-      }];
-      ipv6.addresses = [{
-        address = "fd82:f21d:118d:58::58";
-        prefixLength = 64;
-      }];
-    };
+    interfaces.eth0.useDHCP = true;
   };
 }
