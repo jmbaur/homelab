478b105164d4464cf2c2abfc61531d1877d55f91
Jared Baur <jaredbaur@fastmail.com>
2022-03-12 21:02:14 -0800

    Use new known hosts option

--- hosts/rhubarb/configuration.nix
+++ hosts/rhubarb/configuration.nix
@@ -48,20 +48,14 @@
   };
 
   programs.ssh = {
-    knownHosts = {
-      localhost = {
-        hostNames = [ "localhost" ];
-        publicKey = lib.last (import ../../data/rhubarb-ssh-keys.nix);
-      };
-      broccoli = {
-        hostNames = [ "broccoli" ];
-        publicKey = "broccoli ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5CvQypTDJ1jl+6/xBw7DLITOCzIwZRZIAefI3+uV6M";
-      };
-      kale = {
-        hostNames = [ "kale" ];
-        publicKey = "kale ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSDTqHc9WfeZxTL97QzcmNAGUP/Qt2J5h3q1OqOvIen";
-      };
-    };
+    knownHostsFiles = [
+      (pkgs.writeText "known_hosts" ''
+        kale ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSDTqHc9WfeZxTL97QzcmNAGUP/Qt2J5h3q1OqOvIen
+        kale ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiamAf2qQSBbkyZIc9BJkrcbXg8wcqbBABe6SocaldjJUTslc5zaZfWPbeBwVDYrUv7My09U1QhKBeVx2oFlSTqjT4iKIxcGslgOnLJhtL0AATUC1r6KkyyCWV1F46YRtrb6eXvEhsxxAtQJDWu0ooRUHY/mfK/UAtJ1Kc3WfDZ8m1RfX21nENb9dmvwCUQdfQvg7jmHf7KEXTpySrg3ixcD4/Ns160u2FrlvaSYwkOlQygw+u80VVFflXC1xB3XkAhnR8I4IUsp9kegGt4A+9yYY3ADoH3UC4b1jWRMxNCcHsnuEzVN578TqsRNXDZA7TlhSp0Ube6YqzDNeBhlwQdCPFJFsIPLdDHhcF7CMKlOL1rjyxa0Tyff5oWL0XulA+D2R3t90e/rFutyeXy6TAKntx+HIl7d4DGInVFw76bKN+z7PhT+RrTObRgDIfafHw6kp8drXvRWN8Jr9mLUh3XV5r3qIUxVN9x0ONBdBV+6HJN//YieYLVCxxo5SDHgYwKHv5/86QxD5aXVT3UTL6np5fe2AdebZLlCkdtb4wxmxFNelZeh0k39dsu1u5gT19q/YvxuoQhGl21OWBZGF3mo1iNsamKMrISi873I7Xt5/xlPc5Idpr50RCdndF52TK4gHAU7U+gCMhBu8LCmu/A3gCqrX1+LX3BXQdqeAtdw==
+        broccoli ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5CvQypTDJ1jl+6/xBw7DLITOCzIwZRZIAefI3+uV6M
+        broccoli ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCQpy9S183AAnOzgGDQpCScnfdokhM6TSKcD+A//pIaNcPUgiKoRaTdphc5Fv9RVgHPxz8+mJlGYGgIqMWLufV3EJaKLeWZo0AiivWnZtmTtT8kZNPqXa6O10/3rfwfWtFThBDc2uKnLzE5LBO4pvzbk34kyemSmgwRDIBbJfNgWI/1LUpzIKGPHTGUjBRp39ktK2LFNcYtAF9ofwiHY0e/sQDWug7P4FahOaATH5UOxpSl9PbyUHrvM5G7LIJZ1uTWFqNIb0rMuGfDkyQs8+1tbgE8CgLN3wD2sTFE/6LypLoLpAA3R61HmRwwlYJlcPaxpK2YBjAqx+58JEmnkss3EqZG003FJf8C7E+HlB+2W1kBDgIFe/K0yUxm3VNvMvEwN53Cryj85be8Rm9mnAbPIJgKibFPGZLqmu3GrTJcraS2BK3oW9MF//4y8j4lFLAzlOyhLGuEa5/2R8OhPj8Y9Rq3KMJ+cbYb1m2eMcrZe5ziq56pe1lKDINzGGrway5ycYu9CGPnZeIYW/USTYfnu/E7Cl6MP0Pwza8Z3xtL652pN8Ksd3GU16SwZAJp/1QL6iBAgKV6QqX7NqfRL3SiqS8ajsgps3QGgw6vHnfvFYeiCGcyaaMI9qACJSsuor/t8vnMMIjlQ+ULXC+0bNlTdtFZSYjZdKnEc1Ivj1dJ8Q==
+      '')
+    ];
     extraConfig = ''
       Host kale rhubarb asparagus broccoli
         User root
@@ -69,4 +63,5 @@
         IdentityFile /etc/ssh/ssh_host_ed25519_key
     '';
   };
+};
 }
