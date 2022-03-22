f695b6b7f40ee1ef84154dc562bc631fcbe41d6d
Jared Baur <jaredbaur@fastmail.com>
2022-03-22 15:15:06 -0700

    Use different settings for i3status on desktop vs laptop

--- homes/jared/desktop.nix
+++ homes/jared/desktop.nix
@@ -7,6 +7,43 @@ in
   config = lib.mkIf cfg.enable {
     programs.i3status = {
       enable = true;
+      enableDefault = false;
+      general = {
+        colors = true;
+        interval = 5;
+      };
+      modules = {
+        ipv6 = {
+          position = 1;
+        };
+        "ethernet _first_" = {
+          position = 2;
+          settings = {
+            format_down = "E: down";
+            format_up = "E: %ip (%speed)";
+          };
+        };
+        "disk /" = {
+          position = 3;
+          settings.format = "%avail";
+        };
+        load = {
+          position = 4;
+          settings.format = "%1min";
+        };
+        memory = {
+          position = 5;
+          settings = {
+            format = "%used | %available";
+            format_degraded = "MEMORY < %available";
+            threshold_degraded = "1G";
+          };
+        };
+        "tztime local" = {
+          position = 6;
+          settings.format = "%Y-%m-%d %H:%M:%S";
+        };
+      };
     };
   };
 }
