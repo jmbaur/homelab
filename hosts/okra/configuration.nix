0f50d40e1f534a38a31dc08776ab09518909b4ea
Jared Baur <jaredbaur@fastmail.com>
2022-05-05 20:12:01 -0700

    Allow unfree

--- hosts/okra/configuration.nix
+++ hosts/okra/configuration.nix
@@ -5,6 +5,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  nixpkgs.config.allowUnfree = true;
+
   networking.hostName = "okra";
   networking.wireless.enable = true;
 
