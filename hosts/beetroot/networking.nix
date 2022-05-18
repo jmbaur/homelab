655e2dba58e9a84ae28eae1d39a0ec6f72b7d0c4
Jared Baur <jaredbaur@fastmail.com>
2022-05-18 06:20:34 -0700

    Use systemd networkd again

--- hosts/beetroot/networking.nix
+++ hosts/beetroot/networking.nix
@@ -1,12 +1,38 @@
 { config, lib, ... }: {
+  systemd.services.systemd-networkd-wait-online.enable = false;
+  systemd.network = {
+    networks = {
+      wired = {
+        matchConfig.Name = "en*";
+        networkConfig = {
+          DHCP = "yes";
+          IPv6PrivacyExtensions = true;
+        };
+        dhcpV4Config = {
+          RouteMetric = 10;
+          UseDomains = "yes";
+        };
+      };
+
+      wireless = {
+        matchConfig.Name = "wl*";
+        networkConfig = {
+          DHCP = "yes";
+          IPv6PrivacyExtensions = true;
+        };
+        dhcpV4Config = {
+          RouteMetric = 20;
+          UseDomains = "yes";
+        };
+      };
+    };
+  };
+
   networking = {
     useDHCP = lib.mkForce false;
     hostName = "beetroot";
-    # wireless.iwd.enable = true;
-    networkmanager.enable = true;
-    interfaces.enp3s0f0.useDHCP = false;
-    interfaces.enp4s0.useDHCP = false;
-    interfaces.wlan0.useDHCP = false;
+    useNetworkd = true;
+    wireless.iwd.enable = true;
     # wg-quick.interfaces.wg0 = {
     #   autostart = false;
     #   privateKeyFile = config.sops.secrets.wg0.path;
