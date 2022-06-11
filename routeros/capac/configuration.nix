60d0183b4929db5c8eab0b510c62849ef4bec6a9
Jared Baur <jaredbaur@fastmail.com>
2022-06-11 16:46:49 -0700

    Upgrade CapAC to RouterOS 7

--- routeros/capac/configuration.nix
+++ routeros/capac/configuration.nix
@@ -4,16 +4,18 @@ let
     data=$(cat $1)
 
     cat << EOF
-    # jun/11/2022 15:57:38 by RouterOS 6.49.6
+    # jun/11/2022 16:37:55 by RouterOS 7.3.1
     # software id = TD07-GRS2
     #
     # model = RBcAPGi-5acD2nD
     /interface bridge
-    add admin-mac=DC:2C:6E:0D:FE:6F auto-mac=no comment=defconf name=bridge vlan-filtering=yes
+    add admin-mac=DC:2C:6E:0D:FE:6F auto-mac=no comment=defconf ingress-filtering=no name=bridge vlan-filtering=yes
     /interface wireless
     set [ find default-name=wlan1 ] ssid=MikroTik
     /interface vlan
     add interface=bridge name=mgmt vlan-id=88
+    /interface lte apn
+    set [ find default=yes ] ip-type=ipv4 use-network-apn=no
     /interface wireless security-profiles
     set [ find default=yes ] supplicant-identity=MikroTik
     add authentication-types=wpa2-psk mode=dynamic-keys name=iot supplicant-identity=MikroTik wpa2-pre-shared-key=$(${jq}/bin/jq ".iot.password" <<< $data)
@@ -24,19 +26,31 @@ let
         security-profile=trusted ssid=$(${jq}/bin/jq '.trusted.ssid' <<< $data) vlan-id=30 vlan-mode=use-tag wireless-protocol=802.11
     add disabled=no mac-address=DE:2C:6E:0D:FE:71 master-interface=wlan2 name=wlan3 security-profile=iot ssid=$(${jq}/bin/jq '.iot.ssid' <<< $data) vlan-id=40 vlan-mode=use-tag
     add disabled=no mac-address=DE:2C:6E:0D:FE:72 master-interface=wlan2 name=wlan4 security-profile=work ssid=$(${jq}/bin/jq '.work.ssid' <<< $data) vlan-id=50 vlan-mode=use-tag
+    /routing bgp template
+    set default disabled=no output.network=bgp-networks
+    /routing ospf instance
+    add disabled=no name=default-v2
+    /routing ospf area
+    add disabled=yes instance=default-v2 name=backbone-v2
     /interface bridge port
-    add bridge=bridge comment=defconf interface=ether2
-    add bridge=bridge comment=defconf frame-types=admit-only-vlan-tagged ingress-filtering=yes interface=wlan2 pvid=30
-    add bridge=bridge interface=ether1
-    add bridge=bridge frame-types=admit-only-vlan-tagged ingress-filtering=yes interface=wlan3 pvid=40
-    add bridge=bridge frame-types=admit-only-vlan-tagged ingress-filtering=yes interface=wlan4 pvid=50
+    add bridge=bridge comment=defconf ingress-filtering=no interface=ether2
+    add bridge=bridge comment=defconf frame-types=admit-only-vlan-tagged interface=wlan2 pvid=30
+    add bridge=bridge ingress-filtering=no interface=ether1
+    add bridge=bridge frame-types=admit-only-vlan-tagged interface=wlan3 pvid=40
+    add bridge=bridge frame-types=admit-only-vlan-tagged interface=wlan4 pvid=50
     /ip neighbor discovery-settings
     set discover-interface-list=none
+    /ip settings
+    set max-neighbor-entries=8192
+    /ipv6 settings
+    set disable-ipv6=yes max-neighbor-entries=8192
     /interface bridge vlan
     add bridge=bridge tagged=bridge,ether1 vlan-ids=88
     add bridge=bridge tagged=ether1,wlan2 vlan-ids=30
     add bridge=bridge tagged=wlan3,ether1 vlan-ids=40
     add bridge=bridge tagged=ether1,wlan4 vlan-ids=50
+    /interface ovpn-server server
+    set auth=sha1,md5
     /ip address
     add address=192.168.88.4/24 interface=mgmt network=192.168.88.0
     /ip dns
@@ -44,7 +58,7 @@ let
     /ip dns static
     add address=192.168.88.1 comment=defconf name=router.lan
     /ip route
-    add distance=1 gateway=192.168.88.1 pref-src=192.168.88.4
+    add disabled=no dst-address=0.0.0.0/0 gateway=192.168.88.1 pref-src=192.168.88.4
     /ip service
     set telnet disabled=yes
     set ftp disabled=yes
@@ -61,12 +75,12 @@ let
     set enabled=yes on-event=dark-mode
     /system script
     add comment=defconf dont-require-permissions=no name=dark-mode owner=*sys policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive,romon source="\r\
-        \\n   :if ([system leds settings get all-leds-off] = \"never\") do={\r\
-        \\n     /system leds settings set all-leds-off=immediate \r\
-        \\n   } else={\r\
-        \\n     /system leds settings set all-leds-off=never \r\
-        \\n   }\r\
-        \\n "
+        \n   :if ([system leds settings get all-leds-off] = \"never\") do={\r\
+        \n     /system leds settings set all-leds-off=immediate \r\
+        \n   } else={\r\
+        \n     /system leds settings set all-leds-off=never \r\
+        \n   }\r\
+        \n "
     /tool mac-server
     set allowed-interface-list=none
     /tool mac-server mac-winbox
