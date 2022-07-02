13edfed77aadf28e00ad30c49866b7ea57127979
Jared Baur <jaredbaur@fastmail.com>
2022-07-02 16:41:45 -0700

    Set position for kanshi

--- hosts/beetroot/configuration.nix
+++ hosts/beetroot/configuration.nix
@@ -79,13 +79,13 @@
       profiles = {
         default = {
           outputs = [
-            { criteria = "eDP-1"; status = "enable"; }
+            { criteria = "eDP-1"; status = "enable"; position = "0,0"; }
           ];
         };
         docked = {
           outputs = [
             { criteria = "eDP-1"; status = "disable"; }
-            { criteria = "Lenovo Group Limited LEN P24q-20 V306P4GR"; status = "enable"; mode = "2560x1440@74.78Hz"; }
+            { criteria = "Lenovo Group Limited LEN P24q-20 V306P4GR"; status = "enable"; mode = "2560x1440@74.78Hz"; position = "0,0"; }
           ];
         };
       };
