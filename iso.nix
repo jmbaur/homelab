e720cbb5ad0496b91ae3d0530f07c161653d7521
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 15:43:01 -0700

    Use kernel 5.18 for iso

--- iso.nix
+++ iso.nix
@@ -1,4 +1,5 @@
-{ lib, ... }: {
+{ pkgs, lib, ... }: {
+  boot.kernelPackages = pkgs.linuxPackages_5_18;
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
   users.users.nixos.openssh.authorizedKeys.keyFiles =
     [ (import ./data/jmbaur-ssh-keys.nix) ];
