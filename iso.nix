d461b71a28b3e84734b36e5447231b96b19f1b72
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 17:26:42 -0700

    Add quality-of-life settings to ISO

--- iso.nix
+++ iso.nix
@@ -1,6 +1,21 @@
 { pkgs, lib, ... }: {
   boot.kernelPackages = pkgs.linuxPackages_5_18;
+
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
-  users.users.nixos.openssh.authorizedKeys.keyFiles =
-    [ (import ./data/jmbaur-ssh-keys.nix) ];
+
+  users.users.nixos.openssh.authorizedKeys.keyFiles = [
+    (import ./data/jmbaur-ssh-keys.nix)
+  ];
+
+  console.useXkbConfig = true;
+  services.xserver.xkbOptions = "ctrl:nocaps";
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [ curl git tmux vim ];
 }
