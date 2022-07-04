24c73730b1bdd071977fc81e44a176d88e259aaa
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 20:46:24 -0700

    Setup SFP ports in initrd instead

--- modules/lx2k.nix
+++ modules/lx2k.nix
@@ -8,16 +8,19 @@ in
     boot.kernelParams = [ "arm-smmu.disable_bypass=0" "iommu.passthrough=1" ]; # for onboard nics
     boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
-    systemd.services = lib.listToAttrs (map
-      (name: lib.nameValuePair name {
-        description = "Enable SFP port for dpmac %i";
-        path = [ pkgs.restool ];
-        serviceConfig.Type = "oneshot";
-        serviceConfig.ExecStart = "${pkgs.restool}/bin/ls-addni dpmac.%i";
-        before = [ "network-pre.target" ];
-        wants = [ "network-pre.target" ];
-      })
-      (map (i: "dpmac@${toString i}") [ 7 8 9 10 ]));
-
+    # Setup SFP+ network interfaces early so systemd can pick everything up.
+    boot.initrd.extraUtilsCommands = ''
+      copy_bin_and_libs ${pkgs.restool}/bin/restool
+      copy_bin_and_libs ${pkgs.restool}/bin/ls-main
+      copy_bin_and_libs ${pkgs.restool}/bin/ls-addni
+      # Patch paths
+      # sed -i "1i #!$out/bin/sh" $out/bin/ls-main
+    '';
+    boot.initrd.postDeviceCommands = ''
+      ls-addni dpmac.7
+      ls-addni dpmac.8
+      ls-addni dpmac.9
+      ls-addni dpmac.10
+    '';
   };
 }
