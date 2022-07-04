59276db55b8e3d3f28ed152c94087415b819d638
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 22:56:18 -0700

    Add priority to link files

--- hosts/kale2/networking.nix
+++ hosts/kale2/networking.nix
@@ -7,19 +7,19 @@
 
   systemd.network = {
     links = {
-      "sfp-sfpplus1" = {
+      "10-sfp-sfpplus1" = {
         matchConfig.MACAddress = "d0:63:b4:03:db:6d";
         linkConfig.Name = "sfp-sfpplus1";
       };
-      "sfp-sfpplus2" = {
+      "10-sfp-sfpplus2" = {
         matchConfig.MACAddress = "d0:63:b4:03:db:6e";
         linkConfig.Name = "sfp-sfpplus2";
       };
-      "sfp-sfpplus3" = {
+      "10-sfp-sfpplus3" = {
         matchConfig.MACAddress = "d0:63:b4:03:db:6f";
         linkConfig.Name = "sfp-sfpplus3";
       };
-      "sfp-sfpplus4" = {
+      "10-sfp-sfpplus4" = {
         matchConfig.MACAddress = "d0:63:b4:03:db:70";
         linkConfig.Name = "sfp-sfpplus4";
       };
--- modules/lx2k.nix
+++ modules/lx2k.nix
@@ -5,7 +5,11 @@ in
 {
   options.hardware.lx2k.enable = lib.mkEnableOption "hardware support for the Honeycomb LX2K board";
   config = lib.mkIf cfg.enable {
-    boot.kernelParams = [ "arm-smmu.disable_bypass=0" "iommu.passthrough=1" ]; # for onboard nics
+    boot.kernelParams = [
+      "console=ttyAMA0,115200"
+      "arm-smmu.disable_bypass=0"
+      "iommu.passthrough=1"
+    ];
     boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
     # Setup SFP+ network interfaces early so systemd can pick everything up.
