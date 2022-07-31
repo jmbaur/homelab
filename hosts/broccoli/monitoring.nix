262f186e548a5f939821d88520bbd2bfb08a02f8
Jared Baur <jaredbaur@fastmail.com>
2022-07-31 15:29:16 -0700

    Remove old host

--- hosts/broccoli/monitoring.nix
+++
@@ -1,9 +0,0 @@
-{
-  services.prometheus.exporters = {
-    node = {
-      enable = true;
-      enabledCollectors = [ "ethtool" "network_route" "systemd" ];
-    };
-    wireguard.enable = true;
-  };
-}
--- hosts/broccoli/options.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, ... }:
-with lib;
-{
-  options.router = {
-    guaPrefix = mkOption { type = types.str; };
-    ulaPrefix = mkOption { type = types.str; };
-  };
-}
