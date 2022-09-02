5ac3befb7d2d15f3bb348096cb0b733797b469d8
Jared Baur <jaredbaur@fastmail.com>
2022-09-02 01:56:51 +0000

    Disable magic rollback for rhubarb

--- deploy.nix
+++ deploy.nix
@@ -1,7 +1,7 @@
 inputs: with inputs;
 let
-  mkSystemNode = { name, hostname, system }: {
-    inherit hostname;
+  mkSystemNode = { name, hostname, system, magicRollback ? true, autoRollback ? true }: {
+    inherit hostname magicRollback autoRollback;
     profiles.system = {
       sshUser = "root";
       path = deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${name};
@@ -13,7 +13,7 @@ let
     { name = "kale"; value = mkSystemNode { name = "kale"; hostname = "kale.mgmt.home.arpa"; system = "aarch64-linux"; }; }
     { name = "okra"; value = mkSystemNode { name = "okra"; hostname = "okra.mgmt.home.arpa"; system = "x86_64-linux"; }; }
     { name = "potato"; value = mkSystemNode { name = "potato"; hostname = "potato.mgmt.home.arpa"; system = "x86_64-linux"; }; }
-    { name = "rhubarb"; value = mkSystemNode { name = "rhubarb"; hostname = "rhubarb.mgmt.home.arpa"; system = "aarch64-linux"; }; }
+    { name = "rhubarb"; value = mkSystemNode { name = "rhubarb"; hostname = "rhubarb.mgmt.home.arpa"; system = "aarch64-linux"; magicRollback = false; }; }
   ];
 in
 {
