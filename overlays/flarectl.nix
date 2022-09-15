eb6414f48ed67e0d427b2c3ccf7e80c6d8878ca1
Jared Baur <jaredbaur@fastmail.com>
2022-09-14 18:34:58 -0700

    Update deps

--- overlays/flarectl.nix
+++ overlays/flarectl.nix
@@ -1,13 +1,13 @@
 { buildGoModule, fetchFromGitHub, lib, ... }:
 buildGoModule rec {
   pname = "flarectl";
-  version = "0.49.0";
+  version = "0.50.0";
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "cloudflare-go";
     rev = "v${version}";
-    sha256 = "sha256-VkmimyH5NYR1lEoKKf/R7WFhsKn3rc9IWlnnRWFji5g=";
+    sha256 = "00kd55n58ddrmw6y046gz9fym99j0agb8nq7kgxkr3wsiajyz4md";
   };
-  vendorSha256 = "sha256-e3yww1mrXmcY1R1RjYl92/e9zpUkZ2J0EiwzM+hOAQQ=";
+  vendorSha256 = "sha256-0cQ0ZHlA4OVYG6l8G0t4uhdhUCUhOPuIcpTQJw7jO40=";
   subPackages = [ "cmd/flarectl" ];
 }
