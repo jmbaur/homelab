a45c5ec9638a73dc7525d09da4a2f54479fa3caa
Jared Baur <jaredbaur@fastmail.com>
2022-10-09 08:18:09 -0700

    Add missing cname records

--- cloud/cloudflare.nix
+++ cloud/cloudflare.nix
@@ -1,4 +1,4 @@
-{ secrets, ... }: {
+{ config, secrets, ... }: {
   data.http.publicip = {
     url = "https://ipv4.icanhazip.com";
   };
@@ -9,44 +9,58 @@
       zone = "jmbaur.com";
     };
     cloudflare_record = {
+      logsCNAME = {
+        zone_id = "\${cloudflare_zone.jmbaur_com.id}";
+        name = "logs.${config.resource.cloudflare_zone.jmbaur_com.zone}";
+        value = config.resource.cloudflare_zone.jmbaur_com.zone;
+        type = "CNAME";
+        proxied = false;
+      };
+      monCNAME = {
+        zone_id = "\${cloudflare_zone.jmbaur_com.id}";
+        name = "mon.${config.resource.cloudflare_zone.jmbaur_com.zone}";
+        value = config.resource.cloudflare_zone.jmbaur_com.zone;
+        type = "CNAME";
+        proxied = false;
+      };
       wwwCNAME = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "www.jmbaur.com";
-        value = "jmbaur.com";
+        name = "www.${config.resource.cloudflare_zone.jmbaur_com.zone}";
+        value = config.resource.cloudflare_zone.jmbaur_com.zone;
         type = "CNAME";
         proxied = false;
       };
       authCNAME = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "auth.jmbaur.com";
-        value = "jmbaur.com";
+        name = "auth.${config.resource.cloudflare_zone.jmbaur_com.zone}";
+        value = config.resource.cloudflare_zone.jmbaur_com.zone;
         type = "CNAME";
         proxied = false;
       };
       rootAAAA = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "jmbaur.com";
+        name = config.resource.cloudflare_zone.jmbaur_com.zone;
         value = "\${aws_instance.homelab-web.ipv6_addresses[0]}";
         type = "AAAA";
         proxied = false;
       };
       rootA = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "jmbaur.com";
+        name = config.resource.cloudflare_zone.jmbaur_com.zone;
         value = "\${aws_instance.homelab-web.public_ip}";
         type = "A";
         proxied = false;
       };
       vpnA = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "vpn.jmbaur.com";
+        name = "vpn.${config.resource.cloudflare_zone.jmbaur_com.zone}";
         value = "\${chomp(data.http.publicip.response_body)}";
         type = "A";
         proxied = false;
       };
       vpnAAAA = {
         zone_id = "\${cloudflare_zone.jmbaur_com.id}";
-        name = "vpn.jmbaur.com";
+        name = "vpn.${config.resource.cloudflare_zone.jmbaur_com.zone}";
         value = secrets.networking.hurricane.address;
         type = "AAAA";
         proxied = false;
--- cloud/default.nix
+++ cloud/default.nix
@@ -1,8 +1,8 @@
 { ... }: {
   # Versioning of these providers is provided by Nix.
   terraform.required_providers = {
-    aws.source = "hashicorp/aws";
-    cloudflare.source = "cloudflare/cloudflare";
+    aws.source = "registry.terraform.io/hashicorp/aws";
+    cloudflare.source = "registry.terraform.io/cloudflare/cloudflare";
   };
 
   provider = {
