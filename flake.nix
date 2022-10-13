58c03c93e146e7beb5dfb912a8f85f3b2bb8fc7f
Jared Baur <jaredbaur@fastmail.com>
2022-10-13 10:16:23 -0700

    Add build workflow for linux_cn913x

--- flake.nix
+++ flake.nix
@@ -24,10 +24,12 @@
     extra-substituters = [
       "https://microvm.cachix.org"
       "https://nix-community.cachix.org"
+      "https://jmbaur.cachix.org"
     ];
     extra-trusted-public-keys = [
       "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "jmbaur.cachix.org-1:OpzNp/eBFYOmPtZBjnS6QD0FAPgK4ItMRea0QPuyJYM="
     ];
   };
 
