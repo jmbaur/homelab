5f8de887c04ee4a0de8e7679341c2fb57a2bb156
Jared Baur <jaredbaur@fastmail.com>
2022-10-15 23:11:46 -0700

    Up the max jobs for remote builder

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -272,18 +272,11 @@ inputs: with inputs; {
             (lib.optional config.custom.remoteBuilders.aarch64builder.enable {
               hostName = "aarch64builder";
               system = "aarch64-linux";
-              # if the builder supports building for multiple architectures,
-              # replace the previous line by, e.g.,
-              # systems = ["x86_64-linux" "aarch64-linux"];
-              maxJobs = 1;
+              maxJobs = 8;
               speedFactor = 2;
               supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
               mandatoryFeatures = [ ];
-            })
-            # ++
-            # ()
-          ;
-
+            });
           nix.distributedBuilds = true;
           # optional, useful when the builder has a faster internet connection than yours
           nix.extraOptions = ''
