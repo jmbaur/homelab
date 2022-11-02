f5316beaf1262faf744dc510ef68609f55ec3af9
Jared Baur <jaredbaur@fastmail.com>
2022-11-02 12:33:55 -0700

    Update nixos-configs

--- devShells.nix
+++ devShells.nix
@@ -1,34 +1,32 @@
 inputs: with inputs; nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ] (system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [
-        nixos-configs.overlays.default
-        self.overlays.default
-      ];
-    };
-  in
-  {
-    ci = pkgs.mkShell {
-      buildInputs = with pkgs; [ just jq ];
-    };
-    default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        (terraform.withPlugins (p: with p; [ aws cloudflare http sops ]))
-        awscli2
-        deploy-rs
-        flarectl
-        just
-        pam_u2f
-        sops
-        yubikey-manager
-      ];
-      inherit (pre-commit.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          nixpkgs-fmt = { enable = true; excludes = [ "hardware-configuration\\.nix" ]; };
-          deadnix = { enable = true; excludes = [ "hardware-configuration\\.nix" ]; };
-        };
-      }) shellHook;
-    };
-  })
+let
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+      nixos-configs.overlays.default
+      self.overlays.default
+    ];
+  };
+in
+{
+  ci = pkgs.mkShell { buildInputs = with pkgs; [ just jq ]; };
+  default = pkgs.mkShell {
+    buildInputs = with pkgs; [
+      (terraform.withPlugins (p: with p; [ aws cloudflare http sops ]))
+      awscli2
+      deploy-rs
+      flarectl
+      just
+      pam_u2f
+      sops
+      yubikey-manager
+    ];
+    inherit (pre-commit.lib.${system}.run {
+      src = ./.;
+      hooks = {
+        nixpkgs-fmt = { enable = true; excludes = [ "hardware-configuration\\.nix" ]; };
+        deadnix = { enable = true; excludes = [ "hardware-configuration\\.nix" ]; };
+      };
+    }) shellHook;
+  };
+})
