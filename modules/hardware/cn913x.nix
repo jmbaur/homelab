33a8863375021f5f3cb1a6dfbe4acd6787439670
Jared Baur <jaredbaur@fastmail.com>
2022-11-06 12:12:47 -0800

    Kernel param console already set by sd image

--- modules/hardware/cn913x.nix
+++ modules/hardware/cn913x.nix
@@ -1,7 +1,7 @@
 { pkgs, modulesPath, ... }: {
   imports = [ "${modulesPath}/installer/sd-card/sd-image-aarch64.nix" ];
   boot.initrd.systemd.enable = true;
-  boot.kernelParams = [ "console=ttyS0,115200" "cma=256M" ];
+  boot.kernelParams = [ "cma=256M" ];
   boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_cn913x;
   hardware.deviceTree = {
     enable = true;
