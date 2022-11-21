f74cbe963f8f8cc0f8602f5fe48f8d72d75caa33
Jared Baur <jaredbaur@fastmail.com>
2022-11-20 16:28:58 -0800

    Enable dark mode for wayland variants

--- overlays/mkWaylandVariant.nix
+++ overlays/mkWaylandVariant.nix
@@ -12,25 +12,32 @@ let
   );
 in
 drv:
-if lib.hasAttr "commandLineArgs" (lib.functionArgs drv.override) then
+if lib.hasAttr "commandLineArgs" (lib.functionArgs drv.override)
+then
   (drv.override {
     commandLineArgs = [
       "--enable-features=${enableFeatures}"
       "--ozone-platform-hint=auto"
+      "--force-dark-mode"
+      "--enable-features=WebUIDarkMode"
     ];
   })
 else
   let
-    mainProgram = if lib.hasAttr "mainProgram" drv.meta then drv.meta.mainProgram else drv.pname;
+    mainProgram =
+      if lib.hasAttr "mainProgram" drv.meta
+      then drv.meta.mainProgram
+      else drv.pname;
   in
-  symlinkJoin rec {
+  symlinkJoin {
     name = "${mainProgram}-wayland";
     paths = [ drv ];
     buildInputs = [ makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/${mainProgram} \
       --add-flags "--enable-features=${enableFeatures}" \
-      --add-flags "--ozone-platform-hint=auto"
+      --add-flags "--ozone-platform-hint=auto" \
+      --add-flags "--force-dark-mode" \
+      --add-flags "--enable-features=WebUIDarkMode"
     '';
   }
-
