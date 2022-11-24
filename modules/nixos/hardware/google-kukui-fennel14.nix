b5b882ffc009b91efc261bec9d7990d776582fac
Jared Baur <jaredbaur@fastmail.com>
2022-11-24 12:06:48 -0800

    Start using extraStructuredConfig

--- modules/nixos/hardware/google-kukui-fennel14.nix
+++ modules/nixos/hardware/google-kukui-fennel14.nix
@@ -45,39 +45,44 @@ with lib;
             };
             kernelPatches = [ ];
 
-            extraConfig = lib.replaceStrings
-              [ "CONFIG_" "=" ] [ "" " " ]
-              ''
-                CONFIG_ATH10K=m
-                CONFIG_ATH10K_DEBUG=y
-                CONFIG_ATH10K_DEBUGFS=y
-                CONFIG_SND_SOC=y
-                CONFIG_SND_SOC_MT8173=y
-                CONFIG_SND_SOC_MT8173_RT5650=y
-                CONFIG_SND_SOC_MT8173_RT5650_RT5514=y
-                CONFIG_SND_SOC_MT8173_RT5650_RT5676=y
-                CONFIG_SND_SOC_MT8183=y
-                CONFIG_SND_SOC_MT8183_DA7219_MAX98357A=y
-                CONFIG_SND_SOC_MT8183_MT6358_TS3A227E_MAX98357A=y
-                CONFIG_SND_SOC_MT8186=y
-                CONFIG_SND_SOC_MT8186_MT6366_DA7219_MAX98357=y
-                CONFIG_SND_SOC_MT8186_MT6366_RT1019_RT5682S=y
-                CONFIG_SND_SOC_MT8192=y
-                CONFIG_SND_SOC_MT8192_MT6359_RT1015_RT5682=y
-                CONFIG_SND_SOC_SOF_MT8186=m
-                CONFIG_SND_SOC_SOF_MTK_TOPLEVEL=y
-                CONFIG_SND_SOC_SOF_OF=y
-                CONFIG_SND_SOC_SOF_TOPLEVEL=y
-                CONFIG_VIDEO_DW9768=m
-                CONFIG_VIDEO_MEDIATEK_JPEG=m
-                CONFIG_VIDEO_MEDIATEK_MDP=m
-                CONFIG_VIDEO_MEDIATEK_MDP3=m
-                CONFIG_VIDEO_MEDIATEK_VCODEC=m
-                CONFIG_VIDEO_MEDIATEK_VPU=y
-                CONFIG_VIDEO_OV02A10=m
-                CONFIG_VIDEO_OV5695=m
-                CONFIG_VIDEO_OV8856=m
-              '';
+            extraStructuredConfig = with lib.kernel; {
+              wireless = {
+                ATH10K = module;
+                ATH10K_DEBUG = yes;
+                ATH10K_DEBUGFS = yes;
+              };
+              sound = {
+                SND_SOC_MT8173 = yes;
+                SND_SOC_MT8173_RT5650 = yes;
+                SND_SOC_MT8173_RT5650_RT5514 = yes;
+                SND_SOC_MT8173_RT5650_RT5676 = yes;
+                SND_SOC_MT8183 = yes;
+                SND_SOC_MT8183_DA7219_MAX98357A = yes;
+                SND_SOC_MT8183_MT6358_TS3A227E_MAX98357A = yes;
+                SND_SOC_MT8186 = yes;
+                SND_SOC_MT8186_MT6366_DA7219_MAX98357 = yes;
+                SND_SOC_MT8186_MT6366_RT1019_RT5682S = yes;
+                SND_SOC_MT8192 = yes;
+                SND_SOC_MT8192_MT6359_RT1015_RT5682 = yes;
+                SND_SOC_SOF_MT8186 = module;
+                SND_SOC_SOF_MTK_TOPLEVEL = yes;
+                SND_SOC_SOF_OF = yes;
+                SND_SOC_SOF_TOPLEVEL = yes;
+              };
+              video = {
+                DRM = yes;
+                DRM_DP_AUX_CHARDEV = yes;
+                DRM_EVDI = module;
+                DRM_UDL = yes;
+                DRM_VGEM = yes;
+              };
+              misc = {
+                CROS_EC = yes;
+                CROS_EC_PD_UPDATE = yes;
+                CROS_EC_SENSORHUB = module;
+                CROS_EC_SPI = yes;
+              };
+            };
 
             extraMeta.branch = "chromeos-5.15";
           } // (args.argsOverride or { }));
