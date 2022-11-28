11cdcbce05d5c9191f60c58d49dcf541f9107a9b
Jared Baur <jaredbaur@fastmail.com>
2022-11-27 22:26:50 -0800

    Better colors for foot

--- modules/nixos/home-manager/gui.nix
+++ modules/nixos/home-manager/gui.nix
@@ -41,12 +41,27 @@ with lib; {
           font = "JetBrains Mono:size=10";
           selection-target = "clipboard";
         };
+        mouse.hide-when-typing = "yes";
         colors = {
-          foreground = "ffffff";
+          alpha = 1.0;
           background = "000000";
-        };
-        mouse = {
-          hide-when-typing = "yes";
+          foreground = "ffffff";
+          regular0 = "000000";
+          regular1 = "ff8059";
+          regular2 = "44bc44";
+          regular3 = "d0bc00";
+          regular4 = "2fafff";
+          regular5 = "feacd0";
+          regular6 = "00d3d0";
+          regular7 = "bfbfbf";
+          bright0 = "595959";
+          bright1 = "ef8b50";
+          bright2 = "70b900";
+          bright3 = "c0c530";
+          bright4 = "79a8ff";
+          bright5 = "b6a0ff";
+          bright6 = "6ae4b9";
+          bright7 = "ffffff";
         };
       };
     };
