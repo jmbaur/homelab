e83aec8d0f4739616dbeb15879c8c6cc3803c555
Jared Baur <jaredbaur@fastmail.com>
2022-11-27 18:58:18 -0800

    Add comments to Kconfig settings

--- overlays/kernels/linux_chromiumos_mediatek.nix
+++ overlays/kernels/linux_chromiumos_mediatek.nix
@@ -1,11 +1,15 @@
 { linuxKernel, lib, ... }:
 linuxKernel.kernels.linux_6_0.override {
   structuredExtraConfig = with lib.kernel; {
+
+    # Wireless
     ATH10K = module;
     ATH10K_DEBUG = yes;
     ATH10K_DEBUGFS = yes;
     ATH10K_SDIO = module;
     ATH10K_TRACING = yes;
+
+    # Sound
     SND = yes;
     SND_ALOOP = module;
     SND_DUMMY = module;
@@ -31,5 +35,80 @@ linuxKernel.kernels.linux_6_0.override {
     SND_SOC_SOF_OF = yes;
     SND_SOC_SOF_TOPLEVEL = yes;
     SND_USB_AUDIO = module;
+
+    # TPM
+    # TCG_TIS_I2C_CR50 = yes;
+    # TCG_TIS_I2C_INFINEON = yes;
+    # TCG_TIS_SPI = yes;
+    # TCG_TIS_SPI_CR50 = yes;
+    # TCG_TPM = yes;
+
+    # Mediatek settings
+    # MTK_ADSP_IPC=yes;
+    # MTK_ADSP_MBOX=yes;
+    # MTK_CMDQ=yes;
+    # MTK_EFUSE=yes;
+    # MTK_IOMMU=yes;
+    # MTK_PMIC_WRAP=yes;
+    # MTK_SOC_THERMAL_LVTS=yes;
+    # MTK_SVS=yes;
+    # MTK_THERMAL=yes;
+
+    # Video
+    # DRM=yes;
+    # DRM_ANALOGIX_ANX7625=yes;
+    # DRM_ANALOGIX_ANX78XX=yes;
+    # DRM_DP_AUX_CHARDEV=yes;
+    # DRM_EVDI=module;
+    # DRM_ITE_IT6505=yes;
+    # DRM_MEDIATEK=yes;
+    # DRM_MEDIATEK_HDMI=yes;
+    # DRM_PANEL_BOE_TV101WUM_NL6=yes;
+    # DRM_PANEL_EDP=yes;
+    # DRM_PANEL_INNOLUX_HIMAX8279D=yes;
+    # DRM_PANEL_INNOLUX_P079ZCA=yes;
+    # DRM_PANEL_SIMPLE=yes;
+    # DRM_PANFROST=yes;
+    # DRM_PARADE_PS8640=yes;
+    # DRM_POWERVR_ROGUE_1_17=yes;
+    # DRM_UDL=yes;
+    # DRM_VGEM=yes;
+    # MALI_BIFROST=yes;
+    # MALI_BIFROST_EXPERT=yes;
+    # MALI_BIFROST_PLATFORM_NAME=freeform "mediatek"
+    # VIDEO_DW9768=module;
+    # VIDEO_MEDIATEK_JPEG=module;
+    # VIDEO_MEDIATEK_MDP3=module;
+    # VIDEO_MEDIATEK_MDP=module;
+    # VIDEO_MEDIATEK_VCODEC=module;
+    # VIDEO_MEDIATEK_VPU=yes;
+    # VIDEO_OV02A10=module;
+    # VIDEO_OV5695=module;
+    # VIDEO_OV8856=module;
+
+    # Embedded controller
+    # CROS_EC=yes;
+    # CROS_EC_PD_UPDATE=yes;
+    # CROS_EC_RPMSG=module;
+    # CROS_EC_SENSORHUB=module;
+    # CROS_EC_SPI=yes;
+    # EXTCON_USBC_CROS_EC=yes;
+    # I2C_CROS_EC_TUNNEL=yes;
+    # IIO=yes;
+    # IIO_CROS_EC_ACTIVITY=module;
+    # IIO_CROS_EC_BARO=module;
+    # IIO_CROS_EC_LIGHT_PROX=module;
+    # IIO_CROS_EC_SENSORS=module;
+    # IIO_CROS_EC_SENSORS_CORE=module;
+    # IIO_CROS_EC_SENSORS_LID_ANGLE=module;
+    # IIO_CROS_EC_SENSORS_SYNC=module;
+    # IIO_HRTIMER_TRIGGER=module;
+    # IIO_SW_TRIGGER=module;
+    # IIO_SYSFS_TRIGGER=module;
+    # KEYBOARD_CROS_EC=yes;
+    # PWM=yes;
+    # PWM_CROS_EC=yes;
+    # RTC_DRV_CROS_EC=yes;
+
   };
 }
