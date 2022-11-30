069604307d191966860c8a8f5bd4fd77e5bdb7bd
Jared Baur <jaredbaur@fastmail.com>
2022-11-29 18:38:15 -0800

    Enable console for cn913x

--- modules/nixos/hardware/cn913x.nix
+++ modules/nixos/hardware/cn913x.nix
@@ -6,7 +6,7 @@ with lib;
   };
   config = mkIf config.hardware.clearfog-cn913x.enable {
     boot.initrd.systemd.enable = true;
-    boot.kernelParams = [ "cma=256M" ];
+    boot.kernelParams = [ "console=ttyS0,115200" "cma=256M" ];
     boot.kernelPackages = pkgs.linuxKernel.packagesFor pkgs.linux_cn913x;
 
     hardware.deviceTree = {
