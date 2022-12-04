5c5f030dbed5984dd166f2307edfd29db0296354
Jared Baur <jaredbaur@fastmail.com>
2022-12-03 21:14:14 -0800

    Fix networks assertion

--- nixosModules/router/options.nix
+++ nixosModules/router/options.nix
@@ -114,7 +114,7 @@ in
       message = "Cannot have physical.enable and wireguard.enable set for the same network";
       assertion = (lib.filterAttrs
         (_: network: network.physical.enable && network.wireguard.enable)
-        config.custom.inventory.networks) != { };
+        config.custom.inventory.networks) == { };
     }];
 
     environment.etc."inventory.json".source = (pkgs.formats.json { }).generate
