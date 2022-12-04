a6527fa9424806a7c4847ff274f9f0c76d7354be
Jared Baur <jaredbaur@fastmail.com>
2022-12-03 20:01:12 -0800

    Don't rely on router being in hosts

--- nixosModules/router/wireguard.nix
+++ nixosModules/router/wireguard.nix
@@ -2,13 +2,16 @@
 let
   mkWgInterface = network:
     let
-      wgServerHost = network.hosts.${config.networking.hostName};
+      routerPublicKey = network.wireguard.publicKey;
+      routerIPv4 = "${network.networkIPv4SignificantBits}.1";
+      routerIPv6Gua = "${network.networkGuaPrefix}::1";
+      routerIPv6Ula = "${network.networkUlaPrefix}::1";
       peers = lib.filterAttrs (_: host: host.wgPeer) network.hosts;
       port = 51800 + network.id;
     in
     {
       netdev = {
-        netdevConfig = { Name = wgServerHost.interface; Kind = "wireguard"; };
+        netdevConfig = { Name = network.name; Kind = "wireguard"; };
         wireguardConfig = {
           ListenPort = port;
           PrivateKeyFile = config.sops.secrets."wg/${lib.replaceStrings ["wg-"] [""] network.name}/${config.networking.hostName}".path;
@@ -26,12 +29,12 @@ let
           })
           peers;
       };
-      network = with wgServerHost; {
+      network = {
         inherit (network) name;
         address = [
-          "${ipv4}/${toString network.ipv4Cidr}"
-          "${ipv6.gua}/${toString network.ipv6Cidr}"
-          "${ipv6.ula}/${toString network.ipv6Cidr}"
+          "${routerIPv4}/${toString network.ipv4Cidr}"
+          "${routerIPv6Gua}/${toString network.ipv6Cidr}"
+          "${routerIPv6Ula}/${toString network.ipv6Cidr}"
         ];
       };
       clientConfigs = lib.mapAttrsToList
@@ -46,10 +49,10 @@ let
                   "${host.ipv6.ula}/${toString network.ipv6Cidr}"
                 ];
                 PrivateKey = "$(cat ${config.sops.secrets."wg/${lib.replaceStrings ["wg-"] [""] network.name}/${hostname}".path})";
-                DNS = (with wgServerHost; ([ ipv4 ipv6.gua ipv6.ula ])) ++ [ "home.arpa" ];
+                DNS = (([ routerIPv4 routerIPv6Ula ])) ++ [ "home.arpa" ];
               };
               Peer = {
-                PublicKey = wgServerHost.publicKey;
+                PublicKey = routerPublicKey;
                 Endpoint = "vpn.jmbaur.com:${toString port}";
                 AllowedIPs = [
                   network.networkIPv4Cidr
@@ -71,10 +74,10 @@ let
                   "${host.ipv6.ula}/${toString network.ipv6Cidr}"
                 ];
                 PrivateKey = "$(cat ${config.sops.secrets."wg/${lib.replaceStrings ["wg-"] [""] network.name}/${hostname}".path})";
-                DNS = (with wgServerHost; ([ ipv4 ipv6.gua ipv6.ula ])) ++ [ "home.arpa" ];
+                DNS = (([ routerIPv4 routerIPv6Ula ])) ++ [ "home.arpa" ];
               };
               Peer = {
-                PublicKey = wgServerHost.publicKey;
+                PublicKey = routerPublicKey;
                 Endpoint = "vpn.jmbaur.com:${toString port}";
                 AllowedIPs = [ "0.0.0.0/0" "::/0" ];
               };
