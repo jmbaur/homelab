8cb2f716ef18a4b47d1c7f2d1a6afe68e5c125ca
Jared Baur <jaredbaur@fastmail.com>
2022-12-04 22:56:56 -0800

    More bootstrapping

--- nixosConfigurations/fennel/bootstrap.bash
+++ nixosConfigurations/fennel/bootstrap.bash
@@ -4,6 +4,7 @@ disk="/dev/mmcblk0"
 # kernel1="${disk}p1"
 # kernel2="${disk}p2"
 root="${disk}p3"
+cryptroot="/dev/mapper/cryptroot"
 
 blkdiscard --force "$disk"
 
@@ -16,5 +17,18 @@ parted "$disk" -- mkpart primary 129MiB 100%  # root partition
 
 cgpt add -t kernel -i 1 -S 1 -T 5 -P 10 "$disk"
 
-mount "$root" /mnt
+cryptsetup luksFormat "$root"
+cryptsetup luksOpen "$root" cryptroot
+systemd-cryptenroll --fido2-device=auto "$root"
+
+mkfs.btrfs "$cryptroot"
+mount "$cryptroot" /mnt
+btrfs subvolume create /mnt/{@,@nix,@home}
+umount /mnt
+
+# TODO(jared): verify if mmc devices can do trimming
+mount -o X-mount.mkdir,subvol=@,noatime,discard=async,compress=zstd "$cryptroot" /mnt
+mount -o X-mount.mkdir,subvol=@nix,noatime,discard=async,compress=zstd "$cryptroot" /mnt/nix
+mount -o X-mount.mkdir,subvol=@home,noatime,discard=async,compress=zstd "$cryptroot" /mnt/home
+
 nixos-install --flake github:jmbaur/homelab#fennel --root /mnt --no-root-passwd
