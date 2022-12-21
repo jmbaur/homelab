2d822cd275a75fd4af72435580e8eb6f16e1ba79
Jared Baur <jaredbaur@fastmail.com>
2022-12-21 09:28:41 -0800

    Add u-root patches

--- overlays/u-root.nix
+++ overlays/u-root.nix
@@ -1,4 +1,4 @@
-{ buildPackages, buildGoPackage, fetchFromGitHub, ... }:
+{ buildGoPackage, fetchFromGitHub, buildPackages, writeText, ... }:
 let
   pname = "u-root";
   version = "2022-12-18";
@@ -14,18 +14,25 @@ let
     inherit pname version src goPackagePath;
     subPackages = ".";
   };
+  elvishrc = writeText "rc.elv" ''
+    use readline-binding
+  '';
 in
 buildGoPackage {
   pname = "${pname}-initramfs";
   inherit version src goPackagePath;
+  patches = [
+    ./u-root-extlinux-path.patch # allows for booting extlinux on nixos /boot/extlinux/extlinux.conf
+    ./u-root-elvish-etc-rc.patch # read elvish rc.elv from /etc/rc.elv
+  ];
   buildPhase = ''
+    mkdir -p $out
     GOROOT="$(go env GOROOT)" ${builder}/bin/u-root \
       -uroot-source go/src/$goPackagePath \
-      -uinitcmd=systemboot \
-      core ./cmds/boot/{systemboot,localboot,fbnetboot}
+      -files "${elvishrc}:etc/rc.elv" \
+      -o $out/initramfs.cpio \
+      -uinitcmd=boot \
+      core ./cmds/boot/{boot,systemboot,localboot,fbnetboot}
   '';
-  installPhase = ''
-    mkdir -p $out
-    cp /tmp/initramfs.*.cpio $out/initramfs.cpio
-  '';
+  dontInstall = true;
 }
