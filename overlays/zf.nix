8f783eaa0ae5bc00a4b56679c18419aca3eebd91
Jared Baur <45740526+jmbaur@users.noreply.github.com>
2022-12-24 19:20:54 -0800

    Apply suggestions from code review

--- overlays/zf.nix
+++ overlays/zf.nix
@@ -1,12 +1,10 @@
 { stdenvNoCC, fetchFromGitHub, zig }:
 stdenvNoCC.mkDerivation rec {
   pname = "zf";
-  version = "0.6.0"; # 0.6.0 and up require zig 0.10.0
   src = fetchFromGitHub {
     owner = "natecraddock";
     repo = "zf";
     rev = version;
-    sha256 = "sha256-zmlNvChcRXYCo6BtjSG+G1Re9hPZVFW1EKyj4/CIdmg=";
   };
   preBuild = ''
     export HOME=$TMPDIR
