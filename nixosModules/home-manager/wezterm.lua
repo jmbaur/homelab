b167a2f2f68267990a617f896194150f5b2ea945
Jared Baur <jaredbaur@fastmail.com>
2023-01-09 13:14:52 -0800

    Use same terminal colorscheme definition for all terminals

--- nixosModules/home-manager/wezterm.lua
+++ nixosModules/home-manager/wezterm.lua
@@ -35,6 +35,7 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 	audible_bell = "Disabled",
 	automatically_reload_config = false,
+	color_scheme = "modus-operandi",
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 16.0,
 	force_reverse_video_cursor = true,
@@ -42,12 +43,4 @@ return {
 	mouse_bindings = mouse_bindings,
 	use_fancy_tab_bar = false,
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-	colors = {
-		foreground = "#ffffff",
-		background = "#000000",
-		selection_bg = "#fafad2",
-		selection_fg = "#000000",
-		ansi = { "#000000", "#ff8059", "#44bc44", "#d0bc00", "#2fafff", "#feacd0", "#00d3d0", "#bfbfbf" },
-		brights = { "#595959", "#ef8b50", "#70b900", "#c0c530", "#79a8ff", "#b6a0ff", "#6ae4b9", "#ffffff" },
-	},
 }
