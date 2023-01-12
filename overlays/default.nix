09eab596ed8eb4e838634b081da87478f1d24a37
Jared Baur <jaredbaur@fastmail.com>
2023-01-11 23:09:12 -0800

    Call git push no matter what

--- overlays/default.nix
+++ overlays/default.nix
@@ -26,7 +26,7 @@ inputs: with inputs; {
         mirror-to-x = prev.callPackage ./mirror-to-x.nix { };
         pomo = prev.callPackage ./pomo { };
         v4l-show = prev.callPackage ./v4l-show.nix { };
-        wip = prev.writeShellScriptBin "wip" ''git commit --no-verify --no-gpg-sign --all --message "WIP" && git push'';
+        wip = prev.writeShellScriptBin "wip" ''git commit --no-verify --no-gpg-sign --all --message "WIP"; git push'';
         ixio = prev.writeShellScriptBin "ixio" "${prev.curl}/bin/curl -F 'f:1=<-' ix.io";
         stevenblack-hosts = prev.linkFarm "hosts" (
           let
