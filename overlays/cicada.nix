4d230dc66812634050e001135ce22bca082343cc
jmbaur <jmbaur@users.noreply.github.com>
2023-02-12 03:20:55 +0000

    Update dependencies

--- overlays/cicada.nix
+++ overlays/cicada.nix
@@ -1,16 +1,16 @@
 { rustPlatform, fetchFromGitHub, ... }:
 rustPlatform.buildRustPackage rec {
   pname = "cicada";
-  version = "0.9.35";
+  version = "0.9.36";
 
   src = fetchFromGitHub {
     owner = "mitnk";
     repo = "cicada";
     rev = "v${version}";
-    sha256 = "sha256-HzZGuM9zB/msjG0nMuIJ+nvWc1twiB7q1tsAjV857Sc=";
+    sha256 = "sha256-KEl9jSdH13usE+dC9fUvb6VbayB/XBo9VVMzd+hR/lg=";
   };
 
-  cargoSha256 = "sha256-bDbQVusWPlq9Mu6rXXOFjUFrYttREHEceZRi7FwdBtE=";
+  cargoSha256 = "sha256-NKOD64iN+EwzVXNA2KNsOxwNYx5B/YyGdn5vvcYH3t4=";
 
   doCheck = false;
 }
