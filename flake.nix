2c9097c5acff322610ff2422386c676738c69d93
Jared Baur <jaredbaur@fastmail.com>
2023-02-16 16:53:19 -0800

    Unpin nixpkgs from specific commit

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixos-router.inputs.nixpkgs.follows = "nixpkgs";
     nixos-router.url = "github:jmbaur/nixos-router";
-    nixpkgs.url = "github:nixos/nixpkgs/a51f9c5c1ca965c2dd3780d86692ce7766cc0930";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     pd-notify.inputs.nixpkgs.follows = "nixpkgs";
     pd-notify.url = "github:jmbaur/pd-notify";
     pre-commit.inputs.nixpkgs.follows = "nixpkgs";
