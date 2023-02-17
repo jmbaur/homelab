1643b6b4d6ee71fdfbc33ee7e3aadee962ba9977
Jared Baur <jaredbaur@fastmail.com>
2023-02-16 16:37:13 -0800

    Add udev rules for teensy USB controller

--- nixosModules/gui.nix
+++ nixosModules/gui.nix
@@ -107,5 +107,17 @@ with lib;
         export _JAVA_AWT_WM_NONREPARENTING=1
       '';
     };
+
+    # UDEV rules for Teensy USB devices
+    # Derived from https://www.pjrc.com/teensy/00-teensy.rules
+    services.udev.extraRules = ''
+      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
+      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789a]*", ENV{MTP_NO_PROBE}="1"
+      KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666", RUN:="${pkgs.coreutils}/bin/stty -F /dev/%k raw -echo"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="013*", MODE:="0666"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="013*", MODE:="0666"
+    '';
   };
 }
