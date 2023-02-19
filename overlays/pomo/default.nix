4822fbdeb364388c6fec209ce260da68975e9617
Jared Baur <jaredbaur@fastmail.com>
2023-02-18 21:49:29 -0800

    Fix go build for pomo

--- overlays/pomo/default.nix
+++ overlays/pomo/default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "pomo";
   version = "0.0.1";
   src = ./.;
-  vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+  vendorSha256 = null;
   ldflags = [ "-s" "-w" ];
 }
