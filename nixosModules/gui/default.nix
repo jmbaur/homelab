cc6c27b7224db8721c7e5e16a092b5c489afc255
Jared Baur <jaredbaur@fastmail.com>
2023-02-27 18:01:56 -0800

    Set systemd show status to auto

--- nixosModules/gui/default.nix
+++ nixosModules/gui/default.nix
@@ -7,6 +7,8 @@ with lib;
 {
   options.custom.gui.enable = mkEnableOption "GUI config";
   config = mkIf cfg.enable {
+    boot.kernelParams = [ "systemd.show_status=auto" ];
+
     environment.enableAllTerminfo = true;
 
     hardware.pulseaudio.enable = mkForce false;
