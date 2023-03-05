4fd0945def5a732f47e4c40978113276b06c673f
jmbaur <jmbaur@users.noreply.github.com>
2023-03-05 03:28:37 +0000

    Update dependencies

--- overlays/flarectl.nix
+++ overlays/flarectl.nix
@@ -1,13 +1,13 @@
 { buildGoModule, fetchFromGitHub, ... }:
 buildGoModule rec {
   pname = "flarectl";
-  version = "0.61.0";
+  version = "0.62.0";
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "cloudflare-go";
     rev = "v${version}";
-    sha256 = "sha256-s+jqlw/ma6nah+qkdBxA8CpATPuLGC7iDux7SVaBWbU=";
+    sha256 = "sha256-CLNOAcsby7JU4PrpoIx0K4Zw7Qy7zbYDXzPdGdKY6eE=";
   };
-  vendorSha256 = "sha256-c3MNTLKgy9olUfVMlkd1ICsopRosoevRtaUNTO3AR1E=";
+  vendorSha256 = "sha256-6PzAUnvs00QX4dZyr93Gb8yEN3GhpEB0RwJHQIS5edg=";
   subPackages = [ "cmd/flarectl" ];
 }
--- overlays/mini-nvim.nix
+++ overlays/mini-nvim.nix
@@ -5,7 +5,7 @@ vimUtils.buildVimPluginFrom2Nix rec {
   src = fetchFromGitHub {
     owner = "echasnovski";
     repo = "mini.nvim";
-    rev = "c16a18124da94fd6d35669a2208b78da0fdd18e2";
-    sha256 = "sha256-lBY7OjUpH+kI+a2GWTMev+RiKCn7J9jkd9XIW3jLTmg=";
+    rev = "0.7.024da94fd6d35669a2208b78da0fdd18e2";
+    sha256 = "";
   };
 }
