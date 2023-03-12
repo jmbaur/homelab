e23ae92a89ae80f2b02c93af051e3c1974773294
Jared Baur <jaredbaur@fastmail.com>
2023-03-12 10:33:18 -0700

    Use tailscale on rhubarb

--- nixos-configurations/rhubarb/default.nix
+++ nixos-configurations/rhubarb/default.nix
@@ -1,6 +1,35 @@
 { config, lib, pkgs, modulesPath, ... }: {
   imports = [ "${modulesPath}/installer/sd-card/sd-image-aarch64.nix" ];
 
+  services.tailscale.enable = true;
+  environment.systemPackages = [ pkgs.tailscale ];
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey $(cat /var/lib/tskey)
+    '';
+  };
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+    allowedTCPPorts = [ 22 ];
+  };
+
   custom = {
     server.enable = true;
     disableZfs = true;
@@ -22,10 +51,8 @@
   boot.kernelPackages = pkgs.linuxPackages_6_1;
   boot.initrd.systemd.enable = true;
 
-  networking = {
-    hostName = "rhubarb";
-    useDHCP = false;
-  };
+  networking.hostName = "rhubarb";
+  networking.useDHCP = false;
   services.resolved = {
     enable = true;
     # The RPI does not have an RTC, so DNSSEC without an accurate time does not
