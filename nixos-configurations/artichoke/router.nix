81c3712c37aa333e4a1f94a1604332181e691650
Jared Baur <jaredbaur@fastmail.com>
2023-03-15 00:14:18 -0700

    Add monitoring services to artichoke

--- nixos-configurations/artichoke/router.nix
+++ nixos-configurations/artichoke/router.nix
@@ -75,5 +75,38 @@ in
         in
         [ "internal:echo" "executable:${updateCloudflare}" "executable:${updateHE}" ];
     };
+
+    # monitoring
+    services.journald.enableHttpGateway = true;
+    services.prometheus.exporters = {
+      blackbox = {
+        enable = false;
+        configFile = toString ((pkgs.formats.yaml { }).generate "blackbox-config" {
+          modules = {
+            icmpv6_connectivity = {
+              prober = "icmp";
+              timeout = "5s";
+              icmp = {
+                preferred_ip_protocol = "ip6";
+                ip_protocol_fallback = false;
+              };
+            };
+            icmpv4_connectivity = {
+              prober = "icmp";
+              timeout = "5s";
+              icmp = {
+                preferred_ip_protocol = "ip4";
+                ip_protocol_fallback = false;
+              };
+            };
+          };
+        });
+      };
+      node = {
+        enable = true;
+        enabledCollectors = [ "ethtool" "network_route" "systemd" ];
+      };
+    };
+
   };
 }
