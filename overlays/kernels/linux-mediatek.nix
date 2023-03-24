144116c39ee340bee8d64be3405f3390ab4a829d
Jared Baur <jaredbaur@fastmail.com>
2023-03-23 23:58:45 -0700

    Remove some merged kernel patches

--- overlays/kernels/linux-mediatek.nix
+++ overlays/kernels/linux-mediatek.nix
@@ -23,30 +23,6 @@ in
       };
     }
     rec {
-      name = "mt8192-asurada-audio.patch";
-      patch = fetchpatch {
-        inherit name;
-        url = "https://lore.kernel.org/lkml/20221102190611.283546-4-nfraprado@collabora.com/raw";
-        sha256 = "sha256-cDnsShBTUPpSi4tNjdN+98bZ1ILDyFF6x9DyoqJrL5A=";
-      };
-    }
-    rec {
-      name = "mt8192-asurada-spherion-audio.patch";
-      patch = fetchpatch {
-        inherit name;
-        url = "https://lore.kernel.org/lkml/20221102190611.283546-5-nfraprado@collabora.com/raw";
-        sha256 = "sha256-82rHlVxYxN0Bgd6g+Rb/paUIIrXT3nMyuj8oP3l4aUk=";
-      };
-    }
-    rec {
-      name = "mt8192-asurada-i2c-mmc-aliases.patch";
-      patch = fetchpatch {
-        inherit name;
-        url = "https://lore.kernel.org/lkml/20221102190611.283546-6-nfraprado@collabora.com/raw";
-        sha256 = "sha256-xgP2nyGmfej35kclKffDhL3oklN6+dMnIQW12uei03o=";
-      };
-    }
-    rec {
       name = "mt8192-gpu.patch";
       patch = fetchpatch {
         inherit name;
@@ -62,10 +38,7 @@ in
 }
 ).overrideAttrs
   (old: {
-    # TODO(jared): find a better way to set the config to allow for installing DTBs
     passthru = old.passthru // {
-      config = old.passthru.config // {
-        DTB = stdenv.hostPlatform.system != "x86_64-linux";
-      };
+      config = stdenv.hostPlatform.linux-kernel;
     };
   })
