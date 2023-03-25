5036ca5a6ed88da740de02f3749dc49f062df5b3
Jared Baur <jaredbaur@fastmail.com>
2023-03-25 10:34:31 -0700

    Don't check for supported keys

--- overlays/dookie/src/main.rs
+++ overlays/dookie/src/main.rs
@@ -21,21 +21,13 @@ fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
     let device = Device::open(&cli.device)?;
-
     let key = Key::new(cli.key_code);
-
-    if device
-        .supported_keys()
-        .map_or(false, |keys| keys.contains(key))
-    {
-        eprintln!("device {:?} does not support key {:?}", &cli.device, key);
-        std::process::exit(1);
-    }
+    let action = cli.action;
 
     loop {
         let key_state = device.get_key_state()?;
         if key_state.contains(key) {
-            match cli.action {
+            match action {
                 Action::Restart => {
                     // According to systemd(1), SIGRTMIN+5 reboots the machine by starting the
                     // reboot.target unit.
