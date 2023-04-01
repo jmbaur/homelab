85fe72d50a001f3949ac97eaaf0444779ad4bd27
Jared Baur <jaredbaur@fastmail.com>
2023-04-01 12:06:20 -0700

    Point to lock file directly

--- overlays/dookie/default.nix
+++ overlays/dookie/default.nix
@@ -3,5 +3,5 @@ rustPlatform.buildRustPackage {
   pname = "dookie";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "sha256-gQOoaiURl2QMFTs/byZdsAc5JEFpfNzFaVsE+0rJoj4=";
+  cargoLock.lockFile = ./Cargo.lock;
 }
