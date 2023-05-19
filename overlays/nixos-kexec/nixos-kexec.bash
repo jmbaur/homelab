9060b19136c80cf9c36451a59844a8f2e1f90b1d
Jared Baur <jaredbaur@fastmail.com>
2023-05-19 08:40:59 -0700

    Fix nixos-kexec when there are no specialisations

--- overlays/nixos-kexec/nixos-kexec.bash
+++ overlays/nixos-kexec/nixos-kexec.bash
@@ -5,7 +5,7 @@ choices=()
 for profile in $(find /nix/var/nix/profiles -type l -name "system-*" | sort); do
 	system_name=$(basename "$profile")
 	label=$(jq --raw-output '."org.nixos.bootspec.v1".label' <"${profile}/boot.json")
-	for specialisation in "$profile"/specialisation/*; do
+	for specialisation in $(find "$profile"/specialisation -type l | sort); do
 		specialisation_name=$(basename "$specialisation")
 		label=$(jq --raw-output '."org.nixos.bootspec.v1".label' <"${profile}/boot.json")
 		choices+=("${system_name}/specialisation/${specialisation_name}: ${label}")
