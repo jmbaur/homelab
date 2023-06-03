abcfbec58e71df18db1d5693d350ca76977af9d7
Jared Baur <jaredbaur@fastmail.com>
2023-06-02 18:54:02 -0700

    Add SSHOPTS to deploy script

--- nixos-modules/deploy.nix
+++ nixos-modules/deploy.nix
@@ -3,7 +3,10 @@
     ssh_target=$1
     deploy_type=''${2:-switch}
     nix-copy-closure --to $ssh_target ${config.system.build.toplevel}
-    ssh $ssh_target nix-env --profile /nix/var/nix/profiles/system --set ${config.system.build.toplevel}
-    ssh $ssh_target ${config.system.build.toplevel}/bin/switch-to-configuration $deploy_type
+    ssh $SSHOPTS $ssh_target nix-env --profile /nix/var/nix/profiles/system --set ${config.system.build.toplevel}
+    ssh $SSHOPTS $ssh_target ${config.system.build.toplevel}/bin/switch-to-configuration "$deploy_type"
+    if [[ "$deploy_type" == "boot" ]]; then
+      echo "system set to switch to new configuration at next boot, reboot to see changes"
+    fi
   '';
 }
