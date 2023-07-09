23ee5660187fa0c7baa36e4ffd59c7e7aeda35eb
jmbaur <jmbaur@users.noreply.github.com>
2023-07-09 03:30:02 +0000

    Update dependencies

--- overlays/flarectl.nix
+++ overlays/flarectl.nix
@@ -1,12 +1,12 @@
 { buildGoModule, fetchFromGitHub, ... }:
 buildGoModule rec {
   pname = "flarectl";
-  version = "0.70.0";
+  version = "0.72.0";
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "cloudflare-go";
     rev = "v${version}";
-    sha256 = "sha256-HVQ5n18UF2nlLEQT1ks3/ca5rNZFjFF7tb2nWJ4U11g=";
+    sha256 = "sha256-SDK0jTj058sVY+oIvaX0iZDMa8v3kVnjIWu8aFFbpmU=";
   };
   vendorSha256 = "sha256-3BeOTNVaM04COzl5v4h3nxLPughR0N/5MdW071s5CUc=";
   subPackages = [ "cmd/flarectl" ];
