6b5fef8cc667117a06f454e509c8b52ce0d74258
Jared Baur <jaredbaur@fastmail.com>
2023-07-23 12:02:52 -0700

    Revert "Try out mini.files"
    This reverts commit 0c3dc6adaee871163700d30f9b4b32e061e4ad65.

--- overlays/neovim/settings/lua/filemanager.lua
+++ overlays/neovim/settings/lua/filemanager.lua
@@ -1,41 +1,4 @@
-require("mini.files").setup({ content = { prefix = function() end } })
+local oil = require("oil")
 
-vim.keymap.set("n", "-", MiniFiles.open, { desc = "Open parent directory" })
-
-local show_dotfiles = false
-local filter_show = function(fs_entry)
-	return true
-end
-
-local filter_hide = function(fs_entry)
-	return not vim.startswith(fs_entry.name, ".")
-end
-
-local toggle_dotfiles = function()
-	show_dotfiles = not show_dotfiles
-	local new_filter = show_dotfiles and filter_show or filter_hide
-	MiniFiles.refresh({ content = { filter = new_filter } })
-end
-
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesBufferCreate",
-	callback = function(args)
-		local buf_id = args.data.buf_id
-		-- Tweak left-hand side of mapping to your liking
-		vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
-	end,
-})
-
-local files_set_cwd = function(path)
-	-- Works only if cursor is on the valid file system entry
-	local cur_entry_path = MiniFiles.get_fs_entry().path
-	local cur_directory = vim.fs.dirname(cur_entry_path)
-	vim.fn.chdir(cur_directory)
-end
-
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesBufferCreate",
-	callback = function(args)
-		vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
-	end,
-})
+oil.setup()
+vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
