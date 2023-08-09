bf7669a0bdfc830cad2624ebcbe0f665a1c516cc
Jared Baur <jaredbaur@fastmail.com>
2023-08-09 09:56:35 -0700

    Add nixpkgs-wayland cachix config

--- flake.nix
+++ flake.nix
@@ -2,8 +2,14 @@
   description = "NixOS configurations for the homelab";
 
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
   };
 
   inputs = {
