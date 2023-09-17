bd425a3faa8e2fd0a33b2f43c94cf398ceeaff39
jmbaur <jmbaur@users.noreply.github.com>
2023-09-17 03:15:51 +0000

    Update dependencies

--- overlays/xremap.nix
+++ overlays/xremap.nix
@@ -1,14 +1,14 @@
 { rustPlatform, fetchFromGitHub, buildFeatures ? [ "sway" ], ... }:
 rustPlatform.buildRustPackage rec {
   pname = "xremap";
-  version = "0.8.6";
+  version = "0.8.7";
   src = fetchFromGitHub {
     owner = "k0kubun";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-l4erKAbf3dzxOQpzl7eIvGgQrzEsZYOGswVbz4Zh90g=";
+    sha256 = "sha256-fypHpJTCB0SLzcIuWzJIsBSk8OHuSjmtQNNrs0HtTgM=";
   };
-  cargoSha256 = "sha256-RABdEh+XAVm4NlaVd15aSm8p6/zcP+BUWY3Tzwtqn7E=";
+  cargoSha256 = "sha256-uI1gwE4EXuSzQiRmvD4YO3ziPWk73ZtKw2gtO5JEbu8=";
   buildNoDefaultFeatures = true;
   inherit buildFeatures;
 }
