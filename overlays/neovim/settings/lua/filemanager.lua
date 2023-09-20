f1bd55ac3c957038f08086be3412ace8ad8365e1
Jared Baur <jaredbaur@fastmail.com>
2023-09-19 23:30:08 -0700

    Set MiniFiles to open from current directory of current file

--- overlays/neovim/settings/lua/filemanager.lua
+++ overlays/neovim/settings/lua/filemanager.lua
@@ -3,4 +3,6 @@ local mini_files = require("mini.files")
 -- don't show icons
 mini_files.setup({ content = { prefix = function() end } })
 
-vim.keymap.set("n", "-", MiniFiles.open, { desc = "Open parent directory" })
+vim.keymap.set("n", "-", function()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0))
+end, { desc = "Open parent directory" })
