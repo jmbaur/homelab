78fea7333f4599778e4d6c197efe8b3a89f63fa7
Jared Baur <jaredbaur@fastmail.com>
2023-09-23 18:06:34 -0700

    Add script to armada 38x installer to generate new mac addresses

--- nixos-configurations/default.nix
+++ nixos-configurations/default.nix
@@ -196,6 +196,16 @@ in
         custom.server.enable = true;
         custom.crossCompile.enable = true;
         custom.disableZfs = true;
+        environment.systemPackages = [
+          (pkgs.writeShellScriptBin "generate-macaddrs" ''
+            echo "ethaddr $(${lib.getExe pkgs.macgen})" | tee -a macaddrs
+            echo "eth1addr $(${lib.getExe pkgs.macgen})" | tee -a macaddrs
+            echo "eth2addr $(${lib.getExe pkgs.macgen})" | tee -a macaddrs
+            echo "eth3addr $(${lib.getExe pkgs.macgen})" | tee -a macaddrs
+            ${pkgs.ubootEnvTools}/bin/fw_setenv --script macaddrs
+            echo "wrote new macaddrs to uboot environment"
+          '')
+        ];
         sdImage.populateFirmwareCommands = lib.mkForce "";
         sdImage.populateRootCommands = ''
           mkdir -p ./files/boot
