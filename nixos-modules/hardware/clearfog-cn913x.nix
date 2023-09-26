c9ab7fdcd59daf0b0c501e99a5ff8e67f27a141e
Jared Baur <jaredbaur@fastmail.com>
2023-09-26 01:47:52 -0700

    Use cn913x_build patches against newer kernel than what they specify

--- nixos-modules/hardware/clearfog-cn913x.nix
+++ nixos-modules/hardware/clearfog-cn913x.nix
@@ -11,9 +11,8 @@
 
     boot.kernelParams = [ "console=ttyS0,115200" "cma=256M" ];
 
-    boot.kernelPackages = pkgs.linuxPackages_5_15;
+    boot.kernelPackages = pkgs.linuxPackages_6_1;
     boot.kernelPatches =
-      # NOTE: patches that don't apply cleanly are commented out
       (map
         (patch: {
           name = lib.replaceStrings [ ".patch" ] [ "" ] (builtins.baseNameOf patch);
@@ -24,14 +23,14 @@
         "${pkgs.cn913x_build_repo}/patches/linux/0003-Documentation-bindings-armada-thermal-Added-armada-a.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0004-thermal-armada-ap807-Thermal-data-structure-added.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0005-dts-armada-ap807-updated-thermal-compatibility.patch"
-        # "${pkgs.cn913x_build_repo}/patches/linux/0006-net-sfp-add-support-for-a-couple-of-copper-multi-rat.patch"
-        # "${pkgs.cn913x_build_repo}/patches/linux/0007-net-sfp-add-support-for-HXSX-ATRI-1-copper-SFP-modul.patch"
+        "${pkgs.cn913x_build_repo}/patches/linux/0006-net-sfp-add-support-for-a-couple-of-copper-multi-rat.patch"
+        "${pkgs.cn913x_build_repo}/patches/linux/0007-net-sfp-add-support-for-HXSX-ATRI-1-copper-SFP-modul.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0008-arm64-dts-cn913x-add-cn913x-based-COM-express-type-7.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0009-cn9130-som-for-clearfog-base-and-pro.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0010-linux-add-support-cn9131-cf-solidwan.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0011-linux-add-support-cn9131-bldn-mbv.patch"
-        # "${pkgs.cn913x_build_repo}/patches/linux/0012-DPDK-support-for-MVPP2.patch"
-        # "${pkgs.cn913x_build_repo}/patches/linux/0013-Switch-back-to-kernel-when-MUSDK-stops.patch"
+        "${pkgs.cn913x_build_repo}/patches/linux/0012-DPDK-support-for-MVPP2.patch"
+        "${pkgs.cn913x_build_repo}/patches/linux/0013-Switch-back-to-kernel-when-MUSDK-stops.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0014-linux-fix-5GB-ports-phy-support-cn9132.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0015-Add-phy-support-1G-eth-ports-Belden-cn9131.patch"
         "${pkgs.cn913x_build_repo}/patches/linux/0016-arm64-dts-cn9131-cf-solidwan-update-model-property-t.patch"
