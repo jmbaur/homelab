887e34e25af3c97cdb75bf0140493023d10f2f63
Jared Baur <jaredbaur@fastmail.com>
2023-09-26 21:43:52 -0700

    Use readDir to remove a bunch of manually added patches

--- nixos-modules/hardware/clearfog-cn913x.nix
+++ nixos-modules/hardware/clearfog-cn913x.nix
@@ -1,4 +1,14 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  cn913xBuildRepo = pkgs.fetchFromGitHub {
+    owner = "solidrun";
+    repo = "cn913x_build";
+    rev = "d6d0577e6b6e86d29837618e9a02f5ee4ac136cb";
+    hash = "sha256-5PGu7XQxtg0AP9RovDDqmPuVnrNQow1bYaorAmUFQ7Q=";
+  };
+  cn913xLinuxPatchesPath = "${cn913xBuildRepo}/patches/linux";
+in
+{
   options.hardware.clearfog-cn913x = {
     enable = lib.mkEnableOption "clearfog-cn913x";
   };
@@ -12,57 +22,37 @@
     boot.kernelParams = [ "console=ttyS0,115200" "cma=256M" ];
 
     boot.kernelPackages = pkgs.linuxPackages_6_1;
-    boot.kernelPatches =
-      (map
-        (patch: {
-          name = lib.replaceStrings [ ".patch" ] [ "" ] (builtins.baseNameOf patch);
-          inherit patch;
-        }) [
-        "${pkgs.cn913x_build_repo}/patches/linux/0001-cpufreq-armada-enable-ap807-cpu-clk.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0002-thermal-armada-ap806-Thermal-values-updated.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0003-Documentation-bindings-armada-thermal-Added-armada-a.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0004-thermal-armada-ap807-Thermal-data-structure-added.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0005-dts-armada-ap807-updated-thermal-compatibility.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0006-net-sfp-add-support-for-a-couple-of-copper-multi-rat.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0007-net-sfp-add-support-for-HXSX-ATRI-1-copper-SFP-modul.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0008-arm64-dts-cn913x-add-cn913x-based-COM-express-type-7.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0009-cn9130-som-for-clearfog-base-and-pro.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0010-linux-add-support-cn9131-cf-solidwan.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0011-linux-add-support-cn9131-bldn-mbv.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0012-DPDK-support-for-MVPP2.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0013-Switch-back-to-kernel-when-MUSDK-stops.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0014-linux-fix-5GB-ports-phy-support-cn9132.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0015-Add-phy-support-1G-eth-ports-Belden-cn9131.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0016-arm64-dts-cn9131-cf-solidwan-update-model-property-t.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0017-arm64-dts-cn9130-som-support-eeprom-replacement-part.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0018-arm64-dts-cn9131-cf-solidwan-add-alias-for-ethernet5.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0019-arm64-dts-cn9131-cf-solidwan-switch-cp0_phy0-to-auto.patch"
-        "${pkgs.cn913x_build_repo}/patches/linux/0020-arm64-dts-cn9131-cf-solidwan-enable-only-cp0-rtc.patch"
-      ]) ++ [{
-        name = "cn913x-enablement";
-        patch = null;
-        extraStructuredConfig = with lib.kernel; {
-          ACPI_CPPC_CPUFREQ = yes;
-          ARM_ARMADA_8K_CPUFREQ = yes;
-          CPU_FREQ_DEFAULT_GOV_ONDEMAND = yes;
-          CPU_FREQ_GOV_CONSERVATIVE = yes;
-          CPU_FREQ_GOV_POWERSAVE = yes;
-          EEPROM_AT24 = yes;
-          GPIO_SYSFS = yes;
-          MARVELL_10G_PHY = yes;
-          MARVELL_PHY = yes;
-          NET_DSA = module;
-          NET_DSA_MV88E6XXX = module;
-          SENSORS_MCP3021 = yes;
-          SENSORS_PWM_FAN = yes;
-          SFP = yes;
-          UIO = yes;
-          USB_SERIAL = yes;
-          USB_SERIAL_FTDI_SIO = yes;
-          USB_SERIAL_OPTION = yes;
-          USB_SERIAL_WWAN = yes;
-        };
-      }];
+    boot.kernelPatches = (map
+      (patch: {
+        name = lib.replaceStrings [ ".patch" ] [ "" ] patch;
+        patch = "${cn913xLinuxPatchesPath}/${patch}";
+      })
+      (lib.attrNames (lib.filterAttrs (_: entry: entry == "regular") (builtins.readDir cn913xLinuxPatchesPath))))
+    ++ [{
+      name = "cn913x-enablement";
+      patch = null;
+      extraStructuredConfig = with lib.kernel; {
+        ACPI_CPPC_CPUFREQ = yes;
+        ARM_ARMADA_8K_CPUFREQ = yes;
+        CPU_FREQ_DEFAULT_GOV_ONDEMAND = yes;
+        CPU_FREQ_GOV_CONSERVATIVE = yes;
+        CPU_FREQ_GOV_POWERSAVE = yes;
+        EEPROM_AT24 = yes;
+        GPIO_SYSFS = yes;
+        MARVELL_10G_PHY = yes;
+        MARVELL_PHY = yes;
+        NET_DSA = module;
+        NET_DSA_MV88E6XXX = module;
+        SENSORS_MCP3021 = yes;
+        SENSORS_PWM_FAN = yes;
+        SFP = yes;
+        UIO = yes;
+        USB_SERIAL = yes;
+        USB_SERIAL_FTDI_SIO = yes;
+        USB_SERIAL_OPTION = yes;
+        USB_SERIAL_WWAN = yes;
+      };
+    }];
 
     hardware.deviceTree = {
       enable = true;
