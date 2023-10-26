a19998714843881e55c2a6e4245732fe84bf9c05
Jared Baur <jaredbaur@fastmail.com>
2023-10-26 16:23:37 +0000

    Add document diagnostics picker

--- overlays/neovim/settings/lua/picker.lua
+++ overlays/neovim/settings/lua/picker.lua
@@ -5,19 +5,42 @@ pick.setup({
 
 vim.ui.select = pick.ui_select
 
--- TODO(jared): look into use cases for pick.builtin.cli
-vim.keymap.set("n", "<leader>?", pick.builtin.help, { desc = "Find help" })
-vim.keymap.set("n", "<leader>b", pick.builtin.buffers, { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>f", pick.builtin.files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>g", pick.builtin.grep_live, { desc = "Find regexp pattern" })
-vim.keymap.set("n", "<leader>r", pick.builtin.resume, { desc = "Resume last pick" })
-
 -- TODO(jared): don't do this, but the default link for MiniPickMatchCurrent
 -- makes it unusable. Current colorscheme probably needs to be updated.
 vim.cmd [[ highlight! link MiniPickMatchCurrent Done ]]
 
+-- https://github.com/echasnovski/mini.nvim/issues/513#issuecomment-1775085181
+pick.registry.document_diagnostics = function()
+	local items = {}
+	local results = vim.diagnostic.get()
+
+	for _, result in ipairs(results) do
+		table.insert(items, {
+			text = result.message,
+			bufnr = result.bufnr,
+			lnum = result.lnum + 1,
+			end_lnum = result.end_lnum + 1,
+			col = result.col + 1,
+			end_col = result.end_col + 1,
+		})
+	end
+
+	MiniPick.start({
+		source = {
+			items = items,
+			name = 'Document diagnostics'
+		}
+	})
+end
+
+-- TODO(jared): look into use cases for pick.builtin.cli
+vim.keymap.set("n", "<leader>?", pick.builtin.help, { desc = "Find help" })
+vim.keymap.set("n", "<leader>b", pick.builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>d", pick.registry.document_diagnostics, { desc = "Find document diagnostics" })
+vim.keymap.set("n", "<leader>f", pick.builtin.files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>g", pick.builtin.grep_live, { desc = "Find regexp pattern" })
+vim.keymap.set("n", "<leader>r", pick.builtin.resume, { desc = "Resume last pick" })
+
 -- NOTE: pickers from telescope that do not (yet?) have matching pickers in mini.pick
 -- vim.keymap.set("n", "<leader>_", telescope_builtins.registers, { desc = "Find registers" })
--- vim.keymap.set("n", "<leader>d", document_diagnostics, { desc = "Find document diagnostics" })
 -- vim.keymap.set("n", "<leader>h", telescope_builtins.command_history, { desc = "Find Ex-mode history" })
--- vim.keymap.set("n", "<leader>w", telescope_builtins.diagnostics, { desc = "Find workspace diagnostics" })
