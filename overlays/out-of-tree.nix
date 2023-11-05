13aa2522c14147e2c49bd5a35eec5a6c740051e7
Jared Baur <jaredbaur@fastmail.com>
2023-11-05 04:01:50 +0000

    Remove unused stuff

--- overlays/out-of-tree.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }: {
-  cicada = pkgs.callPackage ./cicada.nix { };
-  depthcharge-tools = pkgs.callPackage ./depthcharge-tools.nix { };
-  stevenblack-blocklist = pkgs.callPackage ./stevenblack-blocklist.nix { };
-}
