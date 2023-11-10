655ba4cfdb211b70fbb4533835e77050f1f89d55
Jared Baur <jaredbaur@fastmail.com>
2023-11-09 22:52:30 -0800

    Oil just feels nicer

--- overlays/neovim/settings/lua/filemanager.lua
+++ overlays/neovim/settings/lua/filemanager.lua
@@ -1,8 +1,2 @@
-local mini_files = require("mini.files")
-
--- don't show icons
-mini_files.setup({ content = { prefix = function() end } })
-
-vim.keymap.set("n", "-", function()
-	MiniFiles.open(vim.api.nvim_buf_get_name(0))
-end, { desc = "Open parent directory" })
+require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
