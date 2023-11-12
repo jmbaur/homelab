5a60ee397f05b2c97cb2e13edb2fc73b7d38c54a
Jared Baur <jaredbaur@fastmail.com>
2023-11-11 16:10:55 -0800

    Remove unused stuff

--- nixos-configurations/beetroot/disko.nix
+++ nixos-configurations/beetroot/disko.nix
@@ -5,48 +5,63 @@
         device = "/dev/nvme0n1";
         type = "disk";
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "1GiB";
-              bootable = true;
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
-            }
-            {
-              name = "luks";
-              start = "1GiB";
-              end = "100%";
+            };
+            root = {
+              size = "100%";
               content = {
-                type = "luks";
-                name = "cryptroot";
-                keyFile = "/tmp/secret.key";
-                content = {
-                  type = "btrfs";
-                  subvolumes = {
-                    "@" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
-                    };
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
-                    };
-                    "@home" = {
-                      mountpoint = "/home";
-                      mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
-                    };
+                type = "btrfs";
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
                   };
                 };
               };
-            }
-          ];
+              # name = "luks";
+              # start = "512MiB";
+              # end = "100%";
+              # content = {
+              #   type = "luks";
+              #   name = "cryptroot";
+              #   passwordFile = "/tmp/secret.key";
+              #   content = {
+              #     type = "btrfs";
+              #     subvolumes = {
+              #       "@" = {
+              #         mountpoint = "/";
+              #         mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
+              #       };
+              #       "@nix" = {
+              #         mountpoint = "/nix";
+              #         mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
+              #       };
+              #       "@home" = {
+              #         mountpoint = "/home";
+              #         mountOptions = [ "compress=zstd" "noatime" "discard=async" ];
+              #       };
+              #     };
+              #   };
+              # };
+            };
+          };
         };
       };
     };
