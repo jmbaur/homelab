c7279c62218f7547347d41ccbb54bb5bd034caaf
Jared Baur <jaredbaur@fastmail.com>
2023-11-18 09:37:46 -0800

    Remove patch that is now upstream

--- nixos-configurations/squash/default.nix
+++ nixos-configurations/squash/default.nix
@@ -1,25 +1,5 @@
-{ pkgs, lib, ... }: {
+{ pkgs, ... }: {
   imports = [ ./router.nix ./hardware-configuration.nix ];
-
-  nixpkgs.overlays = [
-    (final: prev:
-      let
-        systemdAtLeast254 = (lib.versionAtLeast prev.systemd.version "254");
-      in
-      {
-        systemd = (prev.systemd.overrideAttrs (old: lib.optionalAttrs systemdAtLeast254 {
-          # Fix cross-compiling to armv7 with systemd 254. Remove once changes
-          # from https://github.com/NixOS/nixpkgs/pull/258373 are in our
-          # nixpkgs. See https://nixpk.gs/pr-tracker.html?pr=258373.
-          patches = (old.patches or [ ]) ++ [
-            (final.fetchpatch {
-              url = "https://github.com/systemd/systemd/commit/cecbb162a3134b43d2ca160e13198c73ff34c3ef.patch";
-              hash = "sha256-hWpUosTDA18mYm5nIb9KnjwOlnzbEHgzha/WpyHoC54=";
-            })
-          ];
-        }));
-      })
-  ];
 
   boot.kernelParams = [ "cfg80211.ieee80211_regdom=US" ];
   boot.initrd.systemd.enable = true;
