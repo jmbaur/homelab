6f464ccde1d9f07addafc0d8cf35e1e735481ab0
Jared Baur <jaredbaur@fastmail.com>
2024-01-29 01:59:01 -0800

    Fix dtb path for squash

--- nixos-configurations/squash/default.nix
+++ nixos-configurations/squash/default.nix
@@ -7,7 +7,10 @@
   hardware.firmware = [ pkgs.linux-firmware ];
 
   hardware.armada-388-clearfog.enable = true;
-  hardware.deviceTree.name = "marvell/armada-388-clearfog-pro.dtb";
+
+  # TODO(jared): use FIT_BEST_MATCH feature in u-boot to choose this automatically
+  # TODO(jared): latest linux kernels have <vendor>/<dtb> for armv7
+  hardware.deviceTree.name = "armada-388-clearfog-pro.dtb";
 
   boot.initrd.systemd.emergencyAccess = lib.warn "initrd emergency access enabled" true;
 
