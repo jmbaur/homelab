971f18b933ff69adac1717df7a2843711b088395
Jared Baur <jaredbaur@fastmail.com>
2024-01-29 02:43:46 -0800

    Add redundant ubootenv mtd partition

--- nixos-modules/hardware/armada-388-clearfog/default.nix
+++ nixos-modules/hardware/armada-388-clearfog/default.nix
@@ -111,6 +111,7 @@
     environment.etc."fw_env.config".text = ''
       # MTD device name       Device offset   Env. size       Flash sector size       Number of sectors
       /dev/mtd2               0x0             0x20000         0x10000
+      /dev/mtd1               0x0             0x20000         0x10000
     '';
 
     # If no variables for these mac addresses exist, we need to generate them
