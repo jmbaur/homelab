16574bb7eaac637b2dfcde413857a1486b651ba8
Jared Baur <jaredbaur@fastmail.com>
2024-03-24 00:07:13 -0700

    Apply fix for cross-compiled gnupg in overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,21 +7,26 @@ inputs: {
       nixos-kexec = prev.callPackage ./nixos-kexec { };
 
       libfido2 = prev.libfido2.override {
-        withPcsclite = prev.stdenv.hostPlatform == prev.stdenv.buildPlatform;
+        withPcsclite = final.stdenv.hostPlatform == final.stdenv.buildPlatform;
       };
 
       wpa_supplicant = prev.wpa_supplicant.override {
-        withPcsclite = prev.stdenv.hostPlatform == prev.stdenv.buildPlatform;
+        withPcsclite = final.stdenv.hostPlatform == final.stdenv.buildPlatform;
       };
 
-      gnupg = prev.gnupg.override {
-        enableMinimal = prev.stdenv.hostPlatform != prev.stdenv.buildPlatform;
-      };
+      gnupg = (prev.gnupg.override {
+        enableMinimal = final.stdenv.hostPlatform != final.stdenv.buildPlatform;
+      }).overrideAttrs (old: {
+        # TODO(jared): can remove when https://github.com/NixOS/nixpkgs/pull/298001 is merged
+        configureFlags = old.configureFlags ++
+          final.lib.optional (final.stdenv.hostPlatform != final.stdenv.buildPlatform)
+            "GPGRT_CONFIG=${prev.lib.getDev final.libgpg-error}/bin/gpgrt-config";
+      });
 
       tcpdump = prev.tcpdump.overrideAttrs (old: {
         patches = (old.patches or [ ]) ++ [
           # add support for pref64
-          (prev.fetchpatch {
+          (final.fetchpatch {
             url = "https://github.com/the-tcpdump-group/tcpdump/commit/d879d9349ab7b3dffc4797b6a8ece758e93636c1.patch";
             hash = "sha256-Z1gHBYNUMdIkNT+miI3Iis183yJvc29OLAvg6kkvDGY=";
           })
@@ -31,20 +36,20 @@ inputs: {
       # Can be deleted when this PR is merged: https://github.com/NixOS/nixpkgs/pull/284160
       greetd = prev.greetd // {
         wlgreet = prev.greetd.wlgreet.overrideAttrs ({ nativeBuildInputs ? [ ], buildInputs ? [ ], ... }: {
-          nativeBuildInputs = nativeBuildInputs ++ [ prev.buildPackages.autoPatchelfHook ];
-          buildInputs = buildInputs ++ [ prev.gcc-unwrapped ];
-          runtimeDependencies = map prev.lib.getLib (with prev; [ gcc-unwrapped wayland libxkbcommon ]);
+          nativeBuildInputs = nativeBuildInputs ++ [ final.buildPackages.autoPatchelfHook ];
+          buildInputs = buildInputs ++ [ final.gcc-unwrapped ];
+          runtimeDependencies = map prev.lib.getLib (with final; [ gcc-unwrapped wayland libxkbcommon ]);
         });
       };
 
       labwc = prev.labwc.overrideAttrs ({ patches ? [ ], ... }: {
         patches = patches ++ [
-          (prev.fetchpatch2 {
+          (final.fetchpatch2 {
             name = "Add-omnipresent-flag-for-views";
             url = "https://github.com/labwc/labwc/commit/bad8f334ead5587208ec62fb01ddf9dd2be5ff67.patch";
             hash = "sha256-Djx+0cHklJCu/nmpwhO0dlHETeJnp5rG4hTjS3Wadg0=";
           })
-          (prev.fetchpatch2 {
+          (final.fetchpatch2 {
             name = "Add-touchpad-device-type";
             url = "https://github.com/labwc/labwc/commit/6faee17d20637df35e27cd2cac462323e4d665a1.patch";
             hash = "sha256-5HptxaEK7zH1eoyxYYyYZpKECE8MXznj6zLVDC0Kkyg=";
@@ -54,7 +59,7 @@ inputs: {
 
       fuzzel = prev.fuzzel.overrideAttrs ({ patches ? [ ], ... }: {
         patches = patches ++ [
-          (prev.fetchpatch2 {
+          (final.fetchpatch2 {
             name = "config-add-CTRL-to-default-keybindings";
             url = "https://codeberg.org/jmbaur/fuzzel/commit/2ecdc51a4f9ed83e94741a80648429ff7b062a14.patch";
             hash = "sha256-0cWbtc1O37zD8OCgufIurzCyuPzh5IRYPviRiOuhLpo=";
@@ -64,7 +69,7 @@ inputs: {
 
       fnott = prev.fnott.overrideAttrs ({ patches ? [ ], ... }: {
         patches = patches ++ [
-          (prev.fetchpatch2 {
+          (final.fetchpatch2 {
             name = "add-dbus-service-file";
             url = "https://codeberg.org/sewn/fnott/commit/6a092ba5fea58764cb56cfb037fcd334a8e3d67c.patch";
             excludes = [ "README.md" ]; # fails to apply
@@ -75,7 +80,7 @@ inputs: {
 
       libgit2_1_5 = prev.libgit2.overrideAttrs (_: rec {
         version = "1.5.2";
-        src = prev.fetchFromGitHub {
+        src = final.fetchFromGitHub {
           owner = "libgit2";
           repo = "libgit2";
           rev = "v${version}";
@@ -86,7 +91,7 @@ inputs: {
       git-shell-commands = prev.callPackage ./git-shell-commands {
         libgit2 = final.libgit2_1_5;
       };
-      pb = prev.writeShellScriptBin "pb" "${prev.curl}/bin/curl --data-binary @- https://paste.rs/";
+      pb = prev.writeShellScriptBin "pb" "${final.curl}/bin/curl --data-binary @- https://paste.rs/";
       tmux-jump = prev.callPackage ./tmux-jump.nix { };
       kinesis-kint41-jmbaur = prev.callPackage ./kinesis-kint41-jmbaur.nix { };
       macgen = prev.callPackage ./macgen.nix { };
@@ -99,14 +104,14 @@ inputs: {
       };
       jared-neovim-all-languages = final.jared-neovim.override { supportAllLanguages = true; };
 
-      bitwarden-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.bitwarden; };
-      brave-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.brave; };
-      chromium-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.chromium; };
-      discord-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.discord; };
-      google-chrome-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.google-chrome; };
-      signal-desktop-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.signal-desktop; };
-      slack-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.slack; };
-      spotify-wayland = prev.callPackage ./mk-wayland-variant.nix { package = prev.spotify; };
+      bitwarden-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.bitwarden; };
+      brave-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.brave; };
+      chromium-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.chromium; };
+      discord-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.discord; };
+      google-chrome-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.google-chrome; };
+      signal-desktop-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.signal-desktop; };
+      slack-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.slack; };
+      spotify-wayland = prev.callPackage ./mk-wayland-variant.nix { package = final.spotify; };
 
       mkWebApp = prev.callPackage
         ./mk-web-app.nix
@@ -132,7 +137,7 @@ inputs: {
       ubootEnvTools = prev.callPackage ./uboot-env-tools.nix { };
 
       uboot-clearfog_uart = prev.uboot-clearfog.override {
-        extraStructuredConfig = with prev.lib.kernel; {
+        extraStructuredConfig = with final.lib.kernel; {
           MVEBU_SPL_BOOT_DEVICE_MMC = no;
           MVEBU_SPL_BOOT_DEVICE_UART = yes;
         };
@@ -140,7 +145,7 @@ inputs: {
 
       mrvlUart = prev.callPackage ./mrvl-uart.nix { };
 
-      marvellBinaries = prev.fetchFromGitHub {
+      marvellBinaries = final.fetchFromGitHub {
         owner = "MarvellEmbeddedProcessors";
         repo = "binaries-marvell";
         # branch: binaries-marvell-armada-SDK10.0.1.0
@@ -148,14 +153,14 @@ inputs: {
         hash = "sha256-m8NdvFSVo5+TPtpiGevyzXIMR1YcSQu5Xi5ewUX983Y=";
       };
 
-      mvDdrMarvell = prev.applyPatches rec {
-        src = prev.fetchgit {
+      mvDdrMarvell = final.applyPatches rec {
+        src = final.fetchgit {
           url = "https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell";
           rev = "bfcf62051be835f725005bb5137928f7c27b792e";
           hash = "sha256-a6Hjx4/4uxQqNZRRa331B7aOtsyaUNhGh3izOSBrL3c=";
         };
         patches = [
-          (prev.substituteAll {
+          (final.substituteAll {
             src = ./mv-ddr-marvell-version.patch;
             shortRev = builtins.substring 0 7 src.rev;
           })
@@ -167,12 +172,12 @@ inputs: {
 
       mcbinFirmware = prev.callPackage ./mcbin-firmware { };
 
-      jmbaur-keybase-pgp-keys = prev.fetchurl {
+      jmbaur-keybase-pgp-keys = final.fetchurl {
         url = " https://keybase.io/jaredbaur/pgp_keys.asc ";
         sha256 = " sha256-R2a+bF7E6Zogl5XWsjrK5dkCAvK6K2h/bje37aYSgGc=";
       };
 
-      jmbaur-ssh-keys = prev.writeText "jmbaur.keys" ''
+      jmbaur-ssh-keys = final.writeText "jmbaur.keys" ''
         sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBhCHaXn5ghEJQVpVZr4hOajD6Zp/0PO4wlymwfrg/S5AAAABHNzaDo=
         sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHRlxBSW3BzX33FG7444p/M5lb9jYR5OkjS2jPpnuXozAAAABHNzaDo=
         ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDmnCgu1Jbl73bx7ijawfVEIHRFjAJ6qmXmYViGyykyA2DQgR3uzfoe09S9oITgHCIQUA53dy0kjQBhwVZJpXFV1eW+rxKBa024ob1yoBxCg6X5+lhBf5sgIEO48nNuDnYisINdbmxL5QqZjM7QnGukmWR5XjwmI83coWiAgbBueWKM70dxi5UgpBG89/RXgpz3OtEK16ZaW1yWyPwi1AY3xzz5HITUDw4AhhpohI/8uq15eDvgZXJwC9E/j9Frh1HhemWry34/d2RZe1w7l8glMvsEdN1NnfjzjQeZhv0EsbCySpqU3b9e0YMn3hda/FC12V9fuAJckAyh1oPPY2B1O+4nYGcuUv50NNnVB1UsSRKNlL5zHkIBpHB+3jba0tHeo/UUQBafmoTUWZh5k4U3bA2CWZ9N2T0SW632LAFUn5KeZoYgl/v0/uzhsXe87MDvmI869lpaOxbzfM3Mnu/XAPYPraUXdeW8a9fL3R/4f/vPSP/V5VfRzBCNa1AJDSdH5/IwpwqCrlO8woixjRYcknnZLNqkR92iqsNYUTP3+xYHHocRBPcLsuGtdbl81QxW9jtk7Ls9q9A/gMYk4WgiVXtbrmVg3FlNsi0TnjJQgMYnsRen9z904AouQXGf8CrFlmxJvwWlK1RU+Q29+PemjVaTr3vME0HMpyEny0+Wmw==
