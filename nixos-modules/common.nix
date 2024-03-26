8023f0137542600a451b8fba55e44753c4389dc1
Jared Baur <jaredbaur@fastmail.com>
2024-03-26 14:18:27 -0700

    Update inputs and bump version

--- nixos-modules/common.nix
+++ nixos-modules/common.nix
@@ -11,7 +11,7 @@ in
     system.stateVersion = lib.mkDefault "24.05";
 
     system.image.id = config.system.nixos.distroId;
-    system.image.version = "0.0.33";
+    system.image.version = "0.0.34";
 
     # We always build on x86_64-linux.
     #
