3c3f7d0a854096ae4650dad459b2771d1b49fe9d
Jared Baur <jaredbaur@fastmail.com>
2024-04-16 00:56:51 -0700

    Add networkd-dispatcher to clatd module

--- nixos-modules/clatd.nix
+++ nixos-modules/clatd.nix
@@ -9,24 +9,49 @@ let
   ifaceName = "clat0";
 
   isNetworkd = config.networking.useNetworkd;
+
+  clatdConfig =
+    (pkgs.formats.keyValue { listToValue = lib.concatMapStringsSep "," toString; }).generate
+      "clatd.conf"
+      {
+        clat-dev = ifaceName;
+        debug = 2;
+        # TODO(jared): The perl DNS resolver does not seem to work well without
+        # this, but this obviously won't work outside of the home network.
+        dns64-servers = [ "fd4c:ddfe:28e9::1" ];
+      };
 in
 {
   options.services.clatd.enable = lib.mkEnableOption "clatd";
   config = lib.mkIf config.services.clatd.enable {
+    services.networkd-dispatcher = {
+      enable = true;
+      rules.restart-clatd = {
+        onState = [
+          "routable"
+          "off"
+        ];
+        script = ''
+          #!${pkgs.runtimeShell}
+          if [[ $IFACE != "${ifaceName}" ]]; then
+            systemctl restart clatd
+          fi
+        '';
+      };
+    };
+
     systemd.services.clatd = {
       after = [
         "modprobe@tun.service"
         "network-online.target"
       ];
       wants = [ "network-online.target" ];
-      unitConfig = {
-        StartLimitIntervalSec = 5;
-      };
+      unitConfig.StartLimitIntervalSec = 0;
       serviceConfig = {
         ExecStart = toString [
           (lib.getExe pkgs.clatd)
-          "clat-dev=${ifaceName}"
-          "debug=1"
+          "-c"
+          clatdConfig
         ];
         DynamicUser = true;
         PrivateTmp = true;
