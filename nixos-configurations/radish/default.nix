099ed653f45296cad2d7a2fc10d6e53be2ac0386
Jared Baur <jaredbaur@fastmail.com>
2024-04-17 07:31:31 -0700

    Add radish

---
+++ nixos-configurations/radish/default.nix
@@ -0,0 +1,16 @@
+{ lib, pkgs, ... }:
+{
+  nixpkgs.hostPlatform = "aarch64-linux";
+
+  # requires linux 6.8
+  hardware.deviceTree.name = "allwinner/sun50i-h618-orangepi-zero3.dtb";
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  users.users.root.password = lib.warn "EMPTY ROOT PASSWORD, DO NOT USE IN 'PRODUCTION'" "";
+
+  custom.image = {
+    enable = true;
+    primaryDisk = "/dev/disk/by-path/TODO";
+    boot.uboot.enable = true;
+  };
+}
