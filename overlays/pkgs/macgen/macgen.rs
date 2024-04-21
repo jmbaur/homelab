f8477f3946e39ad5058bea25bb06750141d672d7
Jared Baur <jaredbaur@fastmail.com>
2024-04-21 12:47:10 -0700

    Implement macgen

--- overlays/pkgs/macgen/macgen.rs
+++ overlays/pkgs/macgen/macgen.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use std::io::Read;
+
+fn main() -> std::io::Result<()> {
+    let mut urandom = std::fs::OpenOptions::new()
+        .read(true)
+        .open("/dev/urandom")?;
+
+    let mut mac_bytes = [0u8; 6];
+
+    urandom.read_exact(&mut mac_bytes)?;
+
+    println!(
+        "{}",
+        mac_bytes
+            .into_iter()
+            .map(|byte| format!("{:02x}", byte))
+            .collect::<Vec<String>>()
+            .join(":")
+    );
+
+    Ok(())
+}
