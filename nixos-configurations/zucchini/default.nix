abbdf6286ded16cd89234d56a6e294e97fa53e17
Jared Baur <jaredbaur@fastmail.com>
2024-05-26 16:37:58 -0700

    Add patches for rk3588s GPU support

--- nixos-configurations/zucchini/default.nix
+++ nixos-configurations/zucchini/default.nix
@@ -1,6 +1,18 @@
 { pkgs, lib, ... }:
 {
-  nixpkgs.hostPlatform = "aarch64-linux";
+  nixpkgs.hostPlatform = {
+    config = "aarch64-unknown-linux-gnu";
+    linux-kernel = {
+      name = "aarch64-multiplatform";
+      DTB = true;
+      baseConfig = "defconfig";
+      preferBuiltin = true;
+      autoModules = true;
+      # TODO(jared): requires https://github.com/NixOS/nixpkgs/pull/277975
+      target = "Image"; # "vmlinuz.efi"
+    };
+    gcc.arch = "armv8-a";
+  };
 
   custom.basicNetwork.enable = true;
   custom.dev.enable = true;
@@ -21,6 +33,27 @@
       name = "zboot-compression";
       patch = null;
       extraStructuredConfig.EFI_ZBOOT = lib.kernel.yes;
+    }
+    {
+      name = "correct-gpio_pwrctrl1-typos-on";
+      patch = pkgs.fetchpatch {
+        url = "https://github.com/torvalds/linux/commit/d7f2039e5321636069baa77ef2f1e5d22cb69a88.patch";
+        hash = "sha256-7pOrOZx/OnaPzVV6jumRxN4/ZL7KIa8m4IXfA2mow6I=";
+      };
+    }
+    {
+      name = "Enable-GPU-on-Orange-Pi-5";
+      patch = pkgs.fetchpatch {
+        url = "https://github.com/torvalds/linux/commit/8beafb228f2be5de03e73178ac1081847d0d411f.patch";
+        hash = "sha256-Z3K+qL9aRTzBhAI3RhO0va27oxbxegVe11JACaB9Re4=";
+      };
+    }
+    {
+      name = "add-USB-C-support-to-rk3588s-orangepi-5";
+      patch = pkgs.fetchpatch {
+        url = "https://github.com/torvalds/linux/commit/c57d1a970275aabfbfab4c56001394ada3456d8e.patch";
+        hash = "sha256-WNSgdzjOqXwMVQQcn84K4CPB3oguh4Dt/DooUsVdpZQ=";
+      };
     }
   ];
 
