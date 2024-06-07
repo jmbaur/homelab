89823b891a42f97487497206f7178de2be1c3378
Jared Baur <jaredbaur@fastmail.com>
2024-06-06 23:00:15 -0700

    Add firmware to potato

--- nixos-configurations/potato/default.nix
+++ nixos-configurations/potato/default.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.chromebook.enable = true;
 
   boot.initrd.availableKernelModules = [
     "nvme"
@@ -8,11 +12,14 @@
     "usb_storage"
     "xhci_pci"
   ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  boot.kernelParams = [ "console=ttyS0,115200" ];
+  boot.kernelParams = [
+    "console=ttyS0,115200"
+    "console=tty0"
+  ];
 
   tinyboot = {
     enable = true;
@@ -27,7 +34,11 @@
     };
   };
 
+  users.users.root.openssh.authorizedKeys.keyFiles = [ pkgs.jmbaur-ssh-keys ];
+
   custom.basicNetwork.enable = true;
+
+  custom.server.enable = true;
 
   custom.image = {
     enable = true;
