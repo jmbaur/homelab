7c9aac2b5db9164d02c7f70abc640b7fe315f9f3
Jared Baur <jaredbaur@fastmail.com>
2024-06-14 12:29:37 -0700

    Bump version

--- nixos-modules/common.nix
+++ nixos-modules/common.nix
@@ -17,7 +17,7 @@ in
     system.stateVersion = lib.mkDefault "24.11";
 
     system.image.id = config.system.nixos.distroId;
-    system.image.version = lib.mkDefault "0.0.95";
+    system.image.version = lib.mkDefault "0.0.96";
 
     # We always build on x86_64-linux.
     #
