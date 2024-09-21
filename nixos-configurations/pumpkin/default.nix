55014c5c76e830afb242b51776a89b04c7306b8f
Jared Baur <jaredbaur@fastmail.com>
2024-09-20 23:08:49 -0700

    Add mcbin config with some services

--- nixos-configurations/pumpkin/default.nix
+++ nixos-configurations/pumpkin/default.nix
@@ -1,8 +1,34 @@
+{ lib, ... }:
 {
-  nixpkgs.hostPlatform = "aarch64-linux";
-  hardware.deviceTree.name = "marvell/armada-8040-mcbin.dtb";
-
-  custom.image.boot.uefi.enable = true;
-  custom.server.enable = true;
-  custom.basicNetwork.enable = true;
+  config = lib.mkMerge [
+    {
+      nixpkgs.hostPlatform = "aarch64-linux";
+      hardware.deviceTree.name = "marvell/armada-8040-mcbin.dtb";
+    }
+    {
+      custom.image = {
+        boot.uefi.enable = true;
+        installer.targetDisk = "/dev/nvme0n1"; # TODO(jared): be more specific
+      };
+      custom.server.enable = true;
+      custom.basicNetwork.enable = true;
+    }
+    {
+      services.navidrome = {
+        enable = true;
+        settings = {
+          Address = "[::]";
+          Port = 4533;
+          DefaultTheme = "Auto";
+        };
+      };
+    }
+    {
+      services.photoprism = {
+        enable = false;
+        address = "[::]";
+        originalsPath = "/data/photos";
+      };
+    }
+  ];
 }
