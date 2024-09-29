263b26e127a71a31aae5f12d408c389c896aebd9
Jared Baur <jaredbaur@fastmail.com>
2024-09-29 12:07:47 -0700

    Repurpose zucchini

--- nixos-configurations/zucchini/default.nix
+++ nixos-configurations/zucchini/default.nix
@@ -1,62 +1,67 @@
 { pkgs, lib, ... }:
 {
-  nixpkgs.hostPlatform = {
-    config = "aarch64-unknown-linux-gnu";
-    linux-kernel = {
-      name = "aarch64-multiplatform";
-      DTB = true;
-      baseConfig = "defconfig";
-      preferBuiltin = true;
-      autoModules = true;
-      target = "Image";
-    };
-    gcc.arch = "armv8-a";
-  };
+  config = lib.mkMerge [
+    {
+      nixpkgs.hostPlatform = {
+        config = "aarch64-unknown-linux-gnu";
+        linux-kernel = {
+          name = "aarch64-multiplatform";
+          DTB = true;
+          baseConfig = "defconfig";
+          preferBuiltin = true;
+          autoModules = true;
+          target = "Image";
+        };
+        gcc.arch = "armv8-a";
+      };
 
-  custom.dev.enable = true;
-  custom.normalUser.enable = true;
-  custom.image = {
-    boot.uefi.enable = true;
-    installer.targetDisk = "/dev/disk/by-path/platform-a41000000.pcie-pci-0004:41:00.0-nvme-1";
-  };
+      boot.kernelPackages = pkgs.linuxPackages_testing;
 
-  boot.kernelPackages = pkgs.linuxPackages_testing;
+      boot.initrd.availableKernelModules = [
+        "phy-rockchip-naneng-combphy"
+        "nvme"
+      ];
 
-  boot.initrd.availableKernelModules = [
-    "phy-rockchip-naneng-combphy"
-    "nvme"
+      hardware.deviceTree = {
+        enable = true;
+        name = "rockchip/rk3588s-orangepi-5.dtb";
+        overlays = [
+          {
+            name = "use-standard-baudrate";
+            dtsText = ''
+              /dts-v1/;
+              /plugin/;
+
+              / {
+                compatible = "rockchip,rk3588s";
+              };
+
+              &{/chosen} {
+                stdout-path = "serial2:115200n8";
+              };
+            '';
+          }
+        ];
+      };
+
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem pkg.pname [ "rkbin" ];
+      system.build.firmware = pkgs.uboot-orangepi-5-rk3588s.override {
+        artifacts = [ "u-boot-rockchip-spi.bin" ];
+        extraStructuredConfig = with lib.kernel; {
+          BAUDRATE = freeform 115200; # c'mon rockchip
+          ROCKCHIP_SPI_IMAGE = yes;
+          USE_PREBOOT = yes;
+          PREBOOT = freeform "pci enum; usb start; nvme scan";
+        };
+      };
+    }
+    {
+      custom.server.enable = true;
+      custom.image = {
+        mutableNixStore = true;
+        boot.uefi.enable = true;
+        installer.targetDisk = "/dev/disk/by-path/platform-a41000000.pcie-pci-0004:41:00.0-nvme-1";
+      };
+    }
   ];
-
-  hardware.deviceTree = {
-    enable = true;
-    name = "rockchip/rk3588s-orangepi-5.dtb";
-    overlays = [
-      {
-        name = "use-standard-baudrate";
-        dtsText = ''
-          /dts-v1/;
-          /plugin/;
-
-          / {
-            compatible = "rockchip,rk3588s";
-          };
-
-          &{/chosen} {
-            stdout-path = "serial2:115200n8";
-          };
-        '';
-      }
-    ];
-  };
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem pkg.pname [ "rkbin" ];
-  system.build.firmware = pkgs.uboot-orangepi-5-rk3588s.override {
-    artifacts = [ "u-boot-rockchip-spi.bin" ];
-    extraStructuredConfig = with lib.kernel; {
-      BAUDRATE = freeform 115200; # c'mon rockchip
-      ROCKCHIP_SPI_IMAGE = yes;
-      USE_PREBOOT = yes;
-      PREBOOT = freeform "pci enum; usb start; nvme scan";
-    };
-  };
 }
