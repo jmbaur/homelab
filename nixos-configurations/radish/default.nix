d618413935383f7c221e1915b08b31d6065adb63
Jared Baur <jaredbaur@fastmail.com>
2024-10-02 20:45:57 -0700

    Configure firmware for radish

--- nixos-configurations/radish/default.nix
+++ nixos-configurations/radish/default.nix
@@ -7,11 +7,28 @@
       # requires linux >= 6.8
       hardware.deviceTree.name = "allwinner/sun50i-h618-orangepi-zero3.dtb";
       boot.kernelPackages = pkgs.linuxPackages_latest;
+
+      system.build.firmware = pkgs.uboot-orangepi_zero3.override {
+        extraStructuredConfig = with lib.kernel; {
+          DISTRO_DEFAULTS = unset;
+          BOOTSTD_DEFAULTS = yes;
+          FIT = yes;
+
+          # Allow for using u-boot scripts.
+          BOOTSTD_FULL = yes;
+
+          # Allow for larger than the default 8MiB kernel size
+          SYS_BOOTM_LEN = freeform "0x${lib.toHexString (12 * 1024 * 1024)}"; # 12MiB
+
+          BOOTCOUNT_LIMIT = yes;
+          BOOTCOUNT_ENV = yes;
+        };
+      };
     }
     {
       custom.server.enable = true;
       custom.basicNetwork.enable = true;
-      custom.nativeBuild = true;
+      # custom.nativeBuild = true;
       custom.image = {
         installer.targetDisk = "/dev/disk/by-path/platform-4020000.mmc";
         boot.uboot = {
