6fa561d77c1488cdc1b81e52680c8a7e0a9ebc7b
Jared Baur <jaredbaur@fastmail.com>
2024-11-20 20:11:34 -0800

    Use built-in clatd networkmanager integration

--- nixos-modules/basic-network/default.nix
+++ nixos-modules/basic-network/default.nix
@@ -8,11 +8,6 @@ let
   cfg = config.custom.basicNetwork;
 
   hasWireless = with config.networking.wireless; enable || iwd.enable;
-
-  clatIfaceName = "clat0";
-
-  # Set by some desktop modules (e.g. gnome)
-  isNetworkManager = config.networking.networkmanager.enable;
 in
 {
   options.custom.basicNetwork.enable = lib.mkEnableOption "basic network setup";
@@ -36,144 +31,135 @@ in
           5353 # mDNS
         ];
       }
-      (lib.mkIf (!isNetworkManager) {
-        # TODO(jared): This does not work on newer kernels due to using
-        # dev_base_lock.
-        # boot.extraModulePackages = [ (config.boot.kernelPackages.callPackage ./nat46.nix { }) ];
 
-        networking.useNetworkd = true;
+      (lib.mkIf
+        (
+          # Set by some desktop modules (e.g. gnome)
+          !config.networking.networkmanager.enable
+        )
+        {
+          # TODO(jared): This does not work on newer kernels due to using
+          # dev_base_lock.
+          # boot.extraModulePackages = [ (config.boot.kernelPackages.callPackage ./nat46.nix { }) ];
 
-        systemd.network.wait-online.enable = !hasWireless;
+          networking.useNetworkd = true;
 
-        systemd.network = {
-          enable = true;
-          networks = {
-            "50-wireless" = lib.mkIf hasWireless {
-              DHCP = "yes";
-              matchConfig.Type = "wlan";
-              dhcpV4Config = {
-                UseDomains = "route";
-                RouteMetric = 600;
+          systemd.network.wait-online.enable = !hasWireless;
+
+          systemd.network = {
+            enable = true;
+            networks = {
+              "50-wireless" = lib.mkIf hasWireless {
+                DHCP = "yes";
+                matchConfig.Type = "wlan";
+                dhcpV4Config = {
+                  UseDomains = "route";
+                  RouteMetric = 600;
+                };
+                networkConfig = {
+                  IPv6PrivacyExtensions = !config.custom.server.enable;
+                  IPv6LinkLocalAddressGenerationMode = "stable-privacy";
+                  Domains = "~.";
+                  MulticastDNS = true;
+                };
+                ipv6AcceptRAConfig = {
+                  Token = "prefixstable";
+                  UseDomains = "route";
+                  RouteMetric = 100;
+                  UsePREF64 = true;
+                };
               };
-              networkConfig = {
-                IPv6PrivacyExtensions = !config.custom.server.enable;
-                IPv6LinkLocalAddressGenerationMode = "stable-privacy";
-                Domains = "~.";
-                MulticastDNS = true;
-              };
-              ipv6AcceptRAConfig = {
-                Token = "prefixstable";
-                UseDomains = "route";
-                RouteMetric = 100;
-                UsePREF64 = true;
+              "50-wired" = {
+                DHCP = "yes";
+                matchConfig.Type = "ether";
+                dhcpV4Config = {
+                  UseDomains = "route";
+                  RouteMetric = 100;
+                };
+                networkConfig = {
+                  IPv6PrivacyExtensions = !config.custom.server.enable;
+                  IPv6LinkLocalAddressGenerationMode = "stable-privacy";
+                  Domains = "~.";
+                  MulticastDNS = true;
+                };
+                ipv6AcceptRAConfig = {
+                  Token = "prefixstable";
+                  UseDomains = "route";
+                  RouteMetric = 100;
+                  UsePREF64 = true;
+                };
               };
             };
-            "50-wired" = {
-              DHCP = "yes";
-              matchConfig.Type = "ether";
-              dhcpV4Config = {
-                UseDomains = "route";
-                RouteMetric = 100;
-              };
-              networkConfig = {
-                IPv6PrivacyExtensions = !config.custom.server.enable;
-                IPv6LinkLocalAddressGenerationMode = "stable-privacy";
-                Domains = "~.";
-                MulticastDNS = true;
-              };
-              ipv6AcceptRAConfig = {
-                Token = "prefixstable";
-                UseDomains = "route";
-                RouteMetric = 100;
-                UsePREF64 = true;
-              };
-            };
           };
-        };
 
-        services.networkd-dispatcher = {
-          enable = config.services.clatd.enable;
-          rules.restart-clatd = {
-            onState = [
-              "routable"
-              "off"
-            ];
-            script = ''
-              #!${pkgs.runtimeShell}
-              if [[ $IFACE != "${clatIfaceName}" ]]; then
-                systemctl restart clatd
-              fi
-            '';
-          };
-        };
-
-        systemd.services.tinyclatd = {
-          # TODO(jared): finish and enable
-          enable = false;
-
-          description = "464XLAT CLAT daemon";
-          documentation = [ "man:clatd(8)" ];
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network-online.target" ];
-          wants = [ "network-online.target" ];
-          startLimitIntervalSec = 0;
-
-          serviceConfig = {
-            ExecStart = lib.getExe pkgs.tinyclatd;
-
-            # Hardening
-            CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
-            LockPersonality = true;
-            MemoryDenyWriteExecute = true;
-            NoNewPrivileges = true;
-            PrivateTmp = true;
-            ProtectClock = true;
-            ProtectControlGroups = true;
-            ProtectHome = true;
-            ProtectHostname = true;
-            ProtectKernelLogs = true;
-            ProtectKernelModules = true;
-            ProtectProc = "invisible";
-            ProtectSystem = true;
-            RestrictAddressFamilies = [
-              "AF_INET"
-              "AF_INET6"
-              "AF_NETLINK"
-            ];
-            RestrictNamespaces = true;
-            RestrictRealtime = true;
-            RestrictSUIDSGID = true;
-            SystemCallArchitectures = "native";
-            SystemCallFilter = [
-              "@network-io"
-              "@system-service"
-              "~@privileged"
-              "~@resources"
-            ];
-          };
-        };
-      })
-      (lib.mkIf isNetworkManager {
-        networking.networkmanager = {
-          dns = "systemd-resolved";
-          dispatcherScripts = [
-            {
-              type = "basic";
-              # Adapted from https://github.com/toreanderson/clatd/blob/04062b282dfa55ea123b94c6d354525fe6670324/scripts/clatd.networkmanager
-              source = pkgs.writeShellScript "clatd-dispatch-script" ''
-                [ "$DEVICE_IFACE" = "${clatIfaceName}" ] && exit 0
-                [ "$2" != "up" ] && [ "$2" != "down" ] && exit 0
-                ${lib.getExe' config.systemd.package "systemctl"} --no-block restart clatd.service
+          services.networkd-dispatcher = {
+            enable = config.services.clatd.enable;
+            rules.restart-clatd = {
+              onState = [
+                "routable"
+                "off"
+              ];
+              script = ''
+                #!${pkgs.runtimeShell}
+                if [[ $IFACE != "${config.services.clatd.settings.clat-dev or "clat"}" ]]; then
+                  systemctl restart clatd
+                fi
               '';
-            }
-          ];
-        };
-      })
+            };
+          };
+
+          systemd.services.tinyclatd = {
+            # TODO(jared): finish and enable
+            enable = false;
+
+            description = "464XLAT CLAT daemon";
+            documentation = [ "man:clatd(8)" ];
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network-online.target" ];
+            wants = [ "network-online.target" ];
+            startLimitIntervalSec = 0;
+
+            serviceConfig = {
+              ExecStart = lib.getExe pkgs.tinyclatd;
+
+              # Hardening
+              CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
+              LockPersonality = true;
+              MemoryDenyWriteExecute = true;
+              NoNewPrivileges = true;
+              PrivateTmp = true;
+              ProtectClock = true;
+              ProtectControlGroups = true;
+              ProtectHome = true;
+              ProtectHostname = true;
+              ProtectKernelLogs = true;
+              ProtectKernelModules = true;
+              ProtectProc = "invisible";
+              ProtectSystem = true;
+              RestrictAddressFamilies = [
+                "AF_INET"
+                "AF_INET6"
+                "AF_NETLINK"
+              ];
+              RestrictNamespaces = true;
+              RestrictRealtime = true;
+              RestrictSUIDSGID = true;
+              SystemCallArchitectures = "native";
+              SystemCallFilter = [
+                "@network-io"
+                "@system-service"
+                "~@privileged"
+                "~@resources"
+              ];
+            };
+          };
+        }
+      )
       (lib.mkIf (!config.custom.server.enable) {
         services.clatd = {
           enable = true;
           settings = {
-            clat-dev = clatIfaceName;
+            clat-dev = "clat0";
             # NOTE: Perl's Net::DNS resolver does not seem to work well querying
             # for AAAA records to systemd-resolved's default IPv4 bind address
             # (127.0.0.53), so we add an IPv6 listener address to systemd-resolved
@@ -183,7 +169,7 @@ in
         };
 
         systemd.network.networks."50-clatd" = {
-          matchConfig.Name = clatIfaceName;
+          matchConfig.Name = config.services.clatd.settings.clat-dev or "clat";
           linkConfig = {
             Unmanaged = true;
             ActivationPolicy = "manual";
