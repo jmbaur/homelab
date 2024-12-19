1ec57999b33a64efdd3a0681aa2925d8d3483d79
Jared Baur <jaredbaur@fastmail.com>
2024-12-19 09:58:12 -0800

    Fix wrapping in markdown files

--- overlays/pkgs/jared-neovim/settings/ftplugin/markdown.lua
+++ overlays/pkgs/jared-neovim/settings/ftplugin/markdown.lua
@@ -1,2 +1,6 @@
 vim.opt_local.wrap = true
-vim.opt_local.wrapmargin = 120
+vim.opt_local.linebreak = true
+vim.keymap.set({ "n", "v" }, "j", "gj", { buffer = true })
+vim.keymap.set({ "n", "v" }, "<Down>", "g<Down>", { buffer = true })
+vim.keymap.set({ "n", "v" }, "k", "gk", { buffer = true })
+vim.keymap.set({ "n", "v" }, "<Up>", "g<Up>", { buffer = true })
