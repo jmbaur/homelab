4e551a10f80e7eee2b7661a343db9dbfd712932a
Jared Baur <jaredbaur@fastmail.com>
2024-12-24 20:55:05 -0800

    Make links in markdown files look better in neovim

--- overlays/pkgs/jared-neovim/settings/ftplugin/markdown.lua
+++ overlays/pkgs/jared-neovim/settings/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.showbreak = '+++ '
+vim.opt_local.conceallevel = 2 -- make links look nice
 vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, '<Down>', 'g<Down>', { buffer = true })
 vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { buffer = true })
