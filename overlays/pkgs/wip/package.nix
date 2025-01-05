6beab35e0dd9568997336875e2a324e5728fa576
Jared Baur <jaredbaur@fastmail.com>
2025-01-05 13:49:33 -0800

    Make wip not push

--- overlays/pkgs/wip/package.nix
+++ overlays/pkgs/wip/package.nix
@@ -1,18 +1,9 @@
-{ writeShellApplication, gitMinimal }:
+{ writeShellApplication, git }:
+
 writeShellApplication {
   name = "wip";
-  runtimeInputs = [ gitMinimal ];
+  runtimeInputs = [ git ];
   text = ''
-    remote=origin
-
-    for possible_remote in jmbaur jbaur; do
-    	if git config "remote.$possible_remote.url" >/dev/null; then
-    		remote=$possible_remote
-    		break
-    	fi
-    done
-
     git commit --no-verify --no-gpg-sign --all --message "WIP"
-    git push "$remote"
   '';
 }
