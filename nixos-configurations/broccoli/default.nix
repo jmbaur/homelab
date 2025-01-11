df5bbadea829f000e8c47f10705a783c02da3563
Jared Baur <jaredbaur@fastmail.com>
2025-01-11 12:59:40 -0800

    Simplify broccoli config

--- nixos-configurations/broccoli/default.nix
+++ nixos-configurations/broccoli/default.nix
@@ -1,22 +1,10 @@
-{ lib, ... }:
+{ ... }:
 {
-  config = lib.mkMerge [
-    {
-      hardware.blackrock.enable = true;
-      custom.desktop.enable = false;
-      custom.dev.enable = true;
-      custom.basicNetwork.enable = true;
-      custom.normalUser.enable = false;
-      custom.image = {
-        boot.uefi.enable = true;
-        installer.targetDisk = "/dev/nvme0n1";
-      };
-    }
-    # TODO(jared): delete this
-    {
-      users.users.root.initialPassword = "root";
-      services.homed.enable = true;
-      networking.wireless.iwd.enable = true;
-    }
-  ];
+  hardware.blackrock.enable = true;
+  custom.desktop.enable = false;
+  custom.dev.enable = true;
+  custom.image = {
+    boot.uefi.enable = true;
+    installer.targetDisk = "/dev/nvme0n1";
+  };
 }
