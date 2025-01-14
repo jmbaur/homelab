683f95fe4e02fd8e8d80b4431cf3c1f1cd565900
Jared Baur <jaredbaur@fastmail.com>
2025-01-13 20:27:46 -0800

    Build, sign, and push in CI

--- apps/default.nix
+++ apps/default.nix
@@ -1,8 +1,18 @@
 inputs:
+
 inputs.nixpkgs.lib.mapAttrs (
   _: pkgs:
+
   let
     inherit (pkgs) lib;
+
+    inherit (lib)
+      attrNames
+      concatLines
+      filterAttrs
+      getExe
+      getExe'
+      ;
 
     mkApp = script: {
       type = "app";
@@ -20,18 +30,18 @@ inputs.nixpkgs.lib.mapAttrs (
       pkgs.writeShellScript "setup-yubikey" ''
         set -o errexit
         echo "enabling openpgp"
-        ${lib.getExe pkgs.yubikey-manager} config usb --enable openpgp
+        ${getExe pkgs.yubikey-manager} config usb --enable openpgp
         echo "setting cache for openpgp touches"
-        ${lib.getExe pkgs.yubikey-manager} openpgp keys set-touch sig cached-fixed
+        ${getExe pkgs.yubikey-manager} openpgp keys set-touch sig cached-fixed
         echo "changing openpgp pin (default admin pin 12345678, default pin 123456)"
-        ${lib.getExe pkgs.yubikey-manager} openpgp access change-admin-pin
-        ${lib.getExe pkgs.yubikey-manager} openpgp access change-pin
+        ${getExe pkgs.yubikey-manager} openpgp access change-admin-pin
+        ${getExe pkgs.yubikey-manager} openpgp access change-pin
         echo "enabling fido2"
-        ${lib.getExe pkgs.yubikey-manager} config usb --enable fido2
+        ${getExe pkgs.yubikey-manager} config usb --enable fido2
         echo "changing fido2 pin (default pin 123456)"
-        ${lib.getExe pkgs.yubikey-manager} fido access change-pin
+        ${getExe pkgs.yubikey-manager} fido access change-pin
         echo "adding ssh key backed with fido2"
-        ${lib.getExe' pkgs.openssh "ssh-keygen"} -t ed25519-sk -O resident
+        ${getExe' pkgs.openssh "ssh-keygen"} -t ed25519-sk -O resident
       ''
     );
 
@@ -61,22 +71,23 @@ inputs.nixpkgs.lib.mapAttrs (
             %commit
             %echo done
           EOF
-          ${lib.getExe' pkgs.gnupg "gpg"} --batch --generate-key sysupdate
-          ${lib.getExe' pkgs.gnupg "gpg"} --export jared@update.jmbaur.com -a >pubkey.gpg
+          ${getExe' pkgs.gnupg "gpg"} --batch --generate-key sysupdate
+          ${getExe' pkgs.gnupg "gpg"} --export jared@update.jmbaur.com -a >pubkey.gpg
           popd
           echo "key generated at $key_dir"
         ''
     );
 
     updateRepoDependencies = mkApp (
-      lib.getExe (
+      getExe (
         pkgs.writeShellApplication {
           name = "update-repo-dependencies";
-          runtimeInputs = with pkgs; [
-            jq
-            nix-prefetch-scripts
+          runtimeInputs = [
+            pkgs.jq
+            pkgs.nix-prefetch-scripts
           ];
-          text = # bash
+          text =
+            # bash
             ''
               NIX_PATH="nixpkgs=$(nix flake prefetch nixpkgs --json | jq --raw-output '.storePath')"
               export NIX_PATH
@@ -105,5 +116,48 @@ inputs.nixpkgs.lib.mapAttrs (
       )
     );
 
+    ci = mkApp (
+      getExe (
+        pkgs.writeShellApplication {
+          name = "homelab-ci";
+          runtimeInputs = [ ];
+          text =
+            ''
+              signing_key=''${1:-}
+
+              if [[ -z $signing_key ]]; then
+                echo "no signing key"
+                exit 1
+              fi
+            ''
+            + concatLines (
+              map
+                (
+                  name:
+                  let
+                    substituter = "s3://cache?region=auto&scheme=https&endpoint=34455c79130a7a7a9495dc2123622e59.r2.cloudflarestorage.com";
+                  in
+                  #bash
+                  ''
+                    toplevel=$(nix build --print-build-logs --no-link --print-out-paths .#nixosConfigurations.${name}.config.system.build.toplevel)
+                    nix-store --query --requisites "$toplevel" >requisites
+                    nix store sign --key-file "$signing_key" --verbose <requisites
+                    nix copy --to "${substituter}" --stdin --verbose <requisites
+                  ''
+                )
+                (
+                  attrNames (
+                    filterAttrs (
+                      name: _:
+                      # Allow-list for machines we want to build in CI.
+                      # TODO(jared): Build _all_ of them.
+                      (builtins.elem name [ "celery" ])
+                    ) inputs.self.nixosConfigurations
+                  )
+                )
+            );
+        }
+      )
+    );
   }
 ) inputs.self.legacyPackages
