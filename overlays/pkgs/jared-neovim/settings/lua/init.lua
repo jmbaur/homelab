3bc926a586fc4eed11cc770581418a5366c8a8ea
Jared Baur <jaredbaur@fastmail.com>
2025-01-17 20:17:20 -0800

    Make sure outer MANPAGER environment variable doesn't conflict with...
    ...running man in nvim terminals.

--- overlays/pkgs/jared-neovim/settings/lua/init.lua
+++ overlays/pkgs/jared-neovim/settings/lua/init.lua
@@ -2,39 +2,34 @@ vim.loader.enable()
 
 vim.g.mapleader = vim.api.nvim_replace_termcodes("<Space>", true, true, true)
 
--- If not using nvim's remote UI, this means we are using neovim in such a way
--- that we can take advantage of neovim front-end related features.
-if #vim.api.nvim_list_uis() > 0 then
-	require("jmbaur.clipboard")
-	require("jmbaur.color")
-	require("jmbaur.compile")
-	require("jmbaur.filemanager")
-	require("jmbaur.git")
-	require("jmbaur.gzip")
-	require("jmbaur.launcher")
-	require("jmbaur.lsp")
-	require("jmbaur.project")
-	require("jmbaur.readline")
-	require("jmbaur.run")
-	require("jmbaur.rust")
-	require("jmbaur.sessions")
-	require("jmbaur.snippets")
-	require("jmbaur.terminal")
-	require("jmbaur.treesitter")
-	require("mini.trailspace").setup()
-
-	vim.opt.belloff = "all"
-	vim.opt.colorcolumn = "80"
-	vim.opt.foldmethod = "marker"
-	vim.opt.title = true
-end
-
+require("jmbaur.clipboard")
+require("jmbaur.color")
+require("jmbaur.compile")
+require("jmbaur.filemanager")
+require("jmbaur.git")
+require("jmbaur.gzip")
+require("jmbaur.launcher")
+require("jmbaur.lsp")
+require("jmbaur.project")
+require("jmbaur.readline")
+require("jmbaur.run")
+require("jmbaur.rust")
+require("jmbaur.sessions")
+require("jmbaur.snippets")
+require("jmbaur.terminal")
+require("jmbaur.treesitter")
+require("mini.trailspace").setup()
 require("mini.bracketed").setup()
 require("mini.surround").setup()
 require("mini.basics").setup({
 	mappings = { basic = false },
 	autocommands = { relnum_in_visual_mode = true },
 })
+
+vim.opt.belloff = "all"
+vim.opt.colorcolumn = "80"
+vim.opt.foldmethod = "marker"
+vim.opt.title = true
 
 vim.opt.shell = "/bin/sh" -- should exist everywhere
 vim.opt.showmatch = true
@@ -43,3 +38,7 @@ vim.opt.showmatch = true
 -- NOTE: This must come after setup for `mini.basics`.
 vim.opt.cursorline = false
 vim.opt.smartindent = false
+
+-- Ensure that if nvim is used as MANPAGER, that we don't launch a nested nvim
+-- when using man in nvim terminals.
+vim.env.MANPAGER = nil
