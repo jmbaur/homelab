43ae813e9bf079edb08973f4c60c7260f82cc82e
Jared Baur <jaredbaur@fastmail.com>
2025-01-20 02:01:09 -0800

    Fix case when neighbor is failed

--- overlays/pkgs/mac-vendor-lookup/mac-vendor-lookup.bash
+++ overlays/pkgs/mac-vendor-lookup/mac-vendor-lookup.bash
@@ -27,7 +27,9 @@ while IFS=' ' read -r oui description; do
 	vendors[$oui]=$description
 done < <(gzip --decompress <"$manuf_location" | grep '^[^#]' | awk '{$2 = ""; print $0;}')
 
-while IFS=' ' read -r _addr _dev_literal dev _mac_literal mac _maybe_type _maybe_status; do
+while IFS=' ' read -r _addr _dev_literal dev maybe_status mac _foo _bar; do
+	if [[ $maybe_status == "FAILED" ]]; then continue; fi
+
 	# shellcheck disable=SC2001
 	oui=$(sed 's,\([0-9A-F]\{2\}:[0-9A-F]\{2\}:[0-9A-F]\{2\}\).*,\1,' <<<"${mac^^}")
 	vendor=${vendors[$oui]:-}
