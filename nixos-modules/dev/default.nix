ce0d5deb06920b8866aef14f20b7cd2d6c994868
Jared Baur <jaredbaur@fastmail.com>
2025-01-23 20:54:36 -0800

    Enable binfmt for dev machines, just not for nix builds!!

--- nixos-modules/dev/default.nix
+++ nixos-modules/dev/default.nix
@@ -23,5 +23,30 @@ in
     programs.ssh.startAgent = lib.mkDefault true;
     programs.gnupg.agent.enable = lib.mkDefault true;
     services.pcscd.enable = config.custom.desktop.enable;
+
+    boot.binfmt = {
+      # Make sure builder isn't masquerading as being
+      # able to do native builds for non-native
+      # architectures.
+      addEmulatedSystemsToNixSandbox = false;
+
+      # Makes chroot/sandbox environments of
+      # different architectures work.
+      preferStaticEmulators = true;
+
+      emulatedSystems =
+        lib.optionals pkgs.stdenv.hostPlatform.isAarch64 [
+          "riscv32-linux"
+          "riscv64-linux"
+          "i686-linux"
+          "x86_64-linux"
+        ]
+        ++ lib.optionals pkgs.stdenv.hostPlatform.isx86_64 [
+          "riscv32-linux"
+          "riscv64-linux"
+          "armv7l-linux"
+          "aarch64-linux"
+        ];
+    };
   };
 }
