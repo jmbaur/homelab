4af6c12feaeaea99ede139f6b79d01e4bf2fec49
Jared Baur <jaredbaur@fastmail.com>
2025-01-25 08:28:56 -0800

    Add broccoli ip

--- nixos-configurations/network.nix
+++ nixos-configurations/network.nix
@@ -1,10 +1,11 @@
 {
   custom.yggdrasil.nodes = {
-    squash.ip = "200:f01b:c880:839b:52:c41b:2c0d:5757";
+    artichoke.ip = "200::"; # TODO
+    broccoli.ip = "200:b07:f009:2954:83bc:1ac4:c497:5b3f";
     cauliflower.ip = "200:3fd3:128f:89ae:7509:a3a1:ec3c:f5b8";
-    pumpkin.ip = "201:5c00:65be:ef51:f30d:7742:6053:1ab3";
-    artichoke.ip = "200::";
     potato.ip = "200:39c8:70a3:d484:2390:f7c5:f1cc:976e";
+    pumpkin.ip = "201:5c00:65be:ef51:f30d:7742:6053:1ab3";
+    squash.ip = "200:f01b:c880:839b:52:c41b:2c0d:5757";
     c445b61a92026767f4f915eb1af73b70eb4211c63fe72abb068cb4a0f21eb0ca.ip = "200:7774:93ca:dbfb:3130:160d:d429:ca11";
   };
 }
