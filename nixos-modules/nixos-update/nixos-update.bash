c38fd5ece639783b45e104b88b494c33d5ea3b36
Jared Baur <jaredbaur@fastmail.com>
2025-01-24 18:08:38 -0800

    Add some logging to nixos-update

--- nixos-modules/nixos-update/nixos-update.bash
+++ nixos-modules/nixos-update/nixos-update.bash
@@ -4,9 +4,14 @@ update_endpoint=$1
 
 read -ra keys < <(nix --experimental-features nix-command config show trusted-public-keys)
 
-new_toplevel=$(curl --silent --fail "$update_endpoint")
-new_toplevel_sig=$(curl --silent --fail "${update_endpoint}.sig")
-nix-key verify <(echo -n "$new_toplevel") <(echo -n "$new_toplevel_sig") "${keys[@]}"
+new_toplevel=$(curl --silent --fail --write-out="%{stderr}request for toplevel path returned with status %{http_code}\n" "$update_endpoint")
+new_toplevel_sig=$(curl --silent --fail --write-out="%{stderr}request for toplevel path signature returned with status %{http_code}\n" "${update_endpoint}.sig")
+
+if ! nix-key verify <(echo -n "$new_toplevel") <(echo -n "$new_toplevel_sig") "${keys[@]}"; then
+	echo "verification of toplevel path failed"
+	echo "used path '${new_toplevel}' and signature '${new_toplevel_sig}'"
+	exit 1
+fi
 
 if [[ $(readlink --canonicalize /run/current-system) != "$new_toplevel" ]]; then
 	nix-store --realise "$new_toplevel"
