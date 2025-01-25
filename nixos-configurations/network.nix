5f6e6138a974d02d9ebba1d2fea3c6ac1c06835a
Jared Baur <jaredbaur@fastmail.com>
2025-01-25 02:19:51 -0800

    Add potato's real IP

--- nixos-configurations/network.nix
+++ nixos-configurations/network.nix
@@ -4,7 +4,7 @@
     cauliflower.ip = "200:3fd3:128f:89ae:7509:a3a1:ec3c:f5b8";
     pumpkin.ip = "200::";
     artichoke.ip = "200::";
-    potato.ip = "200::";
+    potato.ip = "200:39c8:70a3:d484:2390:f7c5:f1cc:976e";
     c445b61a92026767f4f915eb1af73b70eb4211c63fe72abb068cb4a0f21eb0ca.ip = "200:7774:93ca:dbfb:3130:160d:d429:ca11";
   };
 }
