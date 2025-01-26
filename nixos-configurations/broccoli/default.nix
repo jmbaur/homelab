5be69aa2fd6f677d4bae7422af6de99670fc9f68
Jared Baur <jaredbaur@fastmail.com>
2025-01-26 13:04:53 -0800

    Disable pd-mapper on broccoli

--- nixos-configurations/broccoli/default.nix
+++ nixos-configurations/broccoli/default.nix
@@ -5,4 +5,7 @@
   custom.common.nativeBuild = true;
   custom.dev.enable = true;
   custom.recovery.targetDisk = "/dev/disk/by-path/platform-1c20000.pcie-pci-0002:01:00.0-nvme-1";
+
+  # TODO(jared): doesn't work on wdk2023?
+  systemd.services.pd-mapper.enable = false;
 }
