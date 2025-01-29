4d968ff793eb0c4a40f0c16d783688ecdd9c5349
Jared Baur <jaredbaur@fastmail.com>
2025-01-29 09:17:18 -0800

    Fix checks

--- checks/default.nix
+++ checks/default.nix
@@ -1,17 +1,15 @@
 inputs:
 
-# The tests import self.nixosModules.default, which sets the overlay at
-# self.overlays.default, so we don't reuse the pkgs from legacyPackages and
-# instead use pkgs from nixpkgs directly.
-builtins.listToAttrs (
-  map (system: {
-    name = system;
-    value =
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in
-      {
-        installationLifecycle = pkgs.callPackage ./installation-lifecycle.nix { inherit inputs; };
-      };
-  }) (builtins.attrNames inputs.self.legacyPackages)
-)
+let
+  inherit (inputs.nixpkgs.lib) filterAttrs mapAttrs;
+in
+mapAttrs (
+  system: _:
+  (mapAttrs
+    # The tests import self.nixosModules.default, which sets the overlay at
+    # self.overlays.default, so we don't reuse the pkgs from legacyPackages and
+    # instead use pkgs from nixpkgs directly.
+    (name: _: inputs.nixpkgs.legacyPackages.${system}.callPackage ./${name} { inherit inputs; })
+    (filterAttrs (_: type: type == "directory") (builtins.readDir ./.))
+  )
+) inputs.self.legacyPackages
