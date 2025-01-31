c9a664bc343fe5f11564ed6d23366841e8d1a3b7
Jared Baur <jaredbaur@fastmail.com>
2025-01-30 20:18:14 -0800

    Add some basics for pea

--- nixos-configurations/pea/default.nix
+++ nixos-configurations/pea/default.nix
@@ -1,66 +1,85 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
 
+let
+  inherit (lib)
+    mkMerge
+    ;
+in
 {
-  hardware.chromebook.asurada-spherion.enable = true;
-
-  system.build.testImage = pkgs.callPackage (
+  config = mkMerge [
     {
-      dtc,
-      runCommand,
-      ubootTools,
-      util-linux,
-      vboot_reference,
-      xz,
-      zstd,
-    }:
+      hardware.chromebook.asurada-spherion.enable = true;
 
-    runCommand "test-image"
-      {
-        nativeBuildInputs = [
-          dtc
-          ubootTools
-          util-linux
-          vboot_reference
-          xz
-          zstd
-        ];
-      }
-      ''
-        lzma --threads $NIX_BUILD_CORES <${config.system.build.kernel}/${config.system.boot.loader.kernelFile} >kernel.lzma
-        cp ${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile} initrd
-        cp ${config.hardware.deviceTree.package}/${config.hardware.deviceTree.name} fdt
+      system.build.testImage = pkgs.callPackage (
+        {
+          dtc,
+          runCommand,
+          ubootTools,
+          util-linux,
+          vboot_reference,
+          xz,
+          zstd,
+        }:
 
-        cp ${./fitimage.its} fitimage.its # needs to be in the same directory
-        mkimage -D "-I dts -O dtb -p 2048" -f fitimage.its vmlinux.uimg
+        runCommand "test-image"
+          {
+            nativeBuildInputs = [
+              dtc
+              ubootTools
+              util-linux
+              vboot_reference
+              xz
+              zstd
+            ];
+          }
+          ''
+            mkdir -p $out
 
-        dd if=/dev/zero of=bootloader.bin bs=512 count=1
+            lzma --threads $NIX_BUILD_CORES <${config.system.build.kernel}/${config.system.boot.loader.kernelFile} >kernel.lzma
+            cp ${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile} initrd
+            cp ${config.hardware.deviceTree.package}/${config.hardware.deviceTree.name} fdt
 
-        echo "init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}" >kernel-params
+            cp ${./fitimage.its} fitimage.its # needs to be in the same directory
+            mkimage -D "-I dts -O dtb -p 2048" -f fitimage.its vmlinux.uimg
 
-        futility vbutil_kernel \
-          --pack kpart \
-          --version 1 \
-          --vmlinuz vmlinux.uimg \
-          --arch aarch64 \
-          --keyblock ${vboot_reference}/share/vboot/devkeys/kernel.keyblock \
-          --signprivate ${vboot_reference}/share/vboot/devkeys/kernel_data_key.vbprivk \
-          --config kernel-params \
-          --bootloader bootloader.bin
+            dd status=none if=/dev/zero of=bootloader.bin bs=512 count=1
 
-        dd if=/dev/zero of=image bs=4M count=20
-        sfdisk --no-reread --no-tell-kernel image <<EOF
-            label: gpt
-            label-id: A8ABB0FA-2FD7-4FB8-ABB0-2EEB7CD66AFA
-            size=64m, type=FE3A2A5D-4F32-41A7-B725-ACCC3285A309, uuid=534078AF-3BB4-EC43-B6C7-828FB9A788C6, name=kernel
-        EOF
-        cgpt add -i 1 -S 1 -T 5 -P 10 image
-        eval "$(partx image -o START,SECTORS --nr 1 --pairs)"
-        dd conv=notrunc if=kpart of=image seek="$START" count="$SECTORS"
-        zstd -T$NIX_BUILD_CORES -o $out image
-      ''
-  ) { };
+            echo "init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}" >kernel-params
+
+            futility vbutil_kernel \
+              --pack $out/kpart \
+              --version 1 \
+              --vmlinuz vmlinux.uimg \
+              --arch aarch64 \
+              --keyblock ${vboot_reference}/share/vboot/devkeys/kernel.keyblock \
+              --signprivate ${vboot_reference}/share/vboot/devkeys/kernel_data_key.vbprivk \
+              --config kernel-params \
+              --bootloader bootloader.bin
+
+            dd if=/dev/zero of=$out/image bs=4M count=20
+            sfdisk --no-reread --no-tell-kernel $out/image <<EOF
+                label: gpt
+                label-id: A8ABB0FA-2FD7-4FB8-ABB0-2EEB7CD66AFA
+                size=64m, type=FE3A2A5D-4F32-41A7-B725-ACCC3285A309, uuid=534078AF-3BB4-EC43-B6C7-828FB9A788C6, name=kernel
+            EOF
+            cgpt add -i 1 -S 1 -T 5 -P 10 $out/image
+            eval "$(partx $out/image -o START,SECTORS --nr 1 --pairs)"
+            dd conv=notrunc if=$out/kpart of=$out/image seek="$START" count="$SECTORS"
+            zstd -T$NIX_BUILD_CORES --rm $out/image
+          ''
+      ) { };
+    }
+    {
+      # programs.sway.enable = true;
+
+      networking.wireless.iwd.enable = true;
+      custom.normalUser.enable = true;
+      custom.basicNetwork.enable = true;
+    }
+  ];
 }
--- nixos-modules/desktop/default.nix
+++ nixos-modules/desktop/default.nix
@@ -6,6 +6,20 @@
 }:
 
 let
+  inherit (lib)
+    escape
+    escapeShellArg
+    flatten
+    getExe
+    getExe'
+    gvariant
+    mkDefault
+    mkEnableOption
+    mkForce
+    mkIf
+    optionals
+    ;
+
   cfg = config.custom.desktop;
 
   xcursorSize = 24;
@@ -13,7 +27,7 @@ let
 
   backgroundColor = "1f3023";
 
-  lockCmd = "${lib.getExe pkgs.swaylock} --daemonize --show-failed-attempts --color=${backgroundColor}";
+  lockCmd = "${getExe pkgs.swaylock} --daemonize --show-failed-attempts --color=${backgroundColor}";
 
   # For xwayland applications
   defaultIconTheme = pkgs.runCommand "default-icon-theme" { } ''
@@ -29,9 +43,9 @@ let
   };
 in
 {
-  options.custom.desktop.enable = lib.mkEnableOption "desktop";
+  options.custom.desktop.enable = mkEnableOption "desktop";
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     custom.normalUser.enable = true;
     custom.basicNetwork.enable = true;
 
@@ -50,7 +64,7 @@ in
     };
 
     # Override the upstream defaults
-    environment.etc."sway/config".source = lib.mkForce (
+    environment.etc."sway/config".source = mkForce (
       pkgs.substituteAll {
         name = "sway.conf";
         src = ./sway.conf.in;
@@ -61,7 +75,7 @@ in
         xkbVariant = config.services.xserver.xkb.variant;
 
         postInstall = ''
-          if grep --silent ${lib.escape [ "/" ] builtins.storeDir} $target; then
+          if grep --silent ${escape [ "/" ] builtins.storeDir} $target; then
             echo "The default config should be able to be copied without any"
             echo "reliance on nix store paths, since these paths may not"
             echo "always exist (e.g. after garbage collection)."
@@ -112,37 +126,35 @@ in
       serviceConfig = {
         Restart = "on-failure";
         RestartSec = 3;
-        ExecStart = toString (
-          lib.flatten [
-            (lib.getExe pkgs.swayidle)
-            "-w"
-            [
-              "timeout"
-              "600"
-              (lib.escapeShellArg "loginctl lock-session")
-            ]
-            [
-              "timeout"
-              "900"
-              (lib.escapeShellArg "swaymsg 'output * power off'")
-              "resume"
-              (lib.escapeShellArg "swaymsg 'output * power on'")
-            ]
-            [
-              "timeout"
-              "1200"
-              (lib.escapeShellArg "systemctl suspend")
-            ]
-            [
-              "before-sleep"
-              (lib.escapeShellArg lockCmd)
-            ]
-            [
-              "lock"
-              (lib.escapeShellArg lockCmd)
-            ]
+        ExecStart = toString (flatten [
+          (getExe pkgs.swayidle)
+          "-w"
+          [
+            "timeout"
+            "600"
+            (escapeShellArg "loginctl lock-session")
           ]
-        );
+          [
+            "timeout"
+            "900"
+            (escapeShellArg "swaymsg 'output * power off'")
+            "resume"
+            (escapeShellArg "swaymsg 'output * power on'")
+          ]
+          [
+            "timeout"
+            "1200"
+            (escapeShellArg "systemctl suspend")
+          ]
+          [
+            "before-sleep"
+            (escapeShellArg lockCmd)
+          ]
+          [
+            "lock"
+            (escapeShellArg lockCmd)
+          ]
+        ]);
       };
     };
 
@@ -154,7 +166,7 @@ in
       serviceConfig = {
         Restart = "on-failure";
         RestartSec = 3;
-        ExecStart = "${lib.getExe' pkgs.wl-clipboard "wl-paste"} --type text --watch ${lib.getExe pkgs.clipman} store";
+        ExecStart = "${getExe' pkgs.wl-clipboard "wl-paste"} --type text --watch ${getExe pkgs.clipman} store";
       };
     };
 
@@ -172,7 +184,7 @@ in
         Restart = "on-failure";
         RestartSec = 3;
         ExecStart = toString [
-          (lib.getExe pkgs.gammastep)
+          (getExe pkgs.gammastep)
           "-l"
           "geoclue2"
         ];
@@ -199,11 +211,11 @@ in
       serviceConfig = {
         Restart = "on-failure";
         RestartSec = 3;
-        ExecStart = lib.getExe pkgs.kanshi;
+        ExecStart = getExe pkgs.kanshi;
       };
     };
 
-    system.userActivationScripts.xdg-user-dirs = lib.getExe' pkgs.xdg-user-dirs "xdg-user-dirs-update";
+    system.userActivationScripts.xdg-user-dirs = getExe' pkgs.xdg-user-dirs "xdg-user-dirs-update";
 
     programs.dconf = {
       enable = true;
@@ -211,7 +223,7 @@ in
         {
           settings = {
             "org/gnome/desktop/interface" = {
-              cursor-size = lib.gvariant.mkInt32 xcursorSize;
+              cursor-size = gvariant.mkInt32 xcursorSize;
               cursor-theme = xcursorTheme;
               gtk-theme = "Adwaita-dark";
             };
@@ -240,7 +252,7 @@ in
       pkgs.swaylock
       pkgs.tinybar
       pkgs.wl-clipboard
-    ] ++ lib.optionals config.services.flatpak.enable [ setupFlathub ];
+    ] ++ optionals config.services.flatpak.enable [ setupFlathub ];
 
     programs.uwsm = {
       enable = true;
@@ -249,19 +261,19 @@ in
 
     services.greetd = {
       enable = true;
-      settings.default_session.command = "${lib.getExe' pkgs.greetd.greetd "agreety"} --cmd \"systemd-cat --identifier=uwsm-sway uwsm start -S -F /run/current-system/sw/bin/sway\"";
+      settings.default_session.command = "${getExe' pkgs.greetd.greetd "agreety"} --cmd \"systemd-cat --identifier=uwsm-sway uwsm start -S -F /run/current-system/sw/bin/sway\"";
     };
 
     programs.yubikey-touch-detector.enable = true;
 
-    services.fwupd.enable = lib.mkDefault true;
+    services.fwupd.enable = mkDefault true;
 
-    hardware.bluetooth.enable = lib.mkDefault true;
-    security.rtkit.enable = lib.mkDefault true;
+    hardware.bluetooth.enable = mkDefault true;
+    security.rtkit.enable = mkDefault true;
 
     services.automatic-timezoned.enable = true;
-    services.upower.enable = lib.mkDefault true;
-    services.power-profiles-daemon.enable = lib.mkDefault true;
+    services.upower.enable = mkDefault true;
+    services.power-profiles-daemon.enable = mkDefault true;
 
     # Add a default browser to use
     programs.firefox = {
@@ -278,6 +290,6 @@ in
     # It would be uncommon for a desktop system to have an NMEA serial device,
     # plus setting this to true means that geoclue will be dependent on avahi
     # being enabled, since NMEA support in geoclue uses avahi.
-    services.geoclue2.enableNmea = lib.mkDefault false;
+    services.geoclue2.enableNmea = mkDefault false;
   };
 }
