381a57a0b8f8616afa1d030f83ef10062fadfabb
Jared Baur <jaredbaur@fastmail.com>
2025-01-30 23:18:02 -0800

    Fix ipv6-link-local-ssh-proxy-command preferring incorrect link

--- overlays/pkgs/ipv6-link-local-ssh-proxy-command/ipv6-link-local-ssh-proxy-command.bash
+++ overlays/pkgs/ipv6-link-local-ssh-proxy-command/ipv6-link-local-ssh-proxy-command.bash
@@ -4,6 +4,24 @@ hostname=$1
 port=$2
 
 ipv6_link_local_address=$(dig +short AAAA "$hostname" | grep '^fe80')
-dev=$(ip -j -6 route get "$ipv6_link_local_address" | jq --raw-output '.[0].dev')
 
-nc "${ipv6_link_local_address}%${dev}" "$port"
+mapfile -t interfaces < <(ip -j -6 neighbor show | jq --raw-output --arg ip "$ipv6_link_local_address" '.[] | select(.dst == $ip) | .dev')
+
+declare -a interface=()
+
+for iface in "${interfaces[@]}"; do
+	route_metric=$(ip -j -6 route get "$ipv6_link_local_address" iif "$iface" | jq --raw-output '.[0].metric')
+	if [[ $route_metric == "null" ]]; then
+		continue
+	fi
+
+	if [[ ${#interface[@]} -eq 0 ]] || [[ ${interface[0]} -gt $route_metric ]]; then
+		interface=("$route_metric" "$iface")
+	fi
+done
+
+if [[ ${#interface[@]} -eq 0 ]]; then
+	exit 1
+fi
+
+nc "${ipv6_link_local_address}%${interface[1]}" "$port"
