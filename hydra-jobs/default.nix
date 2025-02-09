d09990ffc0caad1b44c4934933e6fb14836eee91
Jared Baur <jaredbaur@fastmail.com>
2025-02-09 09:31:37 -0800

    Put nixos configurations in hydrajobs

--- hydra-jobs/default.nix
+++ hydra-jobs/default.nix
@@ -1,4 +1,24 @@
 inputs:
-inputs.nixpkgs.lib.mapAttrs (_: pkgs: {
-  inherit (pkgs) hello;
-}) inputs.self.legacyPackages
+
+let
+  inherit (inputs.nixpkgs.lib)
+    mapAttrs
+    listToAttrs
+    mapAttrsToList
+    zipAttrs
+    ;
+
+  nixosConfigurations = zipAttrs (
+    mapAttrsToList (
+      name:
+      { config, ... }:
+      {
+        "${config.nixpkgs.buildPlatform.system}" = {
+          inherit name;
+          value = config.system.build.toplevel;
+        };
+      }
+    ) inputs.self.nixosConfigurations
+  );
+in
+mapAttrs (system: _: listToAttrs nixosConfigurations.${system}) inputs.self.legacyPackages
