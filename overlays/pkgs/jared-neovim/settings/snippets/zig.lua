b992de6b8e938fd308156b9250e2abcf757b8d8f
Jared Baur <jaredbaur@fastmail.com>
2025-02-10 17:00:26 -0800

    Make std.debug.print output a newline correctly

--- overlays/pkgs/jared-neovim/settings/snippets/zig.lua
+++ overlays/pkgs/jared-neovim/settings/snippets/zig.lua
@@ -4,6 +4,6 @@ for _, fn in ipairs({ "err", "warn", "info", "debug" }) do
 	zig_snippets[fn] = { prefix = fn, body = string.format('@import("std").log.%s("$1={${2:any}}", .{$1});', fn) }
 end
 
-zig_snippets["print"] = { prefix = "print", body = '@import("std").debug.print("$1={${2:any}}\n", .{$1});' }
+zig_snippets["print"] = { prefix = "print", body = '@import("std").debug.print("$1={${2:any}}\\n", .{$1});' }
 
 return zig_snippets
