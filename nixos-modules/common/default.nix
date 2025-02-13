b74dd37ec3eb7bfe93c9bf85c9442eb8304db26e
Jared Baur <jaredbaur@fastmail.com>
2025-02-12 21:46:26 -0800

    nix 2.26 causes eval errors for cross-compiled machines, revert to 2.25

--- nixos-modules/common/default.nix
+++ nixos-modules/common/default.nix
@@ -86,7 +86,7 @@ in
       };
 
       nix = {
-        package = pkgs.nixVersions.nix_2_26;
+        package = pkgs.nixVersions.nix_2_25;
         channel.enable = false; # opt out of nix channels
         distributedBuilds = mkDefault true; # allow for populating /etc/nix/machines for remote building
         settings = {
