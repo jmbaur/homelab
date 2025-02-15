d1be60bb5a0337a8965de617ed1f4a6fe40929f9
Jared Baur <jaredbaur@fastmail.com>
2025-02-15 10:35:53 -0800

    Add rhubarb

--- nixos-configurations/network.nix
+++ nixos-configurations/network.nix
@@ -7,6 +7,7 @@
     pea.ip = "204:821b:a55:3059:8468:78c0:b472:d2bc";
     potato.ip = "200:39c8:70a3:d484:2390:f7c5:f1cc:976e";
     pumpkin.ip = "201:5c00:65be:ef51:f30d:7742:6053:1ab3";
+    rhubarb.ip = "200:161:89b:2b1e:e84b:ea2:37b3:9e26";
     squash.ip = "200:f01b:c880:839b:52:c41b:2c0d:5757";
     zucchini.ip = "200:ed08:2f1c:cdcc:d036:30c6:4b25:e4f2";
     c445b61a92026767f4f915eb1af73b70eb4211c63fe72abb068cb4a0f21eb0ca.ip = "200:7774:93ca:dbfb:3130:160d:d429:ca11";
