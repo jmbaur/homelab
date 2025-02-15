34799f004d07cf3530eb8279f2ec76270ec5c7cc
Jared Baur <jaredbaur@fastmail.com>
2025-02-15 11:24:38 -0800

    Remove non-working bluetooth dtbo (for now)

--- nixos-modules/hardware/rpi4/default.nix
+++ nixos-modules/hardware/rpi4/default.nix
@@ -23,31 +23,6 @@ in
         );
       }
       {
-        hardware.deviceTree = {
-          overlays = [
-            {
-              name = "bluetooth-overlay";
-              dtsText = ''
-                /dts-v1/;
-                /plugin/;
-
-                / {
-                    compatible = "brcm,bcm2711";
-
-                    fragment@0 {
-                        target = <&uart0_pins>;
-                        __overlay__ {
-                                brcm,pins = <30 31 32 33>;
-                                brcm,pull = <2 0 0 2>;
-                        };
-                    };
-                };
-              '';
-            }
-          ];
-        };
-      }
-      {
         hardware.deviceTree.overlays = [
           {
             name = "rpi4-cpu-revision";
