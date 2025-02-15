8186d6538d70eca4083d3b0b6bd3b0bc641b5625
Jared Baur <jaredbaur@fastmail.com>
2025-02-15 10:36:32 -0800

    Refine rhubarb disk path

--- nixos-configurations/rhubarb/default.nix
+++ nixos-configurations/rhubarb/default.nix
@@ -12,7 +12,10 @@
 
       custom.server.enable = true;
       custom.basicNetwork.enable = true;
-      custom.recovery.targetDisk = "/dev/sda"; # TODO(jared): refine this
+
+      # NOTE: This might change depending on which USB port we plug into. This
+      # is the bottom USB3 port.
+      custom.recovery.targetDisk = "/dev/disk/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:2:1.0-scsi-0:0:0:0";
     }
     {
       services.kodi.enable = true;
