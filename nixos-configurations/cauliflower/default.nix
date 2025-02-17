cc451ac0d46fbbf6bc143c0669c7aea32216fced
Jared Baur <jaredbaur@fastmail.com>
2025-02-17 12:50:00 -0800

    Fix default audio profile on cauliflower

--- nixos-configurations/cauliflower/default.nix
+++ nixos-configurations/cauliflower/default.nix
@@ -32,4 +32,26 @@
   custom.dev.enable = true;
   nixpkgs.buildPlatform = config.nixpkgs.hostPlatform;
   custom.recovery.targetDisk = "/dev/disk/by-path/pci-0000:01:00.0-nvme-1";
+
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-pro-audio.conf" ''
+      monitor.alsa.rules = [
+        {
+          matches = [
+            {
+              device.name = "alsa_card.pci-0000_00_1f.3-platform-adl_rt5682_def"
+            }
+          ]
+          actions = {
+            update-props = {
+              device.profile = "pro-audio"
+              priority.session = 600
+              priority.driver = 600
+            }
+          }
+        }
+      ]
+    '')
+  ];
+
 }
