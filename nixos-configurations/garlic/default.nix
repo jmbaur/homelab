cec2fdc8a4a105c7fe57f8f7e72e0dbd9db240de
Jared Baur <jaredbaur@fastmail.com>
2025-02-22 14:39:54 -0800

    Update network

--- nixos-configurations/garlic/default.nix
+++ nixos-configurations/garlic/default.nix
@@ -2,5 +2,5 @@
   hardware.thinkpad-t14s-gen6.enable = true;
   custom.dev.enable = true;
   custom.desktop.enable = true;
-  custom.recovery.targetDisk = "/dev/nvme0n1";
+  custom.recovery.targetDisk = "/dev/disk/by-path/platform-1bf8000.pci-pci-0006:01:00.0-nvme-1";
 }
--- nixos-configurations/network.nix
+++ nixos-configurations/network.nix
@@ -5,12 +5,13 @@
     broccoli.ip = "200:b07:f009:2954:83bc:1ac4:c497:5b3f";
     cauliflower.ip = "200:3fd3:128f:89ae:7509:a3a1:ec3c:f5b8";
     celery.ip = "206:db73:272c:557c:37de:4c83:beb6:8478";
+    garlic.ip = "201:d9e1:ed5:3c5f:d730:1331:1ff8:30e2";
     pea.ip = "204:821b:a55:3059:8468:78c0:b472:d2bc";
     potato.ip = "200:39c8:70a3:d484:2390:f7c5:f1cc:976e";
     pumpkin.ip = "201:5c00:65be:ef51:f30d:7742:6053:1ab3";
     rhubarb.ip = "200:161:89b:2b1e:e84b:ea2:37b3:9e26";
     squash.ip = "200:f01b:c880:839b:52:c41b:2c0d:5757";
-    zucchini.ip = "200:ed08:2f1c:cdcc:d036:30c6:4b25:e4f2";
+    zucchini.ip = "200:59ff:6c49:7614:faa8:b669:24d9:5262";
     c445b61a92026767f4f915eb1af73b70eb4211c63fe72abb068cb4a0f21eb0ca.ip = "200:7774:93ca:dbfb:3130:160d:d429:ca11";
   };
 }
