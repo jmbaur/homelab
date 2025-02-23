4ae40e679dbd57f50b042da1e10513c599481e05
Jared Baur <jaredbaur@fastmail.com>
2025-02-22 22:04:50 -0800

    Add KExecWatchdogSec for servers

--- nixos-modules/server/default.nix
+++ nixos-modules/server/default.nix
@@ -42,19 +42,10 @@
       # that we can hopefully still access it remotely.
       enableEmergencyMode = false;
 
-      # For more detail, see:
-      #   https://0pointer.de/blog/projects/watchdog.html
       watchdog = {
-        # systemd will send a signal to the hardware watchdog at half
-        # the interval defined here, so every 10s.
-        # If the hardware watchdog does not get a signal for 20s,
-        # it will forcefully reboot the system.
         runtimeTime = lib.mkDefault "20s";
-        # Forcefully reboot if the final stage of the reboot
-        # hangs without progress for more than 30s.
-        # For more info, see:
-        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
         rebootTime = lib.mkDefault "30s";
+        kexecTime = lib.mkDefault "30s";
       };
 
       sleep.extraConfig = ''
