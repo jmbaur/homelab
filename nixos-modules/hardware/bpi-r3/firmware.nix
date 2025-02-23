5f38d85b9b5776688cc5d041d1cb1f92b3756dc9
Jared Baur <jaredbaur@fastmail.com>
2025-02-23 01:17:11 -0800

    Update ATF for bpi-r3

--- nixos-modules/hardware/bpi-r3/firmware.nix
+++ nixos-modules/hardware/bpi-r3/firmware.nix
@@ -17,24 +17,39 @@
 let
   atf = buildArmTrustedFirmware rec {
     platform = "mt7986";
+
     version = builtins.substring 0 7 src.rev;
+
     # mt7986 not in upstream ATF yet
     src = fetchFromGitHub {
       owner = "mtk-openwrt";
       repo = "arm-trusted-firmware";
-      rev = "bacca82a8cac369470df052a9d801a0ceb9b74ca";
-      hash = "sha256-n5D3styntdoKpVH+vpAfDkCciRJjCZf9ivrI9eEdyqw=";
+      rev = "e090770684e775711a624e68e0b28112227a4c38";
+      hash = "sha256-VI5OB2nWdXUjkSuUXl/0yQN+/aJp9Jkt+hy7DlL+PMg=";
     };
+
+    strictDeps = true;
+
     nativeBuildInputs = [
       dtc
       openssl
     ];
+
+    patches = [ ];
+
     # bromimage is a pre-built binary for x86_64-linux
     postPatch = ''
       echo -e '#!/bin/sh\n${pkgsCross.gnu64.stdenv.hostPlatform.emulator buildPackages} tools/mediatek/bromimage/bromimage-linux-x86_64 "$@"' >tools/mediatek/bromimage/bromimage
       chmod +x tools/mediatek/bromimage/bromimage
     '';
+
     enableParallelBuilding = true;
+
+    env.NIX_CFLAGS_COMPILE = toString [
+      # Accommodate -flto:
+      "-ffat-lto-objects"
+    ];
+
     extraMakeFlags = [
       "OPENSSL_DIR=${
         symlinkJoin {
@@ -48,13 +63,21 @@ let
       "BL33=${uboot-mt7986a_bpir3_emmc}/u-boot.bin"
       "DRAM_USE_DDR4=1"
       "BOOT_DEVICE=spim-nand" # defines where the FIP image lives
+      "OD=$(OBJDUMP)"
+      "OC=$(OBJCOPY)"
+      # GNU's assembler doesn't recognize the `-x` option, so instead,
+      # use the GNU C compiler, which does recognize it.
+      "HOSTAS=$(CC_FOR_BUILD)"
+      "AS=$(CC)"
       "all"
       "fip"
     ];
+
     filesToInstall = [
       "build/${platform}/release/bl2.img"
       "build/${platform}/release/fip.bin"
     ];
+
     extraMeta.platforms = [ "aarch64-linux" ];
   };
   ubinizeConfig = (formats.ini { }).generate "ubinize.ini" rec {
