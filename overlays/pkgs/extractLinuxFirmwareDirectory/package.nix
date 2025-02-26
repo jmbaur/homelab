5307dc9c464207410b7d5def648d07a6b21b77cc
Jared Baur <jaredbaur@fastmail.com>
2025-02-25 19:49:33 -0800

    Fix broken symlinks in extracted linux firmware

--- overlays/pkgs/extractLinuxFirmwareDirectory/package.nix
+++ overlays/pkgs/extractLinuxFirmwareDirectory/package.nix
@@ -5,4 +5,14 @@ directory:
 runCommand "${directory}-linux-firmware" { } ''
   mkdir -p $out/lib/firmware/${directory}
   cp -r ${linux-firmware}/lib/firmware/${directory}/* $out/lib/firmware/${directory}
+
+  # Fix and fix broken symlinks
+  while read -r symlink; do
+    resolved=$(readlink --canonicalize-missing $symlink)
+    if [[ -f $resolved ]]; then
+      continue
+    fi
+
+    install -Dm0444 ''${resolved/$out/${linux-firmware}} $out''${resolved/$out/}
+  done < <(find $out/lib/firmware -type l)
 ''
