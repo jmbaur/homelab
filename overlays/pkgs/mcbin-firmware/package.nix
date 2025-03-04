b6adc2496fc6efe541f70e933332f7df78d1dd3d
Jared Baur <jaredbaur@fastmail.com>
2025-03-04 07:40:12 -0800

    Fix mcbin-firmware build

--- overlays/pkgs/mcbin-firmware/package.nix
+++ overlays/pkgs/mcbin-firmware/package.nix
@@ -1,9 +1,9 @@
 {
   buildArmTrustedFirmware,
   marvellBinaries,
-  buildPackages,
   uboot-mvebu_mcbin-88f8040,
   mv-ddr-marvell,
+  openssl,
 }:
 
 let
@@ -11,15 +11,17 @@ let
     (buildArmTrustedFirmware rec {
       platform = "a80x0_mcbin";
 
+      patches = [ ./marvell-atf-no-git.patch ];
+
       preBuild = ''
-        cp -r ${mv-ddr-marvell} /tmp/mv_ddr_marvell
-        chmod -R +w /tmp/mv_ddr_marvell
+        cp -r ${mv-ddr-marvell} ./mv_ddr_marvell
+        chmod -R +w ./mv_ddr_marvell
       '';
 
       extraMakeFlags = [
         "SCP_BL2=${marvellBinaries}/mrvl_scp_bl2.img"
         "BL33=${uboot-mvebu_mcbin-88f8040}/u-boot.bin"
-        "MV_DDR_PATH=/tmp/mv_ddr_marvell"
+        "MV_DDR_PATH=./mv_ddr_marvell"
         "LOG_LEVEL=20"
         "MARVELL_SECURE_BOOT=0"
         "all"
@@ -32,8 +34,7 @@ let
       filesToInstall = [ "build/${platform}/release/flash-image.bin" ];
     }).overrideAttrs
       (old: {
-        patches = (old.patches or [ ]) ++ [ ./marvell-atf-no-git.patch ];
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ (with buildPackages; [ openssl ]);
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ openssl ];
       });
 in
 atf
