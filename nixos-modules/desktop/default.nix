96f527ace68f78a5679afbb28a8488ba092c7f82
Jared Baur <jaredbaur@fastmail.com>
2025-03-13 00:25:49 -0700

    Add firefoxpwa

--- nixos-modules/desktop/default.nix
+++ nixos-modules/desktop/default.nix
@@ -7,7 +7,6 @@
 
 let
   inherit (lib)
-    gvariant
     mkDefault
     mkEnableOption
     mkIf
@@ -16,19 +15,9 @@ let
 
   cfg = config.custom.desktop;
 
-  backgroundColor = "1f3023";
-
-  enabledExtensions = [
-    pkgs.gnomeExtensions.appindicator
-    pkgs.gnomeExtensions.caffeine
-    pkgs.gnomeExtensions.clipboard-history
-  ];
-
   setupFlathub = pkgs.writeShellApplication {
     name = "setup-flathub";
-    text = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
+    text = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
   };
 in
 {
@@ -41,32 +30,25 @@ in
     networking.wireless.iwd.enable = true;
     networking.networkmanager.wifi.backend = "iwd";
 
+    services.automatic-timezoned.enable = mkDefault true;
+    services.dbus.packages = [ pkgs.mako ];
     services.flatpak.enable = mkDefault true;
     services.fwupd.enable = mkDefault true;
+    services.power-profiles-daemon.enable = mkDefault true;
     services.printing.enable = mkDefault true;
+    services.upower.enable = mkDefault true;
+
+    programs.sway.enable = true;
+    programs.yubikey-touch-detector.enable = mkDefault true;
+
+    systemd.packages = [ pkgs.mako ];
 
     environment.systemPackages =
       [
-        pkgs.ghostty
-        pkgs.gnome-calculator
-        pkgs.gnome-calendar
-        pkgs.gnome-clocks
-        pkgs.gnome-logs
-        pkgs.gnome-text-editor
-        pkgs.gnome-weather
-        pkgs.loupe
-        pkgs.nautilus
-        pkgs.papers
-        pkgs.snapshot
+        pkgs.mako
       ]
-      ++ enabledExtensions
-      ++ optionals config.services.flatpak.enable [
-        setupFlathub
-        pkgs.gnome-software
-      ];
-
-    programs.seahorse.enable = mkDefault true;
-    programs.yubikey-touch-detector.enable = mkDefault true;
+      ++ optionals config.services.flatpak.enable [ setupFlathub ]
+      ++ optionals config.programs.firefox.enable [ pkgs.firefoxpwa ];
 
     # Add a default browser to use
     programs.firefox = {
@@ -77,6 +59,8 @@ in
       preferences."browser.tabs.inTitlebar" = mkIf (
         !config.services.xserver.desktopManager.gnome.enable
       ) 0;
+      # Make it possible for firefox to be the only desktop app.
+      nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
     };
 
     # We use systemd-resolved
@@ -86,39 +70,5 @@ in
     # plus setting this to true means that geoclue will be dependent on avahi
     # being enabled, since NMEA support in geoclue uses avahi.
     services.geoclue2.enableNmea = mkDefault false;
-
-    services.xserver.desktopManager.gnome.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
-
-    services.gnome.core-utilities.enable = false;
-
-    programs.dconf = {
-      enable = true;
-      profiles.user.databases = [
-        {
-          settings = {
-            "org/gnome/desktop/wm/preferences".resize-with-right-button = gvariant.mkBoolean true;
-            "org/gnome/system/location".enabled = gvariant.mkBoolean true;
-            "org/gnome/desktop/datetime".automatic-timezone = gvariant.mkBoolean true;
-            "org/gnome/mutter".experimental-features = gvariant.mkArray [
-              "scale-monitor-framebuffer"
-            ];
-            "org/gnome/shell" = {
-              enabled-extensions = gvariant.mkArray (map (extension: extension.extensionUuid) enabledExtensions);
-              favorite-apps = gvariant.mkArray [
-                "com.mitchellh.ghostty.desktop"
-                "firefox.desktop"
-              ];
-            };
-            "org/gnome/desktop/background" = {
-              picture-uri = gvariant.mkString ""; # "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
-              picture-uri-dark = gvariant.mkString ""; # "file:///run/current-system/sw/share/backgrounds/gnome/vnc-d.png";
-              primary-color = gvariant.mkString "#${backgroundColor}";
-              secondary-color = gvariant.mkString "#${backgroundColor}";
-            };
-          };
-        }
-      ];
-    };
   };
 }
