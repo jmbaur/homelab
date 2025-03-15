8dde88d8c82b13a5647d5c78fe2e9ea6b2343d1d
Jared Baur <jaredbaur@fastmail.com>
2025-03-14 21:03:44 -0700

    Add external display brightness control

--- nixos-modules/desktop/default.nix
+++ nixos-modules/desktop/default.nix
@@ -10,6 +10,7 @@ let
     mkDefault
     mkEnableOption
     mkIf
+    mkMerge
     optionals
     ;
 
@@ -23,66 +24,97 @@ in
 {
   options.custom.desktop.enable = mkEnableOption "desktop";
 
-  config = mkIf cfg.enable {
-    custom.normalUser.enable = true;
-    custom.basicNetwork.enable = true;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      custom.normalUser.enable = true;
 
-    networking.wireless.iwd.enable = true;
-    networking.networkmanager.wifi.backend = "iwd";
+      services.automatic-timezoned.enable = mkDefault true;
+      services.dbus.packages = [ pkgs.mako ];
+      services.flatpak.enable = mkDefault true;
+      services.fwupd.enable = mkDefault true;
+      services.power-profiles-daemon.enable = mkDefault true;
+      services.printing.enable = mkDefault true;
+      services.upower.enable = mkDefault true;
 
-    hardware.bluetooth.enable = true;
+      programs.sway = {
+        enable = true;
+        wrapperFeatures = {
+          base = true;
+          gtk = true;
+        };
+      };
 
-    services.automatic-timezoned.enable = mkDefault true;
-    services.dbus.packages = [ pkgs.mako ];
-    services.flatpak.enable = mkDefault true;
-    services.fwupd.enable = mkDefault true;
-    services.power-profiles-daemon.enable = mkDefault true;
-    services.printing.enable = mkDefault true;
-    services.upower.enable = mkDefault true;
+      programs.yubikey-touch-detector.enable = mkDefault true;
 
-    programs.sway.enable = true;
-    programs.yubikey-touch-detector.enable = mkDefault true;
+      systemd.packages = [ pkgs.mako ];
 
-    systemd.packages = [ pkgs.mako ];
+      # TODO(jared): sway issue, documented here: https://codeberg.org/dnkl/foot/issues/1675#issuecomment-1736249
+      environment.etc."xdg/foot/foot.ini".source = (pkgs.formats.ini { }).generate "foot.ini" {
+        main.resize-by-cells = false;
+      };
 
-    # TODO(jared): sway issue, documented here: https://codeberg.org/dnkl/foot/issues/1675#issuecomment-1736249
-    environment.etc."xdg/foot/foot.ini".source = (pkgs.formats.ini { }).generate "foot.ini" {
-      main.resize-by-cells = false;
-    };
+      environment.systemPackages =
+        [
+          pkgs.mako
+          (pkgs.symlinkJoin {
+            name = "default-${pkgs.xcursor-chromeos.name}";
+            paths = [ pkgs.xcursor-chromeos ];
+            postBuild = ''
+              ln -sf $out/share/icons/${pkgs.xcursor-chromeos.pname} $out/share/icons/default
+            '';
+          })
+        ]
+        ++ optionals config.services.flatpak.enable [ setupFlathub ]
+        ++ optionals config.programs.firefox.enable [ pkgs.firefoxpwa ];
 
-    environment.systemPackages =
-      [
-        pkgs.mako
-        (pkgs.symlinkJoin {
-          name = "default-${pkgs.xcursor-chromeos.name}";
-          paths = [ pkgs.xcursor-chromeos ];
-          postBuild = ''
-            ln -sf $out/share/icons/${pkgs.xcursor-chromeos.pname} $out/share/icons/default
-          '';
-        })
-      ]
-      ++ optionals config.services.flatpak.enable [ setupFlathub ]
-      ++ optionals config.programs.firefox.enable [ pkgs.firefoxpwa ];
+      # Add a default browser to use
+      programs.firefox = {
+        enable = mkDefault true;
+        # Allow users to override preferences set here
+        preferencesStatus = "user";
+        # Default value only looks good in GNOME
+        preferences."browser.tabs.inTitlebar" = mkIf (
+          !config.services.xserver.desktopManager.gnome.enable
+        ) 0;
+        # Make it possible for firefox to be the only desktop app.
+        nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
+      };
+    }
 
-    # Add a default browser to use
-    programs.firefox = {
-      enable = mkDefault true;
-      # Allow users to override preferences set here
-      preferencesStatus = "user";
-      # Default value only looks good in GNOME
-      preferences."browser.tabs.inTitlebar" = mkIf (
-        !config.services.xserver.desktopManager.gnome.enable
-      ) 0;
-      # Make it possible for firefox to be the only desktop app.
-      nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
-    };
+    # Networking defaults
+    {
+      custom.basicNetwork.enable = true;
 
-    # We use systemd-resolved
-    services.avahi.enable = false;
+      networking.wireless.iwd.enable = true;
+      networking.networkmanager.wifi.backend = "iwd";
 
-    # It would be uncommon for a desktop system to have an NMEA serial device,
-    # plus setting this to true means that geoclue will be dependent on avahi
-    # being enabled, since NMEA support in geoclue uses avahi.
-    services.geoclue2.enableNmea = mkDefault false;
-  };
+      hardware.bluetooth.enable = true;
+
+      # We use systemd-resolved
+      services.avahi.enable = false;
+
+      # It would be uncommon for a desktop system to have an NMEA serial device,
+      # plus setting this to true means that geoclue will be dependent on avahi
+      # being enabled, since NMEA support in geoclue uses avahi.
+      services.geoclue2.enableNmea = mkDefault false;
+    }
+
+    # Display brightness
+    {
+      # TODO(jared): Find or write a utility that can modify brightness of
+      # internal and external displays at once.
+      environment.systemPackages = [
+        pkgs.brightnessctl
+        pkgs.ddcutil
+      ];
+
+      boot.kernelModules = [ "i2c-dev" ];
+
+      # The usual case, using TAG+="uaccess":  If a /dev/i2c device is associated
+      # with a video adapter, grant the current user access to it.
+      services.udev.extraRules = ''
+        SUBSYSTEM=="i2c-dev", KERNEL=="i2c-[0-9]*", ATTRS{class}=="0x030000", TAG+="uaccess"
+      '';
+    }
+  ]);
 }
