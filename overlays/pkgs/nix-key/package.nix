324ad5c9d8d1c7608898a6ed6aabd1ca44648986
Jared Baur <jaredbaur@fastmail.com>
2025-03-18 08:05:35 -0700

    Update inputs
    And get dynamic linking with zig working

--- overlays/pkgs/nix-key/package.nix
+++ overlays/pkgs/nix-key/package.nix
@@ -1,28 +1,27 @@
-# TODO(jared): Fix cross-compilation and get dynamic linking working.
+{
+  lib,
+  libsodium,
+  stdenv,
+  zig_0_14,
+}:
 
-{ pkgsStatic }:
-pkgsStatic.callPackage (
-  {
-    lib,
-    libsodium,
-    runCommand,
-    stdenv,
-    zig_0_14,
-  }:
-  runCommand "nix-key" { depsBuildBuild = [ zig_0_14 ]; } ''
+stdenv.mkDerivation {
+  name = "nix-key";
+  depsBuildBuild = [ zig_0_14 ];
+  passthru.broken = !stdenv.hostPlatform.isGnu;
+  buildCommand = ''
     mkdir -p $out/bin
 
     export ZIG_GLOBAL_CACHE_DIR=$TEMPDIR
     args=(
-      "-fno-lld"
       "-j$NIX_BUILD_CORES"
-      "-target ${stdenv.hostPlatform.qemuArch}-linux"
-      "--dynamic-linker ${stdenv.cc.libc}/lib/ld-linux*"
-      "-I${lib.getDev stdenv.cc.libc}/include"
+      "-target ${stdenv.hostPlatform.qemuArch}-linux-gnu"
+      "--dynamic-linker $(cat $NIX_CC/nix-support/dynamic-linker)"
       "-I${lib.getDev libsodium}/include"
       "-L${lib.getLib libsodium}/lib"
       "-lc"
       "-lsodium"
+      "-feach-lib-rpath"
     )
     zig test ''${args[@]} -j$NIX_BUILD_CORES ${./nix-key.zig}
     zig build-exe ''${args[@]} \
@@ -30,5 +29,5 @@ pkgsStatic.callPackage (
       -fstrip -O ReleaseSafe \
       ${./nix-key.zig}
     rm -f $out/bin/*.o
-  ''
-) { }
+  '';
+}
