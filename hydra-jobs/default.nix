54e175d0b471758068b2defcf31301500d5387f9
Jared Baur <jaredbaur@fastmail.com>
2025-08-03 17:01:16 -0700

    Update hydraJobs output
    `hydraJobs` does not have a formal structure, so do it however we want.

--- hydra-jobs/default.nix
+++ hydra-jobs/default.nix
@@ -1,42 +1,7 @@
 inputs:
 
-let
-  inherit (inputs.nixpkgs.lib)
-    filterAttrs
-    flatten
-    listToAttrs
-    mapAttrs
-    mapAttrsToList
-    systems
-    zipAttrs
-    ;
-
-  nixosConfigurations = zipAttrs (
-    mapAttrsToList (
-      name:
-      { config, ... }:
-      {
-        "${config.nixpkgs.buildPlatform.system}" = [
-          {
-            name = "${name}-toplevel";
-            value = config.system.build.toplevel;
-          }
-          {
-            name = "${name}-recovery";
-            value = config.system.build.recoveryImage;
-          }
-        ];
-      }
-    ) inputs.self.nixosConfigurations
-  );
-in
-mapAttrs
-  (
-    system: _:
-    listToAttrs (flatten (nixosConfigurations.${system} or [ ]))
-    //
-      # TODO(jared): use _hydraAggregate to gate nixosConfigurations on checks
-      inputs.self.checks.${system}
-  )
-  # Filter out non-linux platforms, since we only build on linux
-  (filterAttrs (system: _: (systems.elaborate system).isLinux) inputs.self.legacyPackages)
+(builtins.mapAttrs (_: nixosConfig: {
+  inherit (nixosConfig.config.system.build) toplevel recoveryImage;
+}) inputs.self.nixosConfigurations)
+// inputs.self.packages
+// inputs.self.checks
