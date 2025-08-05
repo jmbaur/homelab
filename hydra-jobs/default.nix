df6333ebd89144299155ea9459ff08c5dc7219cd
Jared Baur <jaredbaur@fastmail.com>
2025-08-04 21:03:10 -0700

    Remove darwin from hydraJobs...again

--- hydra-jobs/default.nix
+++ hydra-jobs/default.nix
@@ -1,7 +1,11 @@
 inputs:
 
+let
+  inherit (inputs.nixpkgs.lib) filterAttrs systems;
+  onlyLinuxOutput = filterAttrs (system: _: (systems.elaborate system).isLinux);
+in
 (builtins.mapAttrs (_: nixosConfig: {
   inherit (nixosConfig.config.system.build) toplevel recoveryImage;
 }) inputs.self.nixosConfigurations)
-// inputs.self.packages
-// inputs.self.checks
+// onlyLinuxOutput inputs.self.packages
+// onlyLinuxOutput inputs.self.checks
