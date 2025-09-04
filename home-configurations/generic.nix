cb2f005cec4ae7ef9177569b2ff25e3ccdbf926c
Jared Baur <jaredbaur@fastmail.com>
2025-09-04 08:22:44 +0100

    Use correct gh auth git-credential helper config for HTTPS credentials

--- home-configurations/generic.nix
+++ home-configurations/generic.nix
@@ -126,8 +126,8 @@
           );
           branch.sort = "-committerdate";
           commit.verbose = true;
-          "credential \"https://gist.github.com\"".helper = "gh auth git-credential";
-          "credential \"https://github.com\"".helper = "gh auth git-credential";
+          "credential \"https://gist.github.com\"".helper = "!gh auth git-credential";
+          "credential \"https://github.com\"".helper = "!gh auth git-credential";
           diff = {
             algorithm = "histogram";
             tool = "difftastic";
