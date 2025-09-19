e1923553320eae28464deaa6499ec9669ef2c6bc
Jared Baur <jaredbaur@fastmail.com>
2025-09-19 13:04:04 -0700

    Fix garage-door build

--- overlays/by-name/homelab-garage-door/package.nix
+++ overlays/by-name/homelab-garage-door/package.nix
@@ -8,18 +8,19 @@ runCommand "homelab-garage-door"
   {
     depsBuildBuild = [ zig_0_15 ];
     meta.mainProgram = "homelab-garage-door";
+    src = ./.;
   }
   ''
     mkdir -p $out/bin
 
     export ZIG_GLOBAL_CACHE_DIR=$TEMPDIR
-    zig test -lc -j$NIX_BUILD_CORES ${./homelab-garage-door.zig}
+    zig test -lc -j$NIX_BUILD_CORES $src/homelab-garage-door.zig
     zig build-exe \
       -lc \
       -femit-bin=$out/bin/homelab-garage-door \
       -fstrip \
       -O ReleaseSafe \
       -target ${stdenv.hostPlatform.qemuArch}-${stdenv.hostPlatform.rust.platform.os} \
-      ${./homelab-garage-door.zig}
+      $src/homelab-garage-door.zig
     rm -f $out/bin/*.o
   ''
