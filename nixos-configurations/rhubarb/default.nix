c8787d79d9bb5eaca57f4a4f80512e19e625494f
Jared Baur <jaredbaur@fastmail.com>
2025-09-28 20:06:03 -0700

    Add imx708 camera driver

--- nixos-configurations/rhubarb/default.nix
+++ nixos-configurations/rhubarb/default.nix
@@ -7,6 +7,8 @@
 {
   config = lib.mkMerge [
     {
+      nixpkgs.buildPlatform = "x86_64-linux";
+
       hardware.rpi4.enable = true;
 
       custom.server = {
@@ -46,6 +48,36 @@
       ];
     }
     {
+      boot.kernelPatches = [
+        {
+          name = "media/i2c: Add a driver for the Sony IMX708 image sensor";
+          patch = pkgs.fetchpatch {
+            url = "https://github.com/raspberrypi/linux/commit/9bbe36729058ba8ca2ee8cdd99b3e459211f81fe.patch";
+            excludes = [ "drivers/media/i2c/Makefile" ];
+            hash = "sha256-MgxZLpWSztSI3TEO5K05KDaVU4hAvnIaaixhJ0r4K00=";
+          };
+        }
+        {
+          # The patch from above doesn't apply cleanly to drivers/media/i2c/Makefile, do the change here instead.
+          name = "add imx708 to makefile";
+          patch = ./imx708-makefile.patch;
+        }
+        {
+          name = "media: i2c: imx708: Fix lockdep issues.";
+          patch = pkgs.fetchpatch {
+            url = "https://github.com/raspberrypi/linux/commit/96f6b239ff694192416df9cc3f8e130fb7b19301.patch";
+            hash = "sha256-KHQnpwukO7WU5CZptu9gcoqrV8j/Yh/K13sEj7QxrII=";
+          };
+        }
+        {
+          name = "media: i2c: Tweak default PDAF gain table in imx708 driver";
+          patch = pkgs.fetchpatch {
+            url = "https://github.com/raspberrypi/linux/commit/686f5708baaafb35e03e6e56396339330d0fec48.patch";
+            hash = "sha256-6W8cCJMDRUBaVGynojWCkbCXKAvmkUHWRi29XWESMWk=";
+          };
+        }
+      ];
+
       networking.wireless.iwd.enable = true;
       environment.systemPackages = [ pkgs.iw ];
 
@@ -64,8 +96,6 @@
       services.yggdrasil.settings.Peers = [ "tls://celery.jmbaur.com:443" ];
 
       custom.yggdrasil.peers.pumpkin.allowedTCPPorts = [ 8080 ];
-
-      nixpkgs.buildPlatform = "x86_64-linux";
     }
   ];
 }
