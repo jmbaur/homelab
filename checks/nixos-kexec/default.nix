1249a07506ac896cdf3cf9f973d34c3f15fd1940
Jared Baur <jaredbaur@fastmail.com>
2026-01-03 23:42:43 -0800

    WIP

--- checks/nixos-kexec/default.nix
+++ checks/nixos-kexec/default.nix
@@ -23,6 +23,7 @@ testers.runNixOSTest {
     };
 
   testScript = ''
+    machine.start(allow_reboot=True)
     machine.fail("hello")
     machine.succeed("nixos-kexec /run/current-system/specialisation/hello")
     machine.connected = False
