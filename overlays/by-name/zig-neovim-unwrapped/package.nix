81941ad75e279b5e1b928714b72e72faf9c3ff35
Jared Baur <jaredbaur@fastmail.com>
2026-03-02 10:56:50 -0800

    Prevent zig from showing up in neovim runtime closure

--- overlays/by-name/zig-neovim-unwrapped/package.nix
+++ overlays/by-name/zig-neovim-unwrapped/package.nix
@@ -1,16 +1,24 @@
 {
   fetchFromGitHub,
+  lib,
   neovim-unwrapped,
+  nukeReferences,
+  patchelfUnstable,
+  pkgsBuildBuild,
+  stdenv,
   stdenvNoCC,
   zig_0_15,
 }:
 
-stdenvNoCC.mkDerivation (
+# static musl isn't capable of dlopen(), needed for treesitter to work
+assert !stdenv.hostPlatform.isStatic;
+stdenv.mkDerivation (
   finalAttrs:
   let
     deps = stdenvNoCC.mkDerivation {
       pname = finalAttrs.pname + "-deps";
-      inherit (finalAttrs) version src nativeBuildInputs;
+      inherit (finalAttrs) src version;
+      nativeBuildInputs = [ zig_0_15 ];
       buildCommand = ''
         export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
         runHook unpackPhase
@@ -37,17 +45,45 @@ stdenvNoCC.mkDerivation (
     __structuredAttrs = true;
     strictDeps = true;
 
+    nativeBuildInputs = [
+      nukeReferences
+      patchelfUnstable
+      zig_0_15
+    ];
+
+    # Prevent zig from being in the runtime closure
+    disallowedReferences = [ zig_0_15 ];
+
+    zigBuildFlags = [
+      "-Dtarget=${stdenv.hostPlatform.qemuArch}-${
+        {
+          darwin = "macos";
+          linux = "linux";
+        }
+        .${stdenv.hostPlatform.parsed.kernel.name}
+      }-gnu"
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      "-Ddynamic-linker=${lib.getLib stdenv.cc.libc}/lib/ld-linux-x86-64.so.2"
+    ];
+
     postConfigure = ''
       ln -s ${deps} $ZIG_GLOBAL_CACHE_DIR/p
     '';
-
-    nativeBuildInputs = [ zig_0_15 ];
 
     inherit (neovim-unwrapped) meta lua;
 
     # Not performed by the zig install, but needed by pkgs.wrapNeovimUnstable
     postInstall = ''
-      install -Dm0644 ${neovim-unwrapped}/share/applications/nvim.desktop $out/share/applications/nvim.desktop
+      install -Dm0644 ${pkgsBuildBuild.neovim-unwrapped}/share/applications/nvim.desktop $out/share/applications/nvim.desktop
+    '';
+
+    # Ensure we have the required runtime dependencies, zig builds don't add rpath
+    postFixup = ''
+      find $out/bin $out/share/nvim/runtime/parser -type f | while read i; do
+        nuke-refs -e $out -e ${lib.getLib stdenv.cc.libc} $i
+        patchelf $i --add-rpath ${lib.getLib stdenv.cc.libc}/lib # for libc and friends
+      done
     '';
   }
 )
