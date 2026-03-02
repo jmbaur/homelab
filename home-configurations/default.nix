0aa0505bf801fb5b9d8b9948b3a444283abc4dc2
Jared Baur <jaredbaur@fastmail.com>
2026-03-02 12:23:55 -0800

    Put home-manager configurations in hydra jobs

--- home-configurations/default.nix
+++ home-configurations/default.nix
@@ -1,28 +1,38 @@
 inputs:
-inputs.nixpkgs.lib.mapAttrs' (system: pkgs: {
-  name = "generic-${system}";
-  value = inputs.home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    modules = [
-      inputs.nix-index-database.homeModules.nix-index
-      (
-        { lib, ... }:
-        {
-          news.display = "silent";
-          home =
-            assert (
-              lib.assertMsg (builtins.hasAttr "currentTime" builtins) ''
-                This home-manager configuration must be evaluated with --impure in order for it to be generic over different hosts.
-              ''
-            );
+inputs.nixpkgs.lib.concatMapAttrs (
+  system: pkgs:
+  let
+    homeConfiguration =
+      {
+        stub ? false,
+      }:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          inputs.nix-index-database.homeModules.nix-index
+          (
+            { lib, ... }:
             {
-              stateVersion = "26.05";
-              username = builtins.getEnv "USER";
-              homeDirectory = builtins.getEnv "HOME";
-            };
-        }
-      )
-      ./generic.nix
-    ];
-  };
-}) inputs.self.legacyPackages
+              news.display = "silent";
+              home =
+                assert (
+                  lib.assertMsg (!stub -> builtins.hasAttr "currentTime" builtins) ''
+                    This home-manager configuration must be evaluated with --impure in order for it to be generic over different hosts.
+                  ''
+                );
+                {
+                  stateVersion = "26.05";
+                  username = if stub then "stub" else builtins.getEnv "USER";
+                  homeDirectory = if stub then "/home/stub" else builtins.getEnv "HOME";
+                };
+            }
+          )
+          ./generic.nix
+        ];
+      };
+  in
+  {
+    "generic-${system}" = homeConfiguration { stub = false; };
+    "generic-${system}-stub" = homeConfiguration { stub = true; };
+  }
+) inputs.self.legacyPackages
--- hydra-jobs/default.nix
+++ hydra-jobs/default.nix
@@ -2,13 +2,18 @@ inputs:
 
 let
   inherit (inputs.nixpkgs.lib)
+    const
     filterAttrs
+    flip
+    hasSuffix
+    mapAttrs
     optionalAttrs
     recursiveUpdate
     systems
     ;
   onlyLinuxOutput = filterAttrs (system: _: (systems.elaborate system).isLinux);
 in
+# TODO(jared): put nixos configurations in their own attrset
 (builtins.mapAttrs (
   _: nixosConfig:
   recursiveUpdate
@@ -22,3 +27,9 @@ in
     )
 ) inputs.self.nixosConfigurations)
 // recursiveUpdate (onlyLinuxOutput inputs.self.packages) (onlyLinuxOutput inputs.self.checks)
+// {
+  # The stub home configuration is not impure
+  homeConfigurations = mapAttrs (const (homeConfig: homeConfig.activationPackage)) (
+    filterAttrs (flip (const (hasSuffix "-stub"))) inputs.self.homeConfigurations
+  );
+}
