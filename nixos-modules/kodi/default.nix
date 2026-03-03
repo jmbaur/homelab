6d08ddab3be594c05b504671bb7d1d323f527d30
Jared Baur <jaredbaur@fastmail.com>
2026-03-02 19:46:39 -0800

    Remove unused plugins

--- nixos-modules/kodi/default.nix
+++ nixos-modules/kodi/default.nix
@@ -37,38 +37,14 @@ let
           wayland = pkgs.kodi-wayland;
         }
         .${cfg.backend}.override
-        {
-          sambaSupport = false; # deps don't cross-compile
-        }
+        { sambaSupport = false; } # deps don't cross-compile
       ).overrideAttrs
-      {
-        enableParallelBuilding = true;
-      }
+      { enableParallelBuilding = true; }
     ).withPackages
       (p: [
         p.inputstream-adaptive
         p.jellyfin
         p.joystick
-        (p.buildKodiAddon {
-          name = "subsonic-plugin";
-          namespace = "plugin.audio.subsonic";
-          src = pkgs.fetchFromGitHub {
-            owner = "warwickh";
-            repo = "plugin.audio.subsonic";
-            rev = "4fffcd143e24852ac5d7ab11df2323fd8e504cbc";
-            hash = "sha256-RklVMm+CX1jWeJn3d1WtMGrlLTGyIllBZql/iol82Eo=";
-          };
-        })
-        (p.buildKodiAddon {
-          name = "spotify-plugin";
-          namespace = "plugin.audio.spotify";
-          src = pkgs.fetchFromGitHub {
-            owner = "glk1001";
-            repo = "plugin.audio.spotify";
-            rev = "v1.3.11";
-            hash = "sha256-qztV5+sqWzkXMn3MVNSSTMlvW4eCreBmkXH8wi+1TNc=";
-          };
-        })
       ]);
 in
 {
