ba8c8e5d4b57016ed3f272be6f7145243bf057b3
Jared Baur <jaredbaur@fastmail.com>
2026-03-27 22:36:24 -0700

    Fix jared-neovim on macos

--- overlays/by-name/jared-neovim/package.nix
+++ overlays/by-name/jared-neovim/package.nix
@@ -9,6 +9,7 @@
   gofumpt,
   gopls,
   lua-language-server,
+  neovim-unwrapped,
   neovimUtils,
   nixd,
   nixfmt,
@@ -20,6 +21,7 @@
   rustfmt,
   shellcheck,
   shfmt,
+  stdenv,
   tex-fmt,
   texlab,
   texlive,
@@ -35,63 +37,66 @@
   zls_0_15,
 }:
 
-wrapNeovimUnstable zig-neovim-unwrapped (
-  neovimUtils.makeNeovimConfig {
-    customRC = "set exrc";
+wrapNeovimUnstable
+  # zig neovim build on macos currently doesn't work
+  (if stdenv.hostPlatform.isDarwin then neovim-unwrapped else zig-neovim-unwrapped)
+  (
+    neovimUtils.makeNeovimConfig {
+      customRC = "set exrc";
 
-    extraLuaPackages = ps: [ ps.fennel ];
+      extraLuaPackages = ps: [ ps.fennel ];
 
-    vimAlias = true;
+      vimAlias = true;
 
-    plugins = [
-      (vimUtils.buildVimPlugin {
-        name = "jared-neovim-config";
-        src = ./nvim;
-        buildPhase = "make -j$NIX_BUILD_CORES install";
-        dontInstall = true;
-        depsBuildBuild = [ pkgsBuildBuild.neovim-unwrapped.lua.pkgs.fennel ];
-        runtimeDeps = [
-          bash-language-server
-          clang-tools
-          dts-lsp
-          fd
-          fennel-ls
-          fnlfmt
-          go-tools
-          gofumpt
-          gopls
-          lua-language-server
-          nixd
-          nixfmt
-          pyright
-          ripgrep
-          ruff
-          rust-analyzer
-          rustfmt
-          shellcheck
-          shfmt
-          tex-fmt
-          texlab
-          texlive.pkgs.latexmk
-          tofu-ls
-          tree-sitter
-          ttags
-          typescript-go
-          zig_0_15
-          zls_0_15
-        ];
-      })
-    ]
-    ++ (with vimPlugins; [
-      direnv-vim
-      nvim-lspconfig
-      nvim-treesitter.withAllGrammars
-      vim-dispatch
-      vim-eunuch
-      vim-fugitive
-      vim-rsi
-      vim-surround
-      vim-vinegar
-    ]);
-  }
-)
+      plugins = [
+        (vimUtils.buildVimPlugin {
+          name = "jared-neovim-config";
+          src = ./nvim;
+          buildPhase = "make -j$NIX_BUILD_CORES install";
+          dontInstall = true;
+          depsBuildBuild = [ pkgsBuildBuild.neovim-unwrapped.lua.pkgs.fennel ];
+          runtimeDeps = [
+            bash-language-server
+            clang-tools
+            dts-lsp
+            fd
+            fennel-ls
+            fnlfmt
+            go-tools
+            gofumpt
+            gopls
+            lua-language-server
+            nixd
+            nixfmt
+            pyright
+            ripgrep
+            ruff
+            rust-analyzer
+            rustfmt
+            shellcheck
+            shfmt
+            tex-fmt
+            texlab
+            texlive.pkgs.latexmk
+            tofu-ls
+            tree-sitter
+            ttags
+            typescript-go
+            zig_0_15
+            zls_0_15
+          ];
+        })
+      ]
+      ++ (with vimPlugins; [
+        direnv-vim
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+        vim-dispatch
+        vim-eunuch
+        vim-fugitive
+        vim-rsi
+        vim-surround
+        vim-vinegar
+      ]);
+    }
+  )
