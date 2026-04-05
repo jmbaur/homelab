d79a8ccaa1139b8c69b819e6b6572f31f51f5e9c
Jared Baur <jaredbaur@fastmail.com>
2026-04-05 11:26:01 -0700

    Bump nvim

--- overlays/by-name/zig-neovim-unwrapped/package.nix
+++ overlays/by-name/zig-neovim-unwrapped/package.nix
@@ -30,8 +30,8 @@ stdenv.mkDerivation (
     src = fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "9927d9259d9a91f61fd14cec31bb848ac78d0863";
-      hash = "sha256-ff5Eiz1NOpA39ga2GNxR/dCZKcujMg2MyNMOmlUg+tI=";
+      rev = "398ee83f4f4584322fdc67458a89b5b4c57be49c";
+      hash = "sha256-+YhCdxcIh7bMdTijRzpqX22WhFgJmLKdSztAwo1SCxg=";
     };
 
     __structuredAttrs = true;
