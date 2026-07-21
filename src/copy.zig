c82177b8b8e180c4b755fbe658d0b485318b0a48
Jared Baur <jaredbaur@fastmail.com>
2026-07-21 09:59:01 -0700

    Allow copy command to copy more bytes

--- src/copy.zig
+++ src/copy.zig
@@ -3,17 +3,27 @@ const std = @import("std");
 const encoder = std.base64.standard.Encoder;
 
 pub fn main(init: std.process.Init) !void {
-    var in_buf = [_]u8{0} ** 4096;
+    var in_buf = [_]u8{0} ** 1024;
     var out_buf = [_]u8{0} ** 1024;
 
     var stdout_file = std.Io.File.stdout().writer(init.io, &out_buf);
     var stdout = &stdout_file.interface;
 
-    try stdout.writeAll("\x1b]52;c;");
     var stdin_file = std.Io.File.stdin().reader(init.io, &in_buf);
     var stdin = &stdin_file.interface;
-    const input = try stdin.takeDelimiterExclusive(0);
-    try std.base64.standard.Encoder.encodeWriter(stdout, input);
+
+    var input: std.Io.Writer.Allocating = .init(init.arena.allocator());
+
+    while (true) {
+        const n_bytes = stdin.stream(&input.writer, .unlimited) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        _ = n_bytes;
+    }
+
+    try stdout.writeAll("\x1b]52;c;");
+    try std.base64.standard.Encoder.encodeWriter(stdout, input.written());
     try stdout.writeAll("\x07");
     try stdout.flush();
 }
