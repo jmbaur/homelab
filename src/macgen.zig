77b81d9696302af7d557b8409fe4ce5a20c678e9
Jared Baur <jaredbaur@fastmail.com>
2026-08-08 19:35:13 -0700

    Ensure macgen output has the unicast and locally administered bits set

--- src/macgen.zig
+++ src/macgen.zig
@@ -8,6 +8,9 @@ pub fn main(init: std.process.Init) !void {
     var stdout_file = std.Io.File.stdout().writer(init.io, &out_buf);
     var stdout = &stdout_file.interface;
 
+    buf[0] &= 0xfe; // unicast
+    buf[0] |= 0b10; // locally administered
+
     for (0..6) |i| {
         try stdout.print("{x:0>2}", .{buf[i]});
         if (i != 5) {
