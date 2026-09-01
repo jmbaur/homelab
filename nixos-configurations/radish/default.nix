352af6e3ee4334279d6b0d7972b6333e8c7e5052
Jared Baur <jaredbaur@fastmail.com>
2026-09-01 11:08:32 -0700

    See if linux 7.2.x is more stable on radish
    For GPU issues

--- nixos-configurations/radish/default.nix
+++ nixos-configurations/radish/default.nix
@@ -23,7 +23,7 @@
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
-      boot.kernelPackages = pkgs.linuxPackages_7_1;
+      boot.kernelPackages = pkgs.linuxPackages_7_2;
 
       hardware.cpu.intel.npu.enable = true;
       hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
