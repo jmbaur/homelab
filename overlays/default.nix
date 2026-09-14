8d04902bab0e76f28f78aae9a3c856cda9240d0a
Jared Baur <jaredbaur@fastmail.com>
2026-09-14 13:03:41 -0700

    Remove emacs overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -51,14 +51,6 @@ inputs: {
 
     # all other packages
     (final: prev: {
-      emacs = prev.emacs.overrideAttrs (old: {
-        __structuredAttrs = true;
-        strictDeps = true;
-        nativeBuildInputs =
-          old.nativeBuildInputs
-          ++ final.lib.optionals final.stdenv.buildPlatform.isDarwin [ final.pkgsBuildHost.darwin.sigtool ];
-      });
-
       cros-ec-fizz = prev.cros-ec.override { board = "fizz"; };
 
       gnome-console =
