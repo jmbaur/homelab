9a257d196cfefdd52bae2bd8e1e7ba44c8632089
Jared Baur <jaredbaur@fastmail.com>
2026-09-15 11:26:45 -0700

    Add more stuff to fennel

--- nixos-configurations/fennel/default.nix
+++ nixos-configurations/fennel/default.nix
@@ -1,4 +1,20 @@
-{ lib, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # cuda (on jetspn) does not currently cross-compile
+  nixpkgs.buildPlatform = "aarch64-linux";
+
+  nixpkgs.config = {
+    allowUnfreePredicate = pkgs._cuda.lib.allowUnfreeCudaPredicate;
+    cudaCapabilities = [ "11.0" ];
+    cudaForwardCompat = true;
+    cudaSupport = true;
+  };
+
   hardware.nvidia-jetpack = {
     enable = true;
     majorVersion = "7";
@@ -16,8 +32,7 @@
     }
   ];
 
-  # TODO(jared): doesn't cross
-  # hardware.graphics.enable = true;
+  hardware.graphics.enable = true;
 
   users.users.root.initialPassword = "";
 
@@ -25,4 +40,28 @@
 
   # TODO(jared): be more specific
   custom.recovery.targetDisk = "/dev/nvme0n1";
+
+  custom.yggdrasil.peers.celery.allowedTCPPorts = [ config.services.llama-cpp.settings.port ];
+
+  environment.systemPackages = [ config.services.llama-cpp.package ];
+  services.llama-cpp = {
+    enable = true;
+    package = pkgs.llama-cpp.override { cudaSupport = true; };
+    openFirewall = false;
+    settings = {
+      port = 8080;
+      host = "::";
+
+      # TODO(jared): fill out
+      # model = "./Qwen3.8-27B-UD-Q8_K_XL.gguf";
+      # gpu-layers = 999;
+      # ctx-size = 222144;
+      # temp = "1.0";
+      # top-p = "0.95";
+      # top-k = 20;
+      # min-p = "0.0";
+      # mmproj = "./Qwen3.8-27B-GGUF/mmproj-BF16.gguf";
+      # reasoning = "off";
+    };
+  };
 }
